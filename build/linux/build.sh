ca14b44110f48353ffd57f5b88b1d271b3304dba
Romain Guy <romainguy@google.com>
2018-08-10 13:22:31 -0700

    Run unit tests in continuous builds

--- build/linux/build.sh
+++ build/linux/build.sh
@@ -20,4 +20,4 @@ source `dirname $0`/ci-common.sh
 source `dirname $0`/../common/build-common.sh
 
 pushd `dirname $0`/../.. > /dev/null
-./build.sh -c $GENERATE_ARCHIVES $BUILD_DEBUG $BUILD_RELEASE
+./build.sh -c $RUN_TESTS $GENERATE_ARCHIVES $BUILD_DEBUG $BUILD_RELEASE
