3246f3d2b5a10b6dd6ad447db3a524846df2f131
Benjamin Doherty <bendoherty@google.com>
2018-08-27 14:31:40 -0700

    Introduce property serialization

--- tools/tungsten/core/src/com/google/android/filament/tungsten/model/Properties.kt
+++ tools/tungsten/core/src/com/google/android/filament/tungsten/model/Properties.kt
@@ -21,12 +21,31 @@ import com.google.android.filament.MaterialInstance
 sealed class PropertyValue {
 
     abstract fun applyToMaterialInstance(materialInstance: MaterialInstance, name: String)
+
+    /**
+     * Serialize this value into a Kotlin List, Map, String, or Number
+     */
+    abstract fun serialize(): Any
+
+    /**
+     * Deserialize into a new PropertyValue
+     */
+    abstract fun deserialize(value: Any): PropertyValue
 }
 
 data class Float3(val x: Float = 0.0f, val y: Float = 0.0f, val z: Float = 0.0f) : PropertyValue() {
 
     override fun applyToMaterialInstance(materialInstance: MaterialInstance, name: String) {
         materialInstance.setParameter(name, x, y, z)
+    }
+
+    override fun serialize() = listOf(x, y, z)
+
+    override fun deserialize(value: Any): PropertyValue {
+        if (value !is List<*> || value.size < 3) return this
+        val (x, y, z) = value
+        if (x !is Number || y !is Number || z !is Number) return this
+        return Float3(x.toFloat(), y.toFloat(), z.toFloat())
     }
 }
 
