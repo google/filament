3e6f1a2c39ee05eb4af4846ccc65a951b28e184f
Ben Doherty <benjdoherty15@gmail.com>
2018-09-13 17:30:33 -0700

    Tungsten: add orbit cam manipulator (#244)
    * Add orbit cam to Tungsten
    
    * Add kotlin-math library
    
    * Flip mouse wheel event sign if coming from a physical wheel

--- tools/tungsten/core/src/com/google/android/filament/tungsten/ui/TungstenViewer.kt
+++ tools/tungsten/core/src/com/google/android/filament/tungsten/ui/TungstenViewer.kt
@@ -16,27 +16,73 @@
 
 package com.google.android.filament.tungsten.ui
 
+import com.curiouscreature.kotlin.math.Float2
 import com.google.android.filament.Camera
 import com.google.android.filament.Viewport
 import com.google.android.filament.tungsten.Filament
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import java.awt.event.MouseMotionListener
 
-class TungstenViewer(val camera: Camera, val previewMeshPanel: PreviewMeshPanel)
+private const val SPEED_MULTIPLIER = 100.0f
+private const val DOLLY_MULTIPLIER = 5.0f
+
+private fun MouseEvent.toFloat2() = Float2(this.x.toFloat(), this.y.toFloat())
+
+class TungstenViewer(camera: Camera, val previewMeshPanel: PreviewMeshPanel)
     : Filament.Viewer() {
 
     private val previewCamera = PreviewCamera(camera)
-    private var time: Long = 0
+    private val manipulator = CameraManipulator(camera)
+
+    private var lastMouse: Float2? = null
+
+    init {
+        previewMeshPanel.addMouseMotionListener(object : MouseMotionListener {
+            override fun mouseMoved(e: MouseEvent?) { }
+
+            override fun mouseDragged(e: MouseEvent?) {
+                e ?: return
+                lastMouse?.let {
+                    val delta = e.toFloat2() - it
+                    val scaledDelta = delta / Float2(previewMeshPanel.width.toFloat(),
+                            previewMeshPanel.height.toFloat())
+                    manipulator.rotate(scaledDelta, SPEED_MULTIPLIER)
+                }
+                lastMouse = e.toFloat2()
+            }
+        })
+        previewMeshPanel.addMouseListener(object : MouseListener {
+            override fun mouseReleased(e: MouseEvent?) { }
+
+            override fun mouseEntered(e: MouseEvent?) { }
+
+            override fun mouseClicked(e: MouseEvent?) { }
+
+            override fun mouseExited(e: MouseEvent?) { }
+
+            override fun mousePressed(e: MouseEvent?) {
+                e ?: return
+                lastMouse = e.toFloat2()
+            }
+        })
+        previewMeshPanel.addMouseWheelListener { e ->
+            e?.let { mouseWheelEvent ->
+                // Swing reports wheel events caused by a physical wheel (opposed to a trackpad)
+                // with an inverted sign. Flip it so it feels more intuitive.
+                val causedByWheel = mouseWheelEvent.wheelRotation != 0
+                val multiplier = if(causedByWheel) 1 else -1
+                manipulator.dolly(multiplier * mouseWheelEvent.preciseWheelRotation.toFloat() /
+                        previewMeshPanel.width, DOLLY_MULTIPLIER)
+            }
+        }
+    }
 
     override fun update(deltaTimeMs: Long) {
-        time += deltaTimeMs
+        manipulator.updateCameraTransform()
 
-        // Update preview camera
-        val delta = time / 2000.0 * Math.PI
-        val x = 3f * Math.sin(delta)
-        val z = 3f * Math.cos(delta)
-        camera.lookAt(x, 0.0, z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
-
-        val width = previewMeshPanel.getWidth()
-        val height = previewMeshPanel.getHeight()
+        val width = previewMeshPanel.width
+        val height = previewMeshPanel.height
         view.setViewport(Viewport(0, 0, width, height))
         previewCamera.setProjection(width, height)
     }
