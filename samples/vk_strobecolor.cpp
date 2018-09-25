804c9748191a45e1f56cb1869a78eb5f87701e27
Ben Doherty <benjdoherty15@gmail.com>
2018-09-25 13:28:32 -0700

    Fix, Vulkan samples not compiling on Windows (#306)

--- samples/vk_strobecolor.cpp
+++ samples/vk_strobecolor.cpp
@@ -25,7 +25,7 @@
 
 using namespace filament;
 
-int main() {
+int main(int argc, char** argv) {
     Config config;
     config.title = "strobecolor";
     config.backend = Engine::Backend::VULKAN;
