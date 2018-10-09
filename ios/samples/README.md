855f7ecd74780a08933d2f235e5fe702dba38934
Benjamin Doherty <bendoherty@google.com>
2018-10-09 16:12:53 +0200

    Add hello-triangle iOS sample

---
+++ ios/samples/README.md
@@ -0,0 +1,37 @@
+# Filament sample iOS app
+
+This directory contains a sample app that demonstrates how to use the Filament API in an iOS application.
+
+## Prequisites
+
+iOS support for Filament is experimental and uses the Vulkan backend via MoltenVK. OpenGL is not supported on iOS yet. Because the iOS simulators do not support Metal, Filament can not be run on simulators.
+
+Before attempting to build for iOS, read [Filament's README](../../README.md). You must first cross-compile Filament for ARM64 on a macOS host before running the sample. The easiest way to do this is to run the following command at the root of Filament's source tree:
+
+```
+$ ./build.sh -p ios debug
+```
+
+This will automatically download Apple's iOS toolchain file for CMake and build Filament for iOS.
+
+Afterwards, you should find all the Filament libraries inside of `out/ios-debug/filament/lib/arm64`:
+
+```
+libbluevk.a
+libfilabridge.a
+libfilaflat.a
+libfilament.a
+libutils.a
+```
+
+The sample application expects these libraries to be present here. Likewise, when building the sample in Release mode, the linker looks for the libraries in `out/ios-release/filament/lib/arm64`. To build Filament in Release mode, replace `debug` with `release` in the above `build.sh` command.
+
+The pre-compiled `libMoltenVK.dylib` library must be present in `third_party/moltenvk/ios`. When building, the Xcode project automatically signs and includes this library inside the application bundle.
+
+## Xcode
+
+Open up the Xcode project at `ios/samples/hello-triangle/hello-triangle.xcodeproj`. Before building you will need to sign in to your Apple developer account and select an appropriate development team in the project editor.
+
+## Building
+
+At this point you should be able to hit the "Run" icon to run the sample on your device.
---
+++ ios/samples/hello-triangle/hello-triangle/FilamentView.mm
@@ -0,0 +1,182 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "FilamentView.h"
+
+#import <filament/Engine.h>
+#import <filament/Renderer.h>
+#import <filament/Scene.h>
+#import <filament/View.h>
+#import <filament/RenderableManager.h>
+#import <filament/TransformManager.h>
+
+using namespace filament;
+using utils::Entity;
+using utils::EntityManager;
+
+struct App {
+    VertexBuffer* vb;
+    IndexBuffer* ib;
+    Material* mat;
+    Entity renderable;
+};
+
+struct Vertex {
+    math::float2 position;
+    uint32_t color;
+};
+
+static const Vertex TRIANGLE_VERTICES[3] = {
+    {{1, 0}, 0xffff0000u},
+    {{cos(M_PI * 2 / 3), sin(M_PI * 2 / 3)}, 0xff00ff00u},
+    {{cos(M_PI * 4 / 3), sin(M_PI * 4 / 3)}, 0xff0000ffu},
+};
+
+static constexpr uint16_t TRIANGLE_INDICES[3] = { 0, 1, 2 };
+
+static constexpr uint8_t BAKED_COLOR_PACKAGE[] = {
+#include "bakedColor.inc"
+};
+
+@implementation FilamentView {
+    Engine* engine;
+    Renderer* renderer;
+    Scene* scene;
+    View* filaView;
+    Camera* camera;
+    SwapChain* swapChain;
+    App app;
+    CADisplayLink* displayLink;
+}
+
+- (instancetype)initWithCoder:(NSCoder*)coder
+{
+    if (self = [super initWithCoder:coder]) {
+        [self initializeMetalLayer];
+        [self initializeFilament];
+        self.contentScaleFactor = UIScreen.mainScreen.nativeScale;
+    }
+
+    // Call renderloop 60 times a second.
+    displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(renderloop)];
+    displayLink.preferredFramesPerSecond = 60;
+    [displayLink addToRunLoop:NSRunLoop.currentRunLoop forMode:NSDefaultRunLoopMode];
+
+    return self;
+}
+
+- (void)dealloc
+{
+    engine->destroy(renderer);
+    engine->destroy(scene);
+    engine->destroy(filaView);
+    engine->destroy(camera);
+    engine->destroy(swapChain);
+    engine->destroy(&engine);
+}
+
+- (void)initializeMetalLayer
+{
+    CAMetalLayer* metalLayer = (CAMetalLayer*) self.layer;
+    metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+
+    const CGSize size = self.bounds.size;
+    const float contentScale = [UIScreen mainScreen].nativeScale;
+    metalLayer.drawableSize = CGSizeMake(size.width * contentScale, size.height * contentScale);
+}
+
+- (void)initializeFilament
+{
+    engine = Engine::create(filament::Engine::Backend::VULKAN);
+    swapChain = engine->createSwapChain((__bridge void*) self.layer);
+    renderer = engine->createRenderer();
+    scene = engine->createScene();
+    camera = engine->createCamera();
+
+    filaView = engine->createView();
+    filaView->setClearColor({0.1, 0.125, 0.25, 1.0});
+    filaView->setPostProcessingEnabled(false);
+    filaView->setDepthPrepass(filament::View::DepthPrepass::DISABLED);
+
+    app.vb = VertexBuffer::Builder()
+        .vertexCount(3)
+        .bufferCount(1)
+        .attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT2, 0, 12)
+        .attribute(VertexAttribute::COLOR, 0, VertexBuffer::AttributeType::UBYTE4, 8, 12)
+        .normalized(VertexAttribute::COLOR)
+        .build(*engine);
+    app.vb->setBufferAt(*engine, 0,
+                        VertexBuffer::BufferDescriptor(TRIANGLE_VERTICES, 36, nullptr));
+
+    app.ib = IndexBuffer::Builder()
+        .indexCount(3)
+        .bufferType(IndexBuffer::IndexType::USHORT)
+        .build(*engine);
+    app.ib->setBuffer(*engine,
+                      IndexBuffer::BufferDescriptor(TRIANGLE_INDICES, 6, nullptr));
+
+    app.mat = Material::Builder()
+        .package((void*) BAKED_COLOR_PACKAGE, sizeof(BAKED_COLOR_PACKAGE))
+        .build(*engine);
+
+    app.renderable = EntityManager::get().create();
+    RenderableManager::Builder(1)
+        .boundingBox({{ -1, -1, -1 }, { 1, 1, 1 }})
+        .material(0, app.mat->getDefaultInstance())
+        .geometry(0, RenderableManager::PrimitiveType::TRIANGLES, app.vb, app.ib, 0, 3)
+        .culling(false)
+        .receiveShadows(false)
+        .castShadows(false)
+        .build(*engine, app.renderable);
+    scene->addEntity(app.renderable);
+
+    filaView->setScene(scene);
+    filaView->setCamera(camera);
+    CAMetalLayer* metalLayer = (CAMetalLayer*) self.layer;
+    CGSize size = metalLayer.drawableSize;
+    filaView->setViewport(Viewport(0, 0, size.width, size.height));
+}
+
+- (void)renderloop
+{
+    [self update];
+    if (self->renderer->beginFrame(self->swapChain)) {
+        self->renderer->render(self->filaView);
+        self->renderer->endFrame();
+    }
+}
+
+- (void)update
+{
+    // Rotate the camera.
+    constexpr float ZOOM = 1.5f;
+    const uint32_t w = filaView->getViewport().width;
+    const uint32_t h = filaView->getViewport().height;
+    const float aspect = (float) w / h;
+    camera->setProjection(Camera::Projection::ORTHO,
+                          -aspect * ZOOM, aspect * ZOOM,
+                          -ZOOM, ZOOM, 0, 1);
+    auto& tcm = engine->getTransformManager();
+    tcm.setTransform(tcm.getInstance(app.renderable),
+                     math::mat4f::rotate(CACurrentMediaTime(), math::float3{0, 0, 1}));
+}
+
++ (Class) layerClass
+{
+    return [CAMetalLayer class];
+}
+
+@end
