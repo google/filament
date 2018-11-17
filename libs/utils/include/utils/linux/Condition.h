9e39004b1fe9dc0c724a7c24aae2b7b8b76385a0
Mathias Agopian <mathias@google.com>
2018-11-16 17:15:26 -0800

    Add wait_for and wait_until to our Condition implementation
    We're using timed condition variable in one place, but the STL version
    pulls in a lot of code because it does clock calculations in 
    "long double" (!!!!). Since we already had an implementation
    of condition_variable, we just add the timed version.
    This saves several KiB of code.
    
    Also don't use unique_lock() lock/unlock because it can throw exceptions.

--- libs/utils/include/utils/linux/Condition.h
+++ libs/utils/include/utils/linux/Condition.h
@@ -18,9 +18,13 @@
 #define UTILS_LINUX_CONDITION_H
 
 #include <atomic>
+#include <chrono>
 #include <limits>
 #include <mutex> // for unique_lock
+
 #include <utils/linux/Mutex.h>
+
+#include <time.h>
 
 namespace utils {
 
@@ -44,7 +48,9 @@ public:
         pulse(1);
     }
 
-    void wait(std::unique_lock<Mutex>& lock) noexcept;
+    void wait(std::unique_lock<Mutex>& lock) noexcept {
+        wait_until(lock.mutex(), false, nullptr);
+    }
 
     template <class P>
     void wait(std::unique_lock<Mutex>& lock, P predicate) {
@@ -53,10 +59,58 @@ public:
         }
     }
 
+    template<typename D>
+    std::cv_status wait_until(std::unique_lock<Mutex>& lock,
+            const std::chrono::time_point<std::chrono::steady_clock, D>& timeout_time) noexcept {
+        // convert to nanoseconds
+        int64_t ns = std::chrono::duration<int64_t, std::nano>(timeout_time.time_since_epoch()).count();
+        using sec_t = decltype(timespec::tv_sec);
+        using nsec_t = decltype(timespec::tv_nsec);
+        timespec ts{ sec_t(ns / 1000000000), nsec_t(ns % 1000000000) };
+        return wait_until(lock.mutex(), false, &ts);
+    }
+
+    template<typename D>
+    std::cv_status wait_until(std::unique_lock<Mutex>& lock,
+            const std::chrono::time_point<std::chrono::system_clock, D>& timeout_time) noexcept {
+        // convert to nanoseconds
+        int64_t ns = std::chrono::duration<int64_t, std::nano>(timeout_time.time_since_epoch()).count();
+        using sec_t = decltype(timespec::tv_sec);
+        using nsec_t = decltype(timespec::tv_nsec);
+        timespec ts{ sec_t(ns / 1000000000), nsec_t(ns % 1000000000) };
+        return wait_until(lock.mutex(), true, &ts);
+    }
+
+    template<typename C, typename D, typename P>
+    bool wait_until(std::unique_lock<Mutex>& lock,
+            const std::chrono::time_point<C, D>& timeout_time, P predicate) noexcept {
+        while (!predicate()) {
+            if (wait_until(lock, timeout_time) == std::cv_status::timeout) {
+                return predicate();
+            }
+        }
+        return true;
+    }
+
+    template<typename R, typename Period>
+    std::cv_status wait_for(std::unique_lock<Mutex>& lock,
+            const std::chrono::duration<R, Period>& rel_time) noexcept {
+        return wait_until(lock, std::chrono::steady_clock::now() + rel_time);
+    }
+
+    template<typename R, typename Period, typename P>
+    bool wait_for(std::unique_lock<Mutex>& lock,
+            const std::chrono::duration<R, Period>& rel_time, P pred) noexcept {
+        return wait_until(lock, std::chrono::steady_clock::now() + rel_time, std::move(pred));
+    }
+
 private:
     std::atomic<uint32_t> mState = { 0 };
 
     void pulse(int threadCount) noexcept;
+
+    std::cv_status wait_until(Mutex* lock,
+            bool realtimeClock, timespec* ts) noexcept;
 };
 
 } // namespace utils
