adbe3a75f2f750c07b3f0dda03b7481781307c11
Mathias Agopian <mathias@google.com>
2018-11-19 17:56:49 -0800

    don't use a deque for the frame history
    deque is not worth it with few items.

--- filament/src/View.cpp
+++ filament/src/View.cpp
@@ -108,8 +108,8 @@ void FView::setDynamicResolutionOptions(DynamicResolutionOptions const& options)
     if (dynamicResolution.enabled) {
         // if enabled, sanitize the parameters
 
-        // History can't be more than 30 frames (~0.5s)
-        dynamicResolution.history = std::min(dynamicResolution.history, uint8_t(30));
+        // History can't be more than 32 frames (~0.5s)
+        dynamicResolution.history = std::min(dynamicResolution.history, uint8_t(MAX_FRAMETIME_HISTORY));
 
         // History must at least be 3 frames
         dynamicResolution.history = std::max(dynamicResolution.history, uint8_t(3));
@@ -137,7 +137,7 @@ void FView::setDynamicResolutionOptions(DynamicResolutionOptions const& options)
         dynamicResolution.maxScale = min(dynamicResolution.maxScale, float2(2.0f));
 
         // reset the history, so we start from a known (and current) state
-        mFrameTimeHistory.clear();
+        mFrameTimeHistorySize = 0;
         mScale = 1.0f;
         mDynamicWorkloadScale = 1.0f;
     }
@@ -147,6 +147,14 @@ void FView::setDynamicLightingOptions(float zLightNear, float zLightFar) noexcep
     mFroxelizer.setOptions(zLightNear, zLightFar);
 }
 
+// this is to avoid a call to memmove
+template<class InputIterator, class OutputIterator>
+static inline
+void move_backward(InputIterator first, InputIterator last, OutputIterator result) {
+    while (first != last) {
+        *--result = *--last;
+    }
+}
 
 math::float2 FView::updateScale(duration frameTime) noexcept {
     DynamicResolutionOptions const& options = mDynamicResolution;
@@ -159,10 +167,13 @@ math::float2 FView::updateScale(duration frameTime) noexcept {
 
         // keep an history of frame times
         auto& history = mFrameTimeHistory;
-        history.push_front(frameTime);
-        if (history.size() > options.history) {
-            history.pop_back();
-        } else if (UTILS_UNLIKELY(history.size() < 3)) {
+
+        // this is like doing { pop_back(); push_front(); }
+        details::move_backward(history.begin(), history.end() - 1, history.end());
+        history.front() = frameTime;
+        mFrameTimeHistorySize = std::min(++mFrameTimeHistorySize, size_t(MAX_FRAMETIME_HISTORY));
+
+        if (UTILS_UNLIKELY(mFrameTimeHistorySize < 3)) {
             // don't make any decision if we don't have enough data
             mScale = 1.0f;
             return mScale;
@@ -170,8 +181,8 @@ math::float2 FView::updateScale(duration frameTime) noexcept {
 
         // apply a median filter to get a good representation of the frame time of the last
         // N frames.
-        std::array<duration, 30> median; // NOLINT -- it's initialized below
-        size_t size = std::min(history.size(), median.size());
+        std::array<duration, MAX_FRAMETIME_HISTORY> median; // NOLINT -- it's initialized below
+        size_t size = std::min(mFrameTimeHistorySize, median.size());
         std::uninitialized_copy_n(history.begin(), size, median.begin());
         std::sort(median.begin(), median.begin() + size);
         duration filteredFrameTime = median[size / 2];
