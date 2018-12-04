f05b2f47a88536cb97a42326a60cd2798824a77c
Ben Doherty <benjdoherty15@gmail.com>
2018-12-04 13:23:01 -0800

    Fix use of UV0 and UV1 coordinates (#576)
    * Fix use of UV1
    
    * Add a test case
    
    * Spelling

--- shaders/src/variables.fs
+++ shaders/src/variables.fs
@@ -21,7 +21,7 @@ LAYOUT_LOCATION(8) SHADING_INTERPOLATION centroid in HIGHP vec3 vertex_worldNorm
 LAYOUT_LOCATION(9) in MEDIUMP vec4 vertex_color;
 #endif
 
-#if defined(HAS_ATTRIBUTE_UV0)
+#if defined(HAS_ATTRIBUTE_UV0) && !defined(HAS_ATTRIBUTE_UV1)
 LAYOUT_LOCATION(10) in HIGHP vec2 vertex_uv01;
 #elif defined(HAS_ATTRIBUTE_UV1)
 LAYOUT_LOCATION(10) in HIGHP vec4 vertex_uv01;
