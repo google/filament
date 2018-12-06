14b7d0eb15196923ec8f6ff48662fb066a2c8497
Philip Rideout <philiprideout@gmail.com>
2018-12-06 10:46:34 -0800

    Update zeux/meshoptimizer

--- third_party/meshoptimizer/src/vertexcodec.cpp
+++ third_party/meshoptimizer/src/vertexcodec.cpp
@@ -12,7 +12,7 @@
 #define SIMD_SSE
 #endif
 
-#if !defined(SIMD_SSE) && defined(WIN32) && (defined(_M_IX86) || defined(_M_X64))
+#if !defined(SIMD_SSE) && defined(_MSC_VER) && (defined(_M_IX86) || defined(_M_X64))
 #define SIMD_SSE
 #define SIMD_FALLBACK
 #include <intrin.h>
