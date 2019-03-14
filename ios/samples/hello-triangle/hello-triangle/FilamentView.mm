ee28f0adacddf08719149329e2ab5978f527e51a
Mathias Agopian <mathias@google.com>
2019-03-14 13:12:46 -0700

    Fix #978 -- iOS samples don't build

--- ios/samples/hello-triangle/hello-triangle/FilamentView.mm
+++ ios/samples/hello-triangle/hello-triangle/FilamentView.mm
@@ -211,8 +211,8 @@ static constexpr uint8_t BAKED_COLOR_PACKAGE[] = {
     [self updateRotation];
 
     tcm.setTransform(tcm.getInstance(app.renderable),
-                     filament::math::mat4f::rotate(CACurrentMediaTime(), filament::math::float3{0, 0, 1}) *
-                     filament::math::mat4f::rotate(deviceRotation, filament::math::float3{0, 0, 1}));
+                     filament::math::mat4f::rotation(CACurrentMediaTime(), filament::math::float3{0, 0, 1}) *
+                     filament::math::mat4f::rotation(deviceRotation, filament::math::float3{0, 0, 1}));
 }
 
 - (void)didRotate:(NSNotification*)notification
