6663f6584b2ff4bcd1b757e6bf03a3eeb9da5117
Philip Rideout <philiprideout@gmail.com>
2019-04-10 14:07:58 -0700

    Simplify matc ParametersProcessor. (#1091)
    (1) Merge two same-sized lists into a single map with struct values.
    
    (2) Constify all the string-to-enum maps and move them into the
    scope where they are used.
    
    (3) Move a bunch of object methods into static free functions to
    simplify the header and reduce code duplication.
    
    (4) The PARAM_KEY_FOO string constants were referenced only once (except
    in a few error messages) so replace them with string literals.

--- tools/matc/src/matc/ParametersProcessor.h
+++ tools/matc/src/matc/ParametersProcessor.h
@@ -19,7 +19,6 @@
 
 #include <unordered_map>
 #include <string>
-#include <iostream>
 
 #include "JsonishLexeme.h"
 #include "JsonishParser.h"
@@ -36,74 +35,15 @@ public:
     bool process(filamat::MaterialBuilder& builder, const JsonishObject& jsonObject);
 
 private:
-    bool processName(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processInterpolation(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processParameters(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processVariables(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processRequires(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processBlending(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processPostLightingBlending(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processVertexDomain(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processCulling(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processColorWrite(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processDepthWrite(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processDepthCull(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processDoubleSided(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processTransparencyMode(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processMaskThreshold(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processShadowMultiplier(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processCurvatureToRoughness(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processLimitOverInterpolation(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processClearCoatIorChange(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processFlipUV(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processShading(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processVariantFilter(filamat::MaterialBuilder &builder, const JsonishValue &value);
-    bool processParameter(filamat::MaterialBuilder& builder, const JsonishObject& value) const
-    noexcept;
 
-    template <class T>
-    bool logEnumIssue(const std::string& key, const JsonishString& value,
-            const std::unordered_map<std::string, T>& map) const noexcept {
-        std::cerr << "Error while processing key '" << key << "' value." << std::endl;
-        std::cerr << "Value '" << value.getString() << "' is invalid. Valid values are:"
-                << std::endl;
-        for (auto entries : map) {
-            std::cerr << "    " << entries.first  << std::endl;
-        }
-        return false;
-    }
+    using Callback = bool (*)(filamat::MaterialBuilder& builder, const JsonishValue& value);
 
-    template <class T>
-    bool isStringValidEnum(const std::unordered_map<std::string, T>& map,
-            const std::string& s) const noexcept {
-        return map.find(s) != map.end();
-    }
+    struct ParameterInfo {
+        Callback callback;
+        JsonishValue::Type rootAssert;
+    };
 
-    template <class T>
-    T stringToEnum(const std::unordered_map<std::string, T>& map,
-            const std::string& s) const noexcept {
-        return map.at(s);
-    }
-
-    filamat::MaterialBuilder::Variable intToVariable(size_t i) const noexcept;
-
-    using MaterialConfigProcessor =
-            bool (ParametersProcessor::*)(filamat::MaterialBuilder& builder, const JsonishValue& value);
-    std::unordered_map<std::string, MaterialConfigProcessor> mConfigProcessor;
-
-    std::unordered_map<std::string, JsonishValue::Type> mRootAsserts;
-    std::unordered_map<std::string, filamat::MaterialBuilder::Interpolation>
-            mStringToInterpolation;
-    std::unordered_map<std::string, filamat::MaterialBuilder::CullingMode> mStringToCullingMode;
-    std::unordered_map<std::string, filamat::MaterialBuilder::TransparencyMode>
-            mStringToTransparencyMode;
-    std::unordered_map<std::string, filamat::MaterialBuilder::VertexDomain>
-            mStringToVertexDomain;
-    std::unordered_map<std::string, filamat::MaterialBuilder::BlendingMode>
-            mStringToBlendingMode;
-    std::unordered_map<std::string, filament::VertexAttribute> mStringToAttributeIndex;
-    std::unordered_map<std::string, filamat::MaterialBuilder::Shading> mStringToShading;
-    std::unordered_map<std::string, uint8_t> mStringToVariant;
+    std::unordered_map<std::string, ParameterInfo> mParameters;
 };
 
 } // namespace matc
