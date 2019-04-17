4671b7c090dba1d2ae1e2ad65814a522edf0c682
Philip Rideout <philiprideout@gmail.com>
2019-04-17 12:49:47 -0700

    Some matinfo fixes. (#1108)
    - Fix the "CString size mismatch" assert seen in debug config.
    - Fix the --print-glsl option, which was printing junk.

--- tools/matinfo/src/main.cpp
+++ tools/matinfo/src/main.cpp
@@ -604,7 +604,7 @@ inline utils::CString typeToString(uint64_t v) {
         str[7 - i] = raw[i];
     }
     str[8] = '\0';
-    return utils::CString(str, 7);
+    return utils::CString(str, strnlen(str, 8));
 }
 
 static void printChunks(const ChunkContainer& container) {
@@ -904,7 +904,9 @@ static bool parseChunks(Config config, void* data, size_t size) {
 
             const auto& item = info[config.shaderIndex];
             parser.getShader(item.shaderModel, item.variant, item.pipelineStage, builder);
-            std::cout << builder.data();
+
+            // Casted to char* to print as a string rather than hex value.
+            std::cout << (const char*) builder.data();
 
             return true;
         }
