cd343b0457ead489ee7c8b50521dbde488cd0646
Philip Rideout <philiprideout@gmail.com>
2019-06-24 09:32:45 -0700

    Fix GL_INVALID_OPERATION in the default IBL.
    In commit d955e73 we changed the default IBL from RGBM to RGB, but we
    kept its internal format of RGBA8. Since the internal format is not
    compatible with the data format, we see GL_INVALID_OPERATION during the
    texture upload.
    
    This is a single-pixel black texture, so the format does not really
    matter, but this clears up a GL error seen on some platforms.

--- samples/suzanne.cpp
+++ samples/suzanne.cpp
@@ -88,10 +88,10 @@ int main(int argc, char** argv) {
         auto ao = new image::KtxBundle(TEXTURES_AO_DATA, TEXTURES_AO_SIZE);
         auto metallic = new image::KtxBundle(TEXTURES_METALLIC_DATA, TEXTURES_METALLIC_SIZE);
         auto roughness = new image::KtxBundle(TEXTURES_ROUGHNESS_DATA, TEXTURES_ROUGHNESS_SIZE);
-        app.albedo = KtxUtility::createTexture(engine, albedo, true, false);
-        app.ao = KtxUtility::createTexture(engine, ao, false, false);
-        app.metallic = KtxUtility::createTexture(engine, metallic, false, false);
-        app.roughness = KtxUtility::createTexture(engine, roughness, false, false);
+        app.albedo = KtxUtility::createTexture(engine, albedo, true);
+        app.ao = KtxUtility::createTexture(engine, ao, false);
+        app.metallic = KtxUtility::createTexture(engine, metallic, false);
+        app.roughness = KtxUtility::createTexture(engine, roughness, false);
         app.normal = loadNormalMap(engine, TEXTURES_NORMAL_DATA, TEXTURES_NORMAL_SIZE);
         TextureSampler sampler(TextureSampler::MinFilter::LINEAR_MIPMAP_LINEAR,
                 TextureSampler::MagFilter::LINEAR);
