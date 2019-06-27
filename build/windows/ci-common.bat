494ddc10e4da7e9f748367904f702d9f34a21dcb
Romain Guy <romainguy@curious-creature.com>
2019-06-26 17:36:13 -0700

    More logging

--- build/windows/ci-common.bat
+++ build/windows/ci-common.bat
@@ -1,25 +1,13 @@
 if defined KOKORO_BUILD_ID (
+    echo Installing CMake
     choco install cmake -y
-
-    :: retry if choco install failed
-    if errorlevel 1 (
-        choco install cmake -y
-        if errorlevel 1 exit /b %errorlevel%
-    )
-
     refreshenv
 
-    choco install llvm --version 8.0.0 -y
-
-    :: retry if choco install failed
-    if errorlevel 1 (
-        choco install llvm --version 8.0.8 -y
-        if errorlevel 1 exit /b %errorlevel%
-    )
-
-    :: refreshenv is necessary to put CMake and LLVM on path
+    echo Installing LLVM
+    choco install llvm --version 8.0.8 -y
     refreshenv
+
+    echo Exiting ci-common
 )
 
-echo Exiting ci-common
 exit /b 0
