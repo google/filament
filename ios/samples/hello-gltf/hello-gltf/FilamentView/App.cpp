20cc1e551d9816382d18320513d3da023c2f2896
Philip Rideout <philiprideout@gmail.com>
2019-08-06 15:32:51 -0700

    Replace prebuilt KTX with a build step.
    This removes the samples/envs folder and replaces these KTX files with a
    build step that is driven by CMake / gradle / bash, depending on
    platform.
    
    This makes it easier to use IBL files that are generated by the latest
    and greatest version of cmake.

--- ios/samples/hello-gltf/hello-gltf/FilamentView/App.cpp
+++ ios/samples/hello-gltf/hello-gltf/FilamentView/App.cpp
@@ -84,14 +84,14 @@ void App::setupFilament() {
 }
 
 void App::setupIbl() {
-    image::KtxBundle* iblBundle = new image::KtxBundle(RESOURCES_VENETIAN_CROSSROADS_IBL_DATA,
-                                                       RESOURCES_VENETIAN_CROSSROADS_IBL_SIZE);
+    image::KtxBundle* iblBundle = new image::KtxBundle(RESOURCES_VENETIAN_CROSSROADS_2K_IBL_DATA,
+                                                       RESOURCES_VENETIAN_CROSSROADS_2K_IBL_SIZE);
     float3 harmonics[9];
     iblBundle->getSphericalHarmonics(harmonics);
     app.iblTexture = image::KtxUtility::createTexture(engine, iblBundle, false);
 
-    image::KtxBundle* skyboxBundle = new image::KtxBundle(RESOURCES_VENETIAN_CROSSROADS_SKYBOX_DATA,
-                                                          RESOURCES_VENETIAN_CROSSROADS_SKYBOX_SIZE);
+    image::KtxBundle* skyboxBundle = new image::KtxBundle(RESOURCES_VENETIAN_CROSSROADS_2K_SKYBOX_DATA,
+                                                          RESOURCES_VENETIAN_CROSSROADS_2K_SKYBOX_SIZE);
     app.skyboxTexture = image::KtxUtility::createTexture(engine, skyboxBundle, false);
 
     app.skybox = Skybox::Builder()
