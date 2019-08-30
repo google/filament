eceb4ab2c5a0d7ff56f85d7d949d11858bc88a69
Romain Guy <romainguy@curious-creature.com>
2019-08-30 14:59:54 -0700

    Setup Linux for GitHub CI

--- build/linux/ci-common.sh
+++ build/linux/ci-common.sh
@@ -10,11 +10,29 @@ CMAKE_VERSION=3.13.4
 # version of ninja to use
 NINJA_VERSION=1.8.2
 
+# Install ninja
+wget -q https://github.com/ninja-build/ninja/releases/download/v$NINJA_VERSION/ninja-linux.zip
+unzip -q ninja-linux.zip
+export PATH="$PWD:$PATH"
+
+# Install CMake
+mkdir -p cmake
+cd cmake
+
+sudo wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-Linux-x86_64.sh
+sudo chmod +x ./cmake-$CMAKE_VERSION-Linux-x86_64.sh
+sudo ./cmake-$CMAKE_VERSION-Linux-x86_64.sh --skip-license > /dev/null
+sudo update-alternatives --install /usr/bin/cmake cmake `pwd`/bin/cmake 1000 --force
+
+cd ..
+
 # Steps for GitHub Workflows
 if [[ "$GITHUB_WORKFLOW" ]]; then
     sudo wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get install clang-$GITHUB_CLANG_VERSION libc++-$GITHUB_CLANG_VERSION-dev libc++abi-$GITHUB_CLANG_VERSION-dev
-    sudo apt-get install cmake
+
+    sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang-${GITHUB_CLANG_VERSION}.0 100
+    sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-${GITHUB_CLANG_VERSION}.0 100
 fi
 
 # Steps specific to our CI environment
@@ -27,17 +45,6 @@ if [[ "$KOKORO_BUILD_ID" ]]; then
         # Update NDK
         yes | $ANDROID_HOME/tools/bin/sdkmanager "ndk-bundle" > /dev/null
     fi
-
-    # Install CMake
-    mkdir -p cmake
-    cd cmake
-
-    sudo wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-Linux-x86_64.sh
-    sudo chmod +x ./cmake-$CMAKE_VERSION-Linux-x86_64.sh
-    sudo ./cmake-$CMAKE_VERSION-Linux-x86_64.sh --skip-license > /dev/null
-    sudo update-alternatives --install /usr/bin/cmake cmake `pwd`/bin/cmake 1000 --force
-
-    cd ..
 
     # Install clang
     # This may or may not be needed...
@@ -105,7 +112,3 @@ if [[ "$KOKORO_BUILD_ID" ]]; then
     export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
     export LIBRARY_PATH=/usr/local/lib:$LIBRARY_PATH
 fi
-
-wget -q https://github.com/ninja-build/ninja/releases/download/v$NINJA_VERSION/ninja-linux.zip
-unzip -q ninja-linux.zip
-export PATH="$PWD:$PATH"
