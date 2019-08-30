040c1c686a72fc07ada220ee2a781ecb87a1d97e
Romain Guy <romainguy@curious-creature.com>
2019-08-30 12:02:21 -0700

    Update build script to support GitHub Actions

--- build/common/ci-common.sh
+++ build/common/ci-common.sh
@@ -4,3 +4,8 @@ if [[ "$KOKORO_BUILD_ID" ]]; then
     echo "Running job $KOKORO_JOB_NAME"
     TARGET=`echo "$KOKORO_JOB_NAME" | awk -F "/" '{print $NF}'`
 fi
+
+if [[ "GITHUB_WORKFLOW" ]]; then
+    echo "Running job GITHUB_ACTION in workflow $GITHUB_WORKFLOW"
+    TARGET=`echo "$GITHUB_ACTION" | awk -F "-" '{print $NF}'`
+fi
