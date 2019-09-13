c916069aa26aca7e0645f677caf635d3beba54ab
Romain Guy <romainguy@curious-creature.com>
2019-09-13 16:57:43 -0700

    Rename desktop workflow for consistency

--- build/android/build.sh
+++ build/android/build.sh
@@ -28,8 +28,10 @@ elif [[ "$LC_UNAME" == "darwin" ]]; then
 fi
 source `dirname $0`/../common/build-common.sh
 
-yes | ${ANDROID_HOME}/tools/bin/sdkmanager --update >/dev/null && \
-    yes | ${ANDROID_HOME}/tools/bin/sdkmanager --licenses >/dev/null
+if [[ "$KOKORO_BUILD_ID" ]]; then
+    yes | ${ANDROID_HOME}/tools/bin/sdkmanager --update >/dev/null && \
+        yes | ${ANDROID_HOME}/tools/bin/sdkmanager --licenses >/dev/null
+fi
 
 pushd `dirname $0`/../.. > /dev/null
 ./build.sh -p android -c $GENERATE_ARCHIVES $BUILD_DEBUG $BUILD_RELEASE
