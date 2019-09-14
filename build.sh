36dda03f62fe41d18aac787dd25bb74ca02ce119
Romain Guy <romainguy@curious-creature.com>
2019-09-13 17:53:17 -0700

    Only update the NDK when necessary

--- build.sh
+++ build.sh
@@ -315,7 +315,7 @@ function ensure_android_build {
 
     local ndk_type=0 # 0 = No SDK, 1 = ndk-bundle, 2 = ndk side-by-side
 
-    local ndk_properties="$ANDROID_HOME/ndk-bundle/source.properties"
+    local ndk_properties="${ANDROID_HOME}/ndk-bundle/source.properties"
     if [[ -f $ndk_properties ]]; then
         local ndk_version=`sed -En -e "s/^Pkg.Revision *= *([0-9a-f]+).+/\1/p" ${ndk_properties}`
         if [[ ${ndk_version} -ge ${ANDROID_NDK_VERSION} ]]; then
@@ -324,7 +324,7 @@ function ensure_android_build {
     fi
 
     if [[ ${ndk_type} == 0 ]]; then
-        local ndk_side_by_side="$ANDROID_HOME/ndk/"
+        local ndk_side_by_side="${ANDROID_HOME}/ndk/"
         if [[ -d $ndk_side_by_side ]]; then
             local ndk_version=`ls ${ndk_side_by_side} | sort -V | tail -n 1 | cut -f 1 -d "."`
             if [[ ${ndk_version} -ge ${ANDROID_NDK_VERSION} ]]; then
