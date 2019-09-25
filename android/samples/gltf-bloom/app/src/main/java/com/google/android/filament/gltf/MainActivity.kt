6f606d25874661bcb4ae93d2b855fc63a4a969f2
Romain Guy <romainguy@curious-creature.com>
2019-09-25 13:25:50 +0300

    Fix Android samples
    Fixes #1687

--- android/samples/gltf-bloom/app/src/main/java/com/google/android/filament/gltf/MainActivity.kt
+++ android/samples/gltf-bloom/app/src/main/java/com/google/android/filament/gltf/MainActivity.kt
@@ -431,7 +431,7 @@ class MainActivity : Activity() {
 
         // This ensures that all the commands we've sent to Filament have
         // been processed before we attempt to destroy anything
-        Fence.waitAndDestroy(engine.createFence(Fence.Type.SOFT), Fence.Mode.FLUSH)
+        engine.flushAndWait()
 
         assetLoader.destroyAsset(filamentAsset)
         assetLoader.destroy()
