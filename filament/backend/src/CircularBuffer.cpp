3b2ef049f3346641ff4b20c994bd00efb1e99d2e
Mathias Agopian <mathias@google.com>
2019-10-04 13:17:33 -0700

    improve CircularBuffer error message

--- filament/backend/src/CircularBuffer.cpp
+++ filament/backend/src/CircularBuffer.cpp
@@ -105,7 +105,7 @@ void* CircularBuffer::alloc(size_t size) noexcept {
         data = mmap(nullptr, size * 2 + BLOCK_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
         ASSERT_POSTCONDITION(data,
-                "couldn't allocate %u KiB of memory for the command buffer",
+                "couldn't allocate %u KiB of virtual address space for the command buffer",
                 (size * 2 / 1024));
 
         slog.d << "WARNING: Using soft CircularBuffer (" << (size * 2 / 1024) << " KiB)"
