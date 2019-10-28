446461d2aada01825ad1793f93599f85711a3ff4
Romain Guy <romainguy@curious-creature.com>
2019-10-28 15:15:19 +0000

    Use unnormalized vectors to compute bump mapping.
    If we assume the tangents and bitangents were generated using mikktspace,
    we should use the unnormalized vectors, as indicated at mikktspace.com
    or in http://www.metalliandy.com/mikktspace/downloads/mikktspace/mikktspace.h
    (see last comment at the end of the file).

--- shaders/src/common_shading.fs
+++ shaders/src/common_shading.fs
@@ -3,7 +3,8 @@
 highp mat3  shading_tangentToWorld;   // TBN matrix
 highp vec3  shading_position;         // position of the fragment in world space
       vec3  shading_view;             // normalized vector from the fragment to the eye
-      vec3  shading_normal;           // normalized normal, in world space
+      vec3  shading_normal;           // normalized transformed normal, in world space
+      vec3  shading_geometricNormal;  // normalized geometric normal, in world space
       vec3  shading_reflected;        // reflection of view about normal
       float shading_NoV;              // dot(normal, view), always strictly >= MIN_N_DOT_V
 
