339fa3bd60b0e17a6d5b33e6ad9585aa78ec3b4b
Mathias Agopian <mathias@google.com>
2019-11-14 15:29:59 -0800

    SAO and ssaogen cleanup
    ssaogen now produces better formatted tables, directly usable in
    shader code.
    
    SAO shader now squares the sample radius outside of `tapLocation`, also
    added some comments.

--- filament/tools/ssaogen.cpp
+++ filament/tools/ssaogen.cpp
@@ -34,6 +34,10 @@ int main(int argc, char** argv) {
     std::cout << "const uint kSphereSampleCount = " << sphereSampleCount << "u;" << std::endl;
     std::cout << "const vec3 kSphereSamples[] = vec3[](" << std::endl;
     for (size_t i = 0; i < sphereSampleCount; i++) {
+        const bool first = i == 0;
+        const bool last = i == sphereSampleCount - 1;
+        const bool newline = (i & 0x1u) == 0x1;
+
         float s = float(i) / sphereSampleCount;
         float r = random(generator);
         float3 d = {
@@ -43,36 +47,54 @@ int main(int argc, char** argv) {
         };
         d = normalize(d);
         d = d * r * lerp(0.1f, 1.0f, s * s);
-        if (!(i & 1u)) {
-            std::cout << "    ";
+
+        if (first) {
+            printf("    ");
         }
-        std::cout << " vec3(" << d.x << ", " << d.y << ", " << d.z << "),";
-        if (i & 1u) {
-            std::cout << std::endl;
+        printf("vec3(%9f, %9f, %9f)", d.x, d.y, d.z);
+        if (!last) {
+            printf(",");
+            if (newline) {
+                printf("\n    ");
+            } else {
+                printf(" ");
+            }
+        } else {
+            printf("\n);\n");
         }
     }
-    std::cout << ");" << std::endl;
 
 
     size_t noiseSampleCount = 16;
     std::cout << "const uint kNoiseSampleCount = " << sphereSampleCount << "u;" << std::endl;
     std::cout << "const vec3 kNoiseSamples[] = vec3[](" << std::endl;
     for (size_t i = 0; i < noiseSampleCount; i++) {
+        const bool first = i == 0;
+        const bool last = i == noiseSampleCount - 1;
+        const bool newline = (i & 0x1u) == 0x1;
+
         float3 d = {
                 random(generator) * 2 - 1,
                 random(generator) * 2 - 1,
                 random(generator) * 2 - 1,
         };
         d = normalize(d);
-        if ((i & 0x1u) == 0) {
-            std::cout << "    ";
+
+        if (first) {
+            printf("    ");
         }
-        std::cout << " vec3(" << d.x << ", " << d.y << ", " << d.z << "),";
-        if ((i & 0x1u) == 0x1) {
-            std::cout << std::endl;
+        printf("vec3(%9f, %9f, %9f)", d.x, d.y, d.z);
+        if (!last) {
+            printf(",");
+            if (newline) {
+                printf("\n    ");
+            } else {
+                printf(" ");
+            }
+        } else {
+            printf("\n);\n");
         }
     }
-    std::cout << ");" << std::endl;
 
 
     /*
@@ -120,38 +142,59 @@ int main(int argc, char** argv) {
     std::cout << "const vec3 kSpiralSamples[] = vec3[](" << std::endl;
     const float dalpha = 1.0f / (spiralSampleCount - 0.5f);
     for (size_t i = 0; i < spiralSampleCount; i++) {
+        const bool first = i == 0;
+        const bool last = i == spiralSampleCount - 1;
+        const bool newline = (i & 0x1u) == 0x1;
+
         float radius = (i + 0.5f) * dalpha;
         float angle = float(radius * radius * (2 * F_PI * kSpiralTurns));
-        if ((i & 0x1u) == 0) {
-            std::cout << "    ";
+
+        if (first) {
+            printf("    ");
         }
-        std::cout << " vec3(" << std::cos(angle) << ", " << std::sin(angle) << ", " << radius << "),";
-        if ((i & 0x1u) == 0x1) {
-            std::cout << std::endl;
+        printf("vec3(%9f, %9f, %9f)", std::cos(angle), std::sin(angle), radius);
+        if (!last) {
+            printf(",");
+            if (newline) {
+                printf("\n    ");
+            } else {
+                printf(" ");
+            }
+        } else {
+            printf("\n);\n");
         }
     }
-    std::cout << ");" << std::endl;
-
 
     size_t trigNoiseSampleCount = 16;
     std::cout << "const uint kTrigNoiseSampleCount = " << sphereSampleCount << "u;" << std::endl;
     std::cout << "const vec3 kTrigNoiseSamples[] = vec3[](" << std::endl;
     for (size_t i = 0; i < trigNoiseSampleCount; i++) {
+        const bool first = i == 0;
+        const bool last = i == trigNoiseSampleCount - 1;
+        const bool newline = (i & 0x1u) == 0x1;
+
         float phi = random(generator);
         float dr = phi * dalpha;
         float dphi = float(2.0 * F_PI * kSpiralTurns * phi * phi * dalpha * dalpha
                    + phi * 2.0 * F_PI * (1.0 + kSpiralTurns * dalpha * dalpha)
                    + phi * 4.0 * F_PI * kSpiralTurns * dalpha * dalpha);
         float3 d = { std::cos(dphi), std::sin(dphi), dr };
-        if ((i & 0x1u) == 0) {
-            std::cout << "    ";
+
+        if (first) {
+            printf("    ");
         }
-        std::cout << " vec3(" << d.x << ", " << d.y << ", " << d.z << "),";
-        if ((i & 0x1u) == 0x1) {
-            std::cout << std::endl;
+        printf("vec3(%9f, %9f, %9f)", d.x, d.y, d.z);
+        if (!last) {
+            printf(",");
+            if (newline) {
+                printf("\n    ");
+            } else {
+                printf(" ");
+            }
+        } else {
+            printf("\n);\n");
         }
     }
-    std::cout << ");" << std::endl;
 
 
     float weightSum = 0;
@@ -161,6 +204,10 @@ int main(int argc, char** argv) {
     std::cout << "const int kRadius = kGaussianCount - 1;" << std::endl;
     std::cout << "const float kGaussianSamples[] = float[](" << std::endl;
     for (size_t i = 0; i < gaussianSampleCount; i++) {
+        const bool first = i == 0;
+        const bool last = i == gaussianSampleCount - 1;
+        const bool newline = (i & 0x7u) == 0x7;
+
         float x = i;
 
         // q: standard deviation
@@ -171,18 +218,24 @@ int main(int argc, char** argv) {
         //      fc = 1.1774 / (2pi * q)       (half power frequency or 0.707 amplitude)
 
         float q = (gaussianWidth + 1.0f) / 6.0f;  // ~1.667 for 9 taps
-        float g = (1.0 / (std::sqrt(2.0 * F_PI) * q)) * std::exp(-(x * x) / (2.0 * q * q));
+        float g = float((1.0 / (std::sqrt(2.0 * F_PI) * q)) * std::exp(-(x * x) / (2.0 * q * q)));
         weightSum += g * (i == 0 ? 1.0f : 2.0f);
 
-        if ((i & 0x7u) == 0) {
-            std::cout << "    ";
+        if (first) {
+            printf("    ");
         }
-        std::cout << g << ", ";
-        if ((i & 0x7u) == 0x7) {
-            std::cout << std::endl;
+        printf("%8f", g);
+        if (!last) {
+            printf(",");
+            if (newline) {
+                printf("\n    ");
+            } else {
+                printf(" ");
+            }
+        } else {
+            printf("\n);\n");
         }
     }
-    std::cout << ");" << std::endl;
     std::cout << "const float kGaussianWeightSum = " << weightSum << ";" << std::endl;
 
     return 0;
