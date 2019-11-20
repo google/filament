0d1b8c021245536fe7deb41a310f0b0a82da52ae
Benjamin Doherty <bendoherty@google.com>
2019-11-20 15:56:55 -0800

    Add release GitHub workflow

--- build/windows/build-github.bat
+++ build/windows/build-github.bat
@@ -45,48 +45,60 @@ if errorlevel 1 exit /b %errorlevel%
 msbuild /version
 cmake --version
 
-mkdir out\cmake
-cd out\cmake
-if errorlevel 1 exit /b %errorlevel%
-
-cmake ..\.. -G "Visual Studio 16 2019" -A x64 || exit /b
-
 if "%BUILD_RELEASE%" == "1" (
     :: /MT
-    cmake . -DUSE_STATIC_CRT=ON -DCMAKE_INSTALL_PREFIX=..\mt
-    cmake --build . %INSTALL% --config Release -- /m || exit /b
+    call :BuildVariant mt "-DUSE_STATIC_CRT=ON" Release || exit /b
 
     if "%BUILD_RELEASE_VARIANTS%" == "1" (
         :: /MD
-        cmake . -DUSE_STATIC_CRT=OFF -DCMAKE_INSTALL_PREFIX=..\md
-        cmake --build . %INSTALL% --config Release -- /m || exit /b
+        call :BuildVariant md "-DUSE_STATIC_CRT=OFF" Release || exit /b
     )
 )
 
 if "%BUILD_DEBUG%" == "1" (
     :: MTd
-    cmake . -DUSE_STATIC_CRT=ON -DCMAKE_INSTALL_PREFIX=..\mtd
-    cmake --build . %INSTALL% --config Debug -- /m || exit /b
+    call :BuildVariant mtd "-DUSE_STATIC_CRT=ON" Debug || exit /b
 
     if "%BUILD_RELEASE_VARIANTS%" == "1" (
         :: MDd
-        cmake . -DUSE_STATIC_CRT=OFF -DCMAKE_INSTALL_PREFIX=..\mdd
-        cmake --build . %INSTALL% --config Debug -- /m || exit /b
+        call :BuildVariant mdd "-DUSE_STATIC_CRT=OFF" Debug || exit /b
     )
 )
 
 if "%BUILD_RELEASE_VARIANTS%" == "1" (
     :: Use the /MT version as the "base" install.
-    move ..\mt ..\install
-    mkdir ..\install\lib\x86_64\mt\
-    move ..\install\lib\x86_64\*.lib ..\install\lib\x86_64\mt\
+    move out\mt out\install
+    mkdir out\install\lib\x86_64\mt\
+    move out\install\lib\x86_64\*.lib out\install\lib\x86_64\mt\
 
-    xcopy ..\md\lib\x86_64\*.lib ..\install\lib\x86_64\md\
-    xcopy ..\mtd\lib\x86_64\*.lib ..\install\lib\x86_64\mtd\
-    xcopy ..\mdd\lib\x86_64\*.lib ..\install\lib\x86_64\mdd\
+    xcopy out\md\lib\x86_64\*.lib out\install\lib\x86_64\md\
+    xcopy out\mtd\lib\x86_64\*.lib out\install\lib\x86_64\mtd\
+    xcopy out\mdd\lib\x86_64\*.lib out\install\lib\x86_64\mdd\
 )
 
 :: Create an archive.
 if "%BUILD_RELEASE_VARIANTS%" == "1" (
-    7z a -ttar -so ..\filament-release.tar ..\install\* | 7z a -si ..\filament-windows.tgz
+    cd out\install
+    7z a -ttar -so ..\..\filament-release.tar * | 7z a -si ..\filament-windows.tgz
 )
+
+exit /b 0
+
+:BuildVariant
+set variant=%~1
+set flag=%~2
+set config=%~3
+
+mkdir out\cmake-%variant%
+cd out\cmake-%variant%
+if errorlevel 1 exit /b %errorlevel%
+
+cmake ..\.. -G "Visual Studio 16 2019" -A x64 %flag% -DCMAKE_INSTALL_PREFIX=..\%variant% || exit /b
+cmake --build . %INSTALL% --config %config% -- /m || exit /b
+
+cd ..\..
+
+:: Delete the cmake build folder, otherwise we run out of disk space on CI when
+:: building multiple variants.
+rd /s /q out\cmake-%variant%
+exit /b 0
