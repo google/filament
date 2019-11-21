6f51750b633d486e75f307bfc6eed55c58a1d270
Mathias Agopian <mathias@google.com>
2019-11-20 17:32:24 -0800

    Better workaround a resource culling issue.
    A texture resource can end-up with not usage bit set if it is used as
    an attachment of a render target that gets replaced by a moveResource()
    call. The framegraph should take care of culling our this resource,
    in that case, but currently doesn't.
    
    We worked around this issue by not declaring the resources, but it's
    better to do this in the frame graph code, as to not affect the 
    user facing API.

--- filament/src/Renderer.cpp
+++ filament/src/Renderer.cpp
@@ -344,13 +344,11 @@ void FRenderer::renderJob(ArenaScope& arena, FView& view) {
                 data.color = builder.createTexture("Color Buffer",
                         { .width = svp.width, .height = svp.height, .format = hdrFormat });
 
-                if (colorPassNeedsDepthBuffer) {
-                    data.depth = builder.createTexture("Depth Buffer", {
-                            .width = svp.width, .height = svp.height,
-                            .format = TextureFormat::DEPTH24
-                    });
-                    data.depth = builder.write(builder.read(data.depth));
-                }
+                data.depth = builder.createTexture("Depth Buffer", {
+                        .width = svp.width, .height = svp.height,
+                        .format = TextureFormat::DEPTH24
+                });
+                data.depth = builder.write(builder.read(data.depth));
 
                 data.color = builder.write(builder.read(data.color));
                 data.rt = builder.createRenderTarget("Color Pass Target", {
