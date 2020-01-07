892302442f63138027e6e8c0a4f451ca2308b21e
Daniel Lehmann <59584561+danlehmann@users.noreply.github.com>
2020-01-06 16:27:15 -0800

    Fix Backend enum in Java to be consistent with C definition (#2006)
    The C definition was changed to include METAL, moving NOOP further down.
    The Java definition never caught up, making NOOP mean METAL and making the
    real NOOP unreachable.
    
    This patch puts them back in sync. Also, updated the Java comments to match
    the comments from the C definition.

--- android/filament-android/src/main/java/com/google/android/filament/Engine.java
+++ android/filament-android/src/main/java/com/google/android/filament/Engine.java
@@ -119,13 +119,17 @@ public class Engine {
          */
         DEFAULT,
         /**
-         * Selects the OpenGL ES driver.
+         * Selects the OpenGL driver (which supports OpenGL ES as well).
          */
         OPENGL,
         /**
-         * Selects the experimental Vulkan driver.
+         * Selects the Vulkan driver if the platform supports it.
          */
         VULKAN,
+        /**
+         * Selects the Metal driver if the platform supports it.
+         */
+        METAL,
         /**
          * Selects the no-op driver for testing purposes.
          */
