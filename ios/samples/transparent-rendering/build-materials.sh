7852fd2ffbdb9d626ba1a93b642a1700b50d39de
Ben Doherty <bendoherty@google.com>
2020-01-29 10:59:19 -0800

    Build iOS samples during CI jobs (#2076)

--- ios/samples/transparent-rendering/build-materials.sh
+++ ios/samples/transparent-rendering/build-materials.sh
@@ -1,10 +1,14 @@
 # Compile bakedColor material.
-matc_path="../../../out/release/filament/bin/matc"
+
+HOST_TOOLS_PATH="${HOST_TOOLS_PATH:-../../../out/release/filament/bin}"
+matc_path=`find ${HOST_TOOLS_PATH} -name matc -type f | head -n 1`
+
 if [[ ! -e "${matc_path}" ]]; then
-  echo "No matc binary could be found in ../../../out/release/filament/bin/."
+  echo "No matc binary could be found in ${HOST_TOOLS_PATH}."
   echo "Ensure Filament has been built/installed before building this app."
   exit 1
 fi
+
 mkdir -p "${PROJECT_DIR}/generated/"
 "${matc_path}" \
     --api all \
