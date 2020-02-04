1fde56c9058350122626002a7e136f93ff561d12
Romain Guy <romainguy@curious-creature.com>
2020-02-04 12:06:29 -0800

    Update to cgltf v1.5

--- third_party/cgltf/test/test_all.py
+++ third_party/cgltf/test/test_all.py
@@ -6,27 +6,30 @@ from sys import platform
 num_tested = 0
 num_errors = 0
 
-def collect_files(path, type, exe):
+def get_executable_path(name):
+    if platform == "win32":
+        return "build\\Debug\\" + name
+    else:
+        return "build/" + name
+
+def collect_files(path, type, name):
     global num_tested
     global num_errors
+    exe = get_executable_path(name)
     for the_file in os.listdir(path):
         file_path = os.path.join(os.path.normpath(path), the_file)
         if os.path.isfile(file_path):
             if the_file.endswith(type):
                 num_tested = num_tested +1
-                print("### " + exe + " " + file_path)
-                result = 0
-                if platform == "win32":
-                    result = os.system("build\\Debug\\{0} \"{1}\"".format(exe, file_path))
-                else:
-                    result = os.system("build/{0} \"{1}\"".format(exe, file_path))
+                print("### " + name + " " + file_path)
+                result = os.system("{0} \"{1}\"".format(exe, file_path))
                 print("### Result: " + str(result) + "\n")
                 if result != 0:
                     num_errors = num_errors + 1
                     print("Error.")
                     sys.exit(1)
         elif os.path.isdir(file_path):
-            collect_files(file_path, type, exe)
+            collect_files(file_path, type, name)
 
 if __name__ == "__main__":
     if not os.path.exists("build/"):
@@ -43,8 +46,8 @@ if __name__ == "__main__":
         os.system("git remote add origin https://github.com/KhronosGroup/glTF-Sample-Models.git")
         os.system("git config core.sparsecheckout true")
         f = open(".git/info/sparse-checkout", "w+")
-        f.write("2.0/*\n");
-        f.close();
+        f.write("2.0/*\n")
+        f.close()
         os.system("git pull --depth=1 origin master")
         os.chdir("..")
     collect_files("glTF-Sample-Models/2.0/", ".glb", "cgltf_test")
@@ -52,6 +55,12 @@ if __name__ == "__main__":
     collect_files("glTF-Sample-Models/2.0/", ".glb", "test_conversion")
     collect_files("glTF-Sample-Models/2.0/", ".gltf", "test_conversion")
     collect_files("glTF-Sample-Models/2.0/", ".gltf", "test_write")
+
+    result = os.system(get_executable_path("test_math"))
+    if result != 0:
+        num_errors = num_errors + 1
+        print("Error.")
+        sys.exit(1)
+
     print("Tested files: " + str(num_tested))
     print("Errors: " + str(num_errors))
-
