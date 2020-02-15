f8f2ca269a37d60b4b7a10e2c47f8323ef4faac6
Mathias Agopian <mathias@google.com>
2020-02-14 16:56:49 -0800

    tweak SSAO
    - got rid of all the precomputed samples for now, it makes things
      much more simple, and actually doesn't slow things down. Use the
      analytic offsets (spiral) instead.
    
    - use textureLod instead of texelFetch in places where we need to do
      how own clamping, turns out it's faster (as measured on pixel4).
    
    - use a interleaved gradient noise instead of a hardcoded blue noise,
      the difference isn't big.
    
    Overall we're actually 0.2ms faster out of 3.5ms. It's not immediately
    clear why, might just be noise.

--- filament/src/PostProcessManager.cpp
+++ filament/src/PostProcessManager.cpp
@@ -42,29 +42,6 @@ using namespace math;
 using namespace backend;
 using namespace filament::details;
 
-// The blue noise texture data below is converted from the images provided for
-// download by Christoph Peters under the Creative Commons CC0 Public
-// Domain Dedication (CC0 1.0) license.
-// See http://momentsingraphics.de/BlueNoise.html
-const uint8_t kBlueNoise[] = {
-    0x6f, 0x31, 0x8e, 0xa2, 0x71, 0xc3, 0x47, 0xb1, 0xc9, 0x32, 0x97, 0x5e, 0x42, 0x25, 0x55, 0xfc,
-    0x19, 0x63, 0xef, 0xde, 0x20, 0xfa, 0x94, 0x13, 0x26, 0x6a, 0xdc, 0xaa, 0xc2, 0x8a, 0x0d, 0xa7,
-    0x7d, 0xb2, 0x4f, 0x0f, 0x41, 0xad, 0x7b, 0x57, 0xd5, 0x83, 0xf7, 0x17, 0x74, 0x36, 0xe5, 0xd4,
-    0x29, 0xca, 0x98, 0x84, 0xbd, 0x68, 0x35, 0xec, 0xa1, 0x3e, 0x01, 0xb5, 0x4d, 0xf1, 0x93, 0x44,
-    0x02, 0xf4, 0x38, 0x5b, 0xe6, 0x05, 0xcc, 0x1c, 0xbb, 0x65, 0x90, 0xce, 0x21, 0x5c, 0xbe, 0x6b,
-    0xdf, 0xa4, 0x72, 0x24, 0xd6, 0x9c, 0x8b, 0x46, 0xf5, 0x54, 0xe2, 0x30, 0x7e, 0x9e, 0x11, 0x87,
-    0x53, 0xc4, 0x15, 0xfe, 0x4c, 0x2d, 0xb3, 0x73, 0x0c, 0x28, 0xa9, 0x69, 0xfd, 0xb0, 0xd3, 0x3b,
-    0x64, 0xb4, 0x91, 0x7a, 0xac, 0x61, 0xeb, 0x81, 0xd7, 0x95, 0xc7, 0x08, 0x48, 0x1a, 0xee, 0x2c,
-    0xe8, 0x1f, 0x45, 0x0b, 0xcd, 0x3a, 0x12, 0xc1, 0x58, 0x3c, 0x70, 0xdd, 0x8c, 0x56, 0x78, 0x99,
-    0xd0, 0x82, 0xf3, 0xa0, 0xe0, 0x6e, 0x22, 0xf8, 0xa5, 0x18, 0xea, 0xb8, 0x34, 0xc6, 0xab, 0x06,
-    0x6c, 0xbc, 0x33, 0x59, 0x89, 0xba, 0x9a, 0x4e, 0x2f, 0x86, 0x62, 0x9d, 0x23, 0xf9, 0x5f, 0x3f,
-    0x10, 0x4b, 0xdb, 0x27, 0x00, 0x43, 0xe4, 0x79, 0xc5, 0xf0, 0x03, 0x4a, 0x7f, 0x14, 0xe3, 0x8f,
-    0xf6, 0xaf, 0x77, 0xc8, 0xfb, 0x67, 0x92, 0x0e, 0xd1, 0xae, 0x6d, 0xda, 0xc0, 0x52, 0xcb, 0xa3,
-    0x1d, 0x5d, 0x96, 0x16, 0xa6, 0xb6, 0x37, 0x1e, 0x5a, 0x40, 0x2a, 0x8d, 0xa8, 0x39, 0x75, 0x2e,
-    0xd8, 0xe9, 0x3d, 0x80, 0x51, 0xed, 0xd9, 0x76, 0x9f, 0xff, 0xb9, 0x1b, 0xf2, 0x66, 0x04, 0x85,
-    0x49, 0xbf, 0x09, 0xd2, 0x2b, 0x60, 0x07, 0x88, 0xe7, 0x50, 0x0a, 0x7c, 0xe1, 0xcf, 0x9b, 0xb7
-};
-
 static constexpr uint8_t kMaxBloomLevels = 12u;
 static_assert(kMaxBloomLevels >= 3, "We require at least 3 bloom levels");
 
@@ -155,34 +132,6 @@ void PostProcessManager::init() noexcept {
     mDummyZeroTexture = driver.createTexture(SamplerType::SAMPLER_2D, 1,
             TextureFormat::RGBA8, 0, 1, 1, 1, TextureUsage::DEFAULT);
 
-    mNoiseTexture = driver.createTexture(SamplerType::SAMPLER_2D, 1,
-            TextureFormat::RGB16F, 0, 16, 16, 1, TextureUsage::DEFAULT);
-
-    FMaterialInstance* const mi = mSSAO.getMaterialInstance();
-    mi->setParameter("noise", mNoiseTexture, {});
-
-    // Initialize the blue noise texture for SSAO
-    // These should match the parameters used in ssaogen. We recompute the noise texture from the
-    // blue noise data (instead of using ssaogen) to save space in this binary, as we only
-    // need to store 256 bytes instead of 3 KiB.
-    const float kSpiralTurns = 7.0;
-    const size_t spiralSampleCount = 7;
-    const size_t trigNoiseSampleCount = 256;
-    const float dalpha = 1.0f / (spiralSampleCount - 0.5f);
-    const size_t size = trigNoiseSampleCount * sizeof(float3);
-    PixelBufferDescriptor noiseData(driver.allocate(size), size,
-            PixelDataFormat::RGB, PixelDataType::HALF);
-    half3* const noise = static_cast<half3 *>(noiseData.buffer);
-    for (size_t i = 0; i < trigNoiseSampleCount; i++) {
-        float phi = kBlueNoise[i] / 255.0f;
-        float dr = phi * dalpha;
-        float dphi = float(2.0 * F_PI * kSpiralTurns * phi * phi * dalpha * dalpha
-                           + phi * 2.0 * F_PI * (1.0 + kSpiralTurns * dalpha * dalpha)
-                           + phi * 4.0 * F_PI * kSpiralTurns * dalpha * dalpha);
-        noise[i] = half3{ std::cos(dphi), std::sin(dphi), dr };
-    }
-    driver.update2DImage(mNoiseTexture, 0, 0, 0, 16, 16, std::move(noiseData));
-
     PixelBufferDescriptor dataOne(driver.allocate(4), 4, PixelDataFormat::RGBA, PixelDataType::UBYTE);
     PixelBufferDescriptor dataZero(driver.allocate(4), 4, PixelDataFormat::RGBA, PixelDataType::UBYTE);
     *static_cast<uint32_t *>(dataOne.buffer) = 0xFFFFFFFF;
@@ -195,7 +144,6 @@ void PostProcessManager::terminate(DriverApi& driver) noexcept {
     FEngine& engine = mEngine;
     driver.destroyTexture(mDummyOneTexture);
     driver.destroyTexture(mDummyZeroTexture);
-    driver.destroyTexture(mNoiseTexture);
     mSSAO.terminate(engine);
     mMipmapDepth.terminate(engine);
     mBilateralBlur.terminate(engine);
@@ -529,15 +477,15 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::ssao(FrameGraph& fg, RenderP
 
     auto& SSAOPass = fg.addPass<SSAOPassData>("SSAO Pass",
             [&](FrameGraph::Builder& builder, auto& data) {
+                auto const& desc = builder.getDescriptor(depth);
 
                 data.options = options;
-
-                auto const& desc = builder.getDescriptor(depth);
                 data.depth = builder.sample(depth);
-
                 data.ssao = builder.createTexture("SSAO Buffer", {
-                        .width = desc.width, .height = desc.height,
-                        .format = TextureFormat::R8 });
+                        .width = desc.width,
+                        .height = desc.height,
+                        .format = TextureFormat::R8
+                });
 
                 // Here we use the depth test to skip pixels at infinity (i.e. the skybox)
                 // Note that we have to clear the SAO buffer because blended objects will end-up
@@ -545,11 +493,9 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::ssao(FrameGraph& fg, RenderP
                 // The bilateral filter in the blur pass will ignore pixels at infinity.
 
                 data.ssao = builder.write(data.ssao);
-                data.depth = builder.sample(data.depth);
-
-                data.rt = builder.createRenderTarget("SSAO Target",
-                        { .attachments = { data.ssao, data.depth }
-                        }, TargetBufferFlags::COLOR);
+                data.rt = builder.createRenderTarget("SSAO Target", {
+                        .attachments = { data.ssao, data.depth }
+                }, TargetBufferFlags::COLOR);
             },
             [=](FrameGraphPassResources const& resources,
                     auto const& data, DriverApi& driver) {
@@ -563,7 +509,9 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::ssao(FrameGraph& fg, RenderP
                         0.5f * cameraInfo.projection[1].y * desc.height);
 
                 FMaterialInstance* const mi = mSSAO.getMaterialInstance();
-                mi->setParameter("depth", depth, {});
+                mi->setParameter("depth", depth, {
+                        .filterMin = SamplerMinFilter::NEAREST_MIPMAP_NEAREST
+                });
                 mi->setParameter("resolution",
                         float4{ desc.width, desc.height, 1.0f / desc.width, 1.0f / desc.height });
                 mi->setParameter("radius", data.options.radius);
@@ -627,11 +575,9 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::depthPass(FrameGraph& fg, Re
 
                 data.depth = builder.write(builder.read(data.depth));
 
-                // nested designated initializers not in C++ standard: https://tinyurl.com/y6krwocx
-                FrameGraphRenderTarget::Descriptor d;
-                d.attachments.depth = data.depth;
-                data.rt = builder.createRenderTarget("SSAO Depth Target", d,
-                                                     TargetBufferFlags::DEPTH);
+                data.rt = builder.createRenderTarget("SSAO Depth Target", {
+                        .attachments = {{}, { data.depth }}
+                }, TargetBufferFlags::DEPTH);
             },
             [=](FrameGraphPassResources const& resources, auto const& data, DriverApi& driver) {
                 auto out = resources.getRenderTarget(data.rt);
@@ -726,12 +672,10 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::bilateralBlurPass(FrameGraph
                 // TODO: "oneOverEdgeDistance" should be a user-settable parameter
                 //       z-distance that constitute an edge for bilateral filtering
                 FMaterialInstance* const mi = mBilateralBlur.getMaterialInstance();
-                mi->setParameter("ssao", ssao, {});
-                mi->setParameter("depth", depth, {});
-                mi->setParameter("axis", axis);
+                mi->setParameter("ssao", ssao, { /* only reads level 0 */ });
+                mi->setParameter("depth", depth, { /* only reads level 0 */ });
+                mi->setParameter("axis", axis / float2{desc.width, desc.height});
                 mi->setParameter("oneOverEdgeDistance", 1.0f / 0.1f);
-                mi->setParameter("resolution",
-                        float4{ desc.width, desc.height, 1.0f / desc.width, 1.0f / desc.height });
                 mi->commit(driver);
                 mi->use(driver);
 
