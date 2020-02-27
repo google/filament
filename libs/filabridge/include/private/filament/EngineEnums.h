ee3cb95af6dd8b294e866587890f3c0a97e8ba08
Benjamin Doherty <bendoherty@google.com>
2020-02-27 12:38:37 -0800

    Set MAX_SHADOW_CASTING_SPOTS to 0 for now

--- libs/filabridge/include/private/filament/EngineEnums.h
+++ libs/filabridge/include/private/filament/EngineEnums.h
@@ -53,7 +53,7 @@ constexpr size_t CONFIG_MAX_LIGHT_INDEX = CONFIG_MAX_LIGHT_COUNT - 1;
 // Light space coordinates are computed in the vertex shader and interpolated across fragments.
 // Thus, each additional shadow-casting spot light adds 4 additional varying components. Higher
 // values may cause the number of varyings to exceed the driver limit.
-constexpr size_t CONFIG_MAX_SHADOW_CASTING_SPOTS = 2;
+constexpr size_t CONFIG_MAX_SHADOW_CASTING_SPOTS = 0;
 
 // This value is also limited by UBO size, ES3.0 only guarantees 16 KiB.
 // We store 64 bytes per bone.
