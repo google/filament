ee91ab53d696b07589ce05d030b2d20be40f04ef
Pixelflinger <pixelflinger@gmail.com>
2020-03-16 14:29:52 -0700

    apply micro-shadowing after contact shadows
    also, don't compute contact shadows when we know we're fully shadowed.

--- shaders/src/light_directional.fs
+++ shaders/src/light_directional.fs
@@ -39,18 +39,22 @@ void evaluateDirectionalLight(const MaterialInputs material,
 #if defined(HAS_SHADOWING)
     if (light.NoL > 0.0) {
         float ssContactShadowOcclusion = 0.0;
-        if (frameUniforms.ssContactShadowDistance != 0.0) {
+
+        if ((frameUniforms.directionalShadows & 1u) != 0u) {
+            visibility = shadow(light_shadowMap, 0u, getLightSpacePosition());
+        }
+
+        if (visibility > 0.0 && frameUniforms.ssContactShadowDistance != 0.0) {
             if (objectUniforms.screenSpaceContactShadows != 0) {
                 ssContactShadowOcclusion = screenSpaceContactShadow(light.l);
             }
         }
-        if ((frameUniforms.directionalShadows & 1u) != 0u) {
-            visibility = shadow(light_shadowMap, 0u, getLightSpacePosition());
-            #if defined(MATERIAL_HAS_AMBIENT_OCCLUSION)
-            visibility *= computeMicroShadowing(light.NoL, material.ambientOcclusion);
-            #endif
-        }
+
         visibility = visibility * (1.0 - ssContactShadowOcclusion);
+
+        #if defined(MATERIAL_HAS_AMBIENT_OCCLUSION)
+        visibility *= computeMicroShadowing(light.NoL, material.ambientOcclusion);
+        #endif
     } else {
 #if defined(MATERIAL_CAN_SKIP_LIGHTING)
         return;
