dd1d2f3d8e2d3c81f9df45b7f66aca026ac2e970
Philip Rideout <philiprideout@gmail.com>
2020-03-30 15:54:48 -0700

    Bump material version to prep for release.

--- libs/filabridge/include/filament/MaterialEnums.h
+++ libs/filabridge/include/filament/MaterialEnums.h
@@ -27,7 +27,7 @@
 namespace filament {
 
 // update this when a new version of filament wouldn't work with older materials
-static constexpr size_t MATERIAL_VERSION = 4;
+static constexpr size_t MATERIAL_VERSION = 5;
 
 /**
  * Supported shading models
