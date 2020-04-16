ce6b17803400e2e28d1d6e82d42aead55d19bc5f
Pixelflinger <pixelflinger@gmail.com>
2020-04-16 13:03:22 -0700

    fog: correct for exp2 used in the shader

--- filament/src/View.cpp
+++ filament/src/View.cpp
@@ -547,9 +547,10 @@ void FView::prepare(FEngine& engine, backend::DriverApi& driver, ArenaScope& are
     constexpr float epsilon = 0.001f;
     const float heightFalloff = std::max(epsilon, fogOptions.heightFalloff);
 
-    // precalculate the constant part of density  integral
-    const float density = (fogOptions.density / heightFalloff) *
-            std::exp(-heightFalloff * (camera->getPosition().y - fogOptions.height));
+    // precalculate the constant part of density  integral and correct for exp2() in the shader
+    const float density = ((fogOptions.density / heightFalloff) *
+            std::exp(-heightFalloff * (camera->getPosition().y - fogOptions.height)))
+                    * float(1.0f / F_LN2);
 
     u.setUniform(offsetof(PerViewUib, fogStart),             fogOptions.distance);
     u.setUniform(offsetof(PerViewUib, fogMaxOpacity),        fogOptions.maximumOpacity);
