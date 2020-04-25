2690316d9055616b654cfd951dff8a5863df7713
Romain Guy <romainguy@curious-creature.com>
2020-04-24 23:04:16 -0700

    Make CI scripts safer
    Display and prompt and require user confirmation
    Only runs install commands in GitHub environments
    Add extra README file detailing what the scripts are for

--- build/android/build.sh
+++ build/android/build.sh
@@ -8,11 +8,27 @@
 #
 # The default is release
 
-set -e
-set -x
-
 NDK_VERSION="ndk;21.0.6113669"
 ANDROID_NDK_VERSION=21
+
+echo "This script is intended to run in a CI environment and may modify your current environment."
+echo "Please refer to ./BUILDING.md for more information."
+
+read -r -p "Do you wish to proceed (y/n)? " choice
+case "${choice}" in
+    y|Y)
+	      echo "Build will proceed..."
+	      ;;
+    n|N)
+    	  exit 0
+    	  ;;
+	  *)
+        exit 0
+        ;;
+esac
+
+set -e
+set -x
 
 UNAME=`echo $(uname)`
 LC_UNAME=`echo $UNAME | tr '[:upper:]' '[:lower:]'`
