92c017bdd3208dd4bda7e22baac08917fc8557f9
Philip Rideout <philiprideout@gmail.com>
2020-05-29 08:40:51 -0700

    Simplify WebGL context creation.

--- web/filament-js/createContext.js
+++
@@ -1,6 +0,0 @@
-Filament.createContext = (canvas, options) => {
-    const ctx = canvas.getContext("webgl2", options);
-    const handle = GL.registerContext(ctx, options);
-    GL.makeContextCurrent(handle);
-    return ctx;
-};
