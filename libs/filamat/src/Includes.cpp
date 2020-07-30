1c767324fba623ce1c7673799cbfb56f1eb9cf1f
Benjamin Doherty <bendoherty@google.com>
2020-07-30 16:38:14 -0700

    Ignore commented-out #includes in material files

--- libs/filamat/src/Includes.cpp
+++ libs/filamat/src/Includes.cpp
@@ -28,6 +28,53 @@ static bool isWhitespace(char c) {
     return (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v');
 }
 
+struct CommentRange {
+    size_t begin;
+    size_t end;
+
+    CommentRange(size_t begin, size_t end) :
+        begin(begin), end(end) {}
+};
+
+template<size_t BTSize, size_t ETSize>
+static void findCommentRanges(const char (&beginToken)[BTSize], const char (&endToken)[ETSize],
+        std::string& source, std::vector<CommentRange>& comments) {
+    // BTSize and ETSize include the null terminator.
+    constexpr size_t beginTokenSize = BTSize - 1;
+    constexpr size_t endTokenSize = ETSize - 1;
+
+    // Find the first occurance of the begin token.
+    size_t r = source.find(beginToken);
+    while (r != std::string::npos) {
+        // This is the start of a new comment.
+        const size_t commentStart = r;
+
+        // Advance the pointer past the token.
+        r += beginTokenSize;
+
+        // Find the terminating token for this comment.
+        r = source.find(endToken, r);
+        if (r != std::string::npos) {
+            // Advance the pointer past the token.
+            r += endTokenSize;
+
+            // The end of the comment is the last character of the end token.
+            size_t commentEnd = r - 1;
+
+            comments.emplace_back(commentStart, commentEnd);
+
+            // Find the next comment.
+            r = source.find(beginToken, r);
+        } else {
+            // This comment was never terminated- consider the rest of the source to a comment.
+            // This also solves the case of a single-line source with a // comment but no
+            // terminating newline.
+            comments.emplace_back(commentStart, source.size() - 1);
+        }
+    }
+}
+
+
 bool resolveIncludes(IncludeResult& root, IncludeCallback callback,
         const ResolveOptions& options, size_t depth) {
     if (depth > 30) {
@@ -173,6 +220,19 @@ std::vector<FoundInclude> parseForIncludes(const utils::CString& source) {
     }
     std::string sourceString = source.c_str();
 
+    std::vector<CommentRange> commentRanges;
+    findCommentRanges("/*", "*/", sourceString, commentRanges);
+    findCommentRanges("//", "\n", sourceString, commentRanges);
+
+    auto isInsideComment = [&commentRanges] (size_t pos) {
+        for (auto r : commentRanges) {
+            if (pos >= r.begin && pos <= r.end) {
+                return true;
+            }
+        }
+        return false;
+    };
+
     size_t result = sourceString.find("#include");
 
     while(result != std::string::npos) {
@@ -180,6 +240,11 @@ std::vector<FoundInclude> parseForIncludes(const utils::CString& source) {
 
         // Move to the character immediately after #include
         result += 8;
+
+        // If this include is within a comment, ignore it.
+        if (isInsideComment(includeStart)) {
+            continue;
+        }
 
         // Eat up any whitespace after "#include"
         while (result < sourceString.length() && isWhitespace(sourceString[result])) {
