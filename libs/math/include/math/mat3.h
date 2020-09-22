450fe214f9b62a37e6069648a185afa7ccf72f82
Philip Rideout <philiprideout@gmail.com>
2020-09-21 17:37:49 -0700

    Flip the shading normal when det < 0.
    This was tested by replacing the node 0 scale in BusterDrone with
    [-1, 1, 1].
    
    For future reference, commit f728776 shows when we switched from
    transpose(inverse()) to cof(). This was a good change, but before that
    particular change, we had a "two wrongs made a right" situation for
    mirrored normals.
    
    Fixes #3001.

--- libs/math/include/math/mat3.h
+++ libs/math/include/math/mat3.h
@@ -260,6 +260,26 @@ public:
     /**
      * Returns a matrix suitable for transforming normals
      *
+     * Note that the inverse-transpose of a matrix is equal to its cofactor matrix divided by its
+     * determinant:
+     *
+     *     transpose(inverse(M)) = cof(M) / det(M)
+     *
+     * The cofactor matrix is faster to compute than the inverse-transpose, and it can be argued
+     * that it is a more correct way of transforming normals anyway. Some references from Dale
+     * Weiler, Nathan Reed, Inigo Quilez, and Eric Lengyel:
+     *
+     *   - https://github.com/graphitemaster/normals_revisited
+     *   - http://www.reedbeta.com/blog/normals-inverse-transpose-part-1/
+     *   - https://www.shadertoy.com/view/3s33zj
+     *   - FGED Volume 1, section 1.7.5 "Inverses of Small Matrices"
+     *   - FGED Volume 1, section 3.2.2 "Transforming Normal Vectors"
+     *
+     * In "Transforming Normal Vectors", Lengyel notes that there are two types of transformed
+     * normals: one that uses the transposed adjugate (aka cofactor matrix) and one that uses the
+     * transposed inverse. He goes on to say that this difference is inconsequential, except when
+     * mirroring is involved.
+     *
      * @param m the transform applied to vertices
      * @return a matrix to apply to normals
      *
