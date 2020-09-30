2ba7f6418e176b938ed8018a8aae1094dabd0adf
Benjamin Doherty <bendoherty@google.com>
2020-09-30 13:19:42 -0600

    Delete testing code

--- android/samples/sample-gltf-viewer/src/main/java/com/google/android/filament/gltf/MainActivity.kt
+++ android/samples/sample-gltf-viewer/src/main/java/com/google/android/filament/gltf/MainActivity.kt
@@ -23,7 +23,6 @@ import android.view.Choreographer
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.SurfaceView
-import com.google.android.filament.View
 import com.google.android.filament.utils.KtxLoader
 import com.google.android.filament.utils.ModelViewer
 import com.google.android.filament.utils.Utils
@@ -73,8 +72,6 @@ class MainActivity : Activity() {
         val bloomOptions = modelViewer.view.bloomOptions
         bloomOptions.enabled = true
         modelViewer.view.bloomOptions = bloomOptions
-
-        modelViewer.view.setShadowType(View.ShadowType.VSM)
     }
 
     private fun createRenderables() {
