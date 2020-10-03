79f1744823450306580d0466a7244905d704844f
Philip Rideout <philiprideout@gmail.com>
2020-10-03 15:42:48 -0700

    Automation: fix segfault when closing the app early.
    The Filament View that gets passed to tick() is retained briefly when
    screenshots are enabled, but it may be destroyed by the time the
    PixelBufferDescriptor callback is invoked. So, we needed a way of
    notifying the AutomationEngine.

--- samples/gltf_viewer.cpp
+++ samples/gltf_viewer.cpp
@@ -873,6 +873,7 @@ int main(int argc, char** argv) {
     };
 
     auto cleanup = [&app](Engine* engine, View*, Scene*) {
+        app.automationEngine->terminate();
         app.loader->destroyAsset(app.asset);
         app.materials->destroyMaterials();
 
