8b6ddfe97a50cf3edfd597d4b3d4885ae68d0380
Philip Rideout <philiprideout@gmail.com>
2020-12-04 14:00:51 -0800

    Add zbloat tool.
    This tool makes it easy to analyze the composition of Android
    applications that use Filament.
    
    - Filament materials are shown in the treemap if `resgen --json` was
      used in the build.
    - The input path can be a so file, folder, or zip archive (apk or aar).
    - If the path is a zip or folder, helps you find the file to analyze.
    - The generated web report is a self-contained HTML file.
    - Reports the gzipped size of all Filament materials.
    
    For example, from a mac, you can generate a self-contained HTML file by
    typing this from the Filament repo root:
    
        ./tools/zbloat/zbloat.py ./android/filament-android
        open index.html
    
    This tool uses Python, `nm`, and `objdump`.
    
    The `nm` tool works slightly differently between macOS and Linux, so a
    `Dockerfile` is provided that installs dependencies inside a Linux
    container. This is also convenient if you do not have both versions of
    Python on your system.
    
    The easy way to use docker is to invoke the helper bash script. Simply
    type `zbloat.sh [args...]` instead of `zbloat.py [args...]`. The first
    time you run it, it will be slow but subsequent times will be fast.
    
    Many thanks to Evan Martin for his interactive treemap widget.

---
+++ tools/zbloat/zbloat.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+For usage help, see README.md
+"""
+
+import argparse
+import os
+import tempfile
+import zipfile
+import json
+
+from pathlib import Path
+
+TEMPDIR = tempfile.gettempdir()
+SCRIPTDIR = Path(os.path.realpath(__file__)).parent
+DEBUG = False
+RESGEN_MAGIC = b'__RESGEN__'
+
+def format_bytes(bytes):
+    """Pretty-print a number of bytes."""
+    if bytes > 1e6:
+        bytes = bytes / 1.0e6
+        return '%.1fm' % bytes
+    if bytes > 1e3:
+        bytes = bytes / 1.0e3
+        return '%.1fk' % bytes
+    return str(bytes)
+
+
+def get_hint_marker(filename):
+    HINTS = ['renderer', 'libfilament-jni']
+    hints = [hint in filename for hint in HINTS]
+    return '*' if True in hints else ' '
+
+
+def choose_from_dir(path: Path):
+    paths = list(path.glob('**/*.so'))
+    has_dso = False
+    for index, dso in enumerate(paths):
+        has_dso = True
+        size = dso.stat().st_size / 1000
+        filename = str(dso)
+        marker = get_hint_marker(filename)
+        print(f"{index:3} {marker} {size:8.0f} kB {filename}")
+    has_dso or quit("No .so file found in the specified path")
+    val = input("Which dso should be analyzed? Type a number. ")
+    return str(paths[int(val)])
+
+
+def choose_from_zip(path: Path):
+    z = zipfile.ZipFile(path)
+    paths = []
+    for info in z.infolist():
+        if info.filename.endswith('.so'):
+            paths.append((info.filename, info.compress_size, info.file_size))
+    has_dso = False
+    for index, dso in enumerate(paths):
+        has_dso = True
+        filename, csize, usize = dso
+        csize = csize / 1000
+        usize = usize / 1000
+        marker = get_hint_marker(filename)
+        print(f"{index:3} {marker} {usize:8.0f} kB ({csize:8.0f} kB) {filename}")
+    has_dso or quit("No .so file found in the specified path")
+    val = input("Which dso should be analyzed? Type a number. ")
+    chosen_filename = paths[int(val)][0]
+    result_path = z.extract(chosen_filename, TEMPDIR)
+    if DEBUG: os.system(f'cp {result_path} .')
+    return result_path
+
+
+def extract_resgen(path: Path):
+    with open(path, mode='rb') as file:
+        content = file.read()
+    offset = content.find(RESGEN_MAGIC)
+    if offset < 0:
+        return "{}", b''
+    idx = offset + len(RESGEN_MAGIC)
+    size_string = ""
+    while chr(content[idx]) != '{':
+        size_string += chr(content[idx])
+        idx += 1
+        if len(size_string) >= 10: quit('Bad JSON chunk: ' + size_string)
+    json_string = content[idx:idx+int(size_string)]
+    if DEBUG: print(json_string)
+    resgen_json = json.loads(json_string)
+    total_size = 0
+    for mat in resgen_json:
+        total_size += resgen_json[mat]
+    return json.loads(json_string), content[offset - total_size:offset]
+
+
+def get_compressed_size(blob):
+    tmpzip = Path(f"{TEMPDIR}/tmp.zip")
+    with zipfile.ZipFile(tmpzip, "w") as tmp:
+        tmp.writestr("tmp", blob, zipfile.ZIP_DEFLATED)
+    return tmpzip.stat().st_size
+
+
+def treeify_resgen(resgen_json, resgen_blob):
+    csize = format_bytes(get_compressed_size(resgen_blob))
+    result = {}
+    total_size = 0
+    materials = []
+    for mat in resgen_json:
+        material_size = resgen_json[mat]
+        total_size += material_size
+        materials.append({
+            "data": {"$area": material_size, "$symbol": "read-only data"},
+            "name": mat + " " + format_bytes(material_size),
+        })
+    result["data"] = {"$area": total_size}
+    result["name"] = f"materials {format_bytes(total_size)} ({csize})"
+    result["children"] = materials
+    return result
+
+
+def main(args):
+    path = Path(args.input)
+    path.exists() or quit("No file or folder at the specified path.")
+    if path.is_dir():
+        path = choose_from_dir(path)
+    elif path.suffix in ['.zip', '.aar', '.apk']:
+        path = choose_from_zip(path)
+
+    dsopath = Path(path)
+    size = format_bytes(dsopath.stat().st_size)
+    info = f'Uncompressed size is {size}'
+
+    print('Scanning for resgen resources...')
+    resgen_json, resgen_blob = extract_resgen(dsopath)
+
+    print('Running nm... (this might take a while)')
+    os.system(f'nm -C -S {path} > {TEMPDIR}/nm.out')
+    if DEBUG: os.system(f'cp {TEMPDIR}/nm.out .')
+
+    print('Running objdump...')
+    os.system(f'objdump -h {path} > {TEMPDIR}/objdump.out')
+    if DEBUG: os.system(f'cp {TEMPDIR}/objdump.out .')
+
+    print('Generating treemap JSON...')
+    os.system(f'cd {TEMPDIR} ; python {SCRIPTDIR}/evmar_bloat.py syms > syms.json')
+    os.system(f'cd {TEMPDIR} ; python {SCRIPTDIR}/evmar_bloat.py sections > sections.json')
+
+    # Splice the materials JSON into the sections JSON.
+    sections_json = json.loads(open(f'{TEMPDIR}/sections.json').read())
+    for child in sections_json['children']:
+        if child["name"].startswith('sections '):
+            for section in child['children']:
+                if section["name"].startswith('rodata'):
+                    section["children"] = [ treeify_resgen(resgen_json, resgen_blob) ]
+    sections_json = "const kSections = " + json.dumps(sections_json) + ";\n"
+
+    symbols_json = open(f'{TEMPDIR}/syms.json').read()
+    symbols_json = "const kSymbols = " + symbols_json + ";\n"
+
+    if DEBUG: open('syms.json', 'wt').write(symbols_json)
+    if DEBUG: open('sections.json', 'wt').write(sections_json)
+
+    print(f'Generating {args.output}...')
+    template = open(f'{SCRIPTDIR}/template.html').read()
+    template = template.replace('$TITLE$', dsopath.name)
+    template = template.replace('$INFO$', info)
+    template = template.replace('$SYMBOLS_JSON$', symbols_json)
+    template = template.replace('$SECTIONS_JSON$', sections_json)
+    open(args.output, 'w').write(template)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", default="index.html", help="generated HTML file name")
+    parser.add_argument("input", help="path to folder, zip, aar, apk, or so")
+    args = parser.parse_args()
+    main(args)
