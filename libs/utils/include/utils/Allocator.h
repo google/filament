5ab7a9e3ab6237012fbb50c74911ac074c22cbaf
Adrian Perez <amperez@google.com>
2021-01-22 15:45:20 -0800

    Add swap() methods to Allocator.h (#3445)
    * Add swap() methods to Allocator.h
    
    This allows StructureOfArraysBase's move constructor to compile.
    Removed an apparent workaround in SingleInstanceComponentManager.
    
    * swap can be noexcept
    
    * Add swap() methods to Allocator.h
    
    This allows StructureOfArraysBase's move constructor to compile.
    
    Co-authored-by: Mathias Agopian <pixelflinger@gmail.com>

--- libs/utils/include/utils/Allocator.h
+++ libs/utils/include/utils/Allocator.h
@@ -416,6 +416,12 @@ public:
     void* end() const noexcept { return mEnd; }
     size_t getSize() const noexcept { return uintptr_t(mEnd) - uintptr_t(mBegin); }
 
+    friend void swap(HeapArea& lhs, HeapArea& rhs) noexcept {
+        using std::swap;
+        swap(lhs.mBegin, rhs.mBegin);
+        swap(lhs.mEnd, rhs.mEnd);
+    }
+
 private:
     void* mBegin = nullptr;
     void* mEnd = nullptr;
@@ -630,6 +636,15 @@ public:
     // An arena can't be copied
     Arena(Arena const& rhs) noexcept = delete;
     Arena& operator=(Arena const& rhs) noexcept = delete;
+
+    friend void swap(Arena& lhs, Arena& rhs) noexcept {
+        using std::swap;
+        swap(lhs.mArea, rhs.mArea);
+        swap(lhs.mAllocator, rhs.mAllocator);
+        swap(lhs.mLock, rhs.mLock);
+        swap(lhs.mListener, rhs.mListener);
+        swap(lhs.mArenaName, rhs.mArenaName);
+    }
 
 private:
     HeapArea mArea; // We might want to make that a template parameter too eventually.
