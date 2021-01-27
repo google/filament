02501c6e60dd1a192b820fe4ac60152ada5a01d7
Romain Guy <romainguy@curious-creature.com>
2021-01-26 16:35:49 -0800

    Modulate emissive by alpha on blended objects (#3459)
    This prevents light from being emitted from completely
    transparent pixels.

--- shaders/src/shading_lit.fs
+++ shaders/src/shading_lit.fs
@@ -299,7 +299,7 @@ void addEmissive(const MaterialInputs material, inout vec4 color) {
 #if defined(MATERIAL_HAS_EMISSIVE)
     highp vec4 emissive = material.emissive;
     highp float attenuation = mix(1.0, frameUniforms.exposure, emissive.w);
-    color.rgb += emissive.rgb * attenuation;
+    color.rgb += emissive.rgb * (attenuation * color.a);
 #endif
 }
 
