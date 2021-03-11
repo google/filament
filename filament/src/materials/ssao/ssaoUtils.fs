ed09870512d708ecb73f20fefb9811476afd9c5b
Benjamin Doherty <bendoherty@google.com>
2021-03-11 11:09:01 -0800

    Simplify

--- filament/src/materials/ssao/ssaoUtils.fs
+++ filament/src/materials/ssao/ssaoUtils.fs
@@ -10,18 +10,13 @@ highp float linearizeDepth(highp float depth, highp float depthParams) {
 }
 
 highp float sampleDepthLinear(const sampler2D depthTexture, const vec2 uv, float lod, highp float depthParams) {
-    return linearizeDepth(textureLod(depthTexture, uv, lod).r, depthParams);
-}
-
-highp vec2 ssFromUv(highp vec2 uv) {
 #if defined(TARGET_METAL_ENVIRONMENT) || defined(TARGET_VULKAN_ENVIRONMENT)
-    return vec2(uv.x, 1.0 - uv.y);
+    // On metal/vulkan, texture space is flipped vertically and we need to adjust the uv
+    // coordinates.
+    return linearizeDepth(textureLod(depthTexture, vec2(uv.x, 1.0 - uv.y), lod).r, depthParams);
+#else
+    return linearizeDepth(textureLod(depthTexture, uv, lod).r, depthParams);
 #endif
-    return uv;
-}
-
-highp vec2 uvFromSs(highp vec2 ss) {
-    return ssFromUv(ss);
 }
 
 #endif // MATERIALS_SSAO_UTILS
