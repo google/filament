812f0d577b1466dfd4223060dea6f8d1fa4c6ed0
Philip Rideout <philiprideout@gmail.com>
2021-05-06 19:23:21 -0700

    VulkanTexture: support format conversion via vkCmdBlitImage.

--- filament/backend/src/vulkan/VulkanStagePool.h
+++ filament/backend/src/vulkan/VulkanStagePool.h
@@ -33,7 +33,17 @@ struct VulkanStage {
     mutable uint64_t lastAccessed;
 };
 
+struct VulkanStageImage {
+    VkFormat format;
+    uint32_t width;
+    uint32_t height;
+    mutable uint64_t lastAccessed;
+    VmaAllocation memory;
+    VkImage image;
+};
+
 // Manages a pool of stages, periodically releasing stages that have been unused for a while.
+// This class manages two types of host-mappable staging areas: buffer stages and image stages.
 class VulkanStagePool {
 public:
     explicit VulkanStagePool(VulkanContext& context) noexcept : mContext(context) {}
@@ -41,6 +51,9 @@ public:
     // Finds or creates a stage whose capacity is at least the given number of bytes.
     // The stage is automatically released back to the pool after TIME_BEFORE_EVICTION frames.
     VulkanStage const* acquireStage(uint32_t numBytes);
+
+    VulkanStageImage const* acquireImage(PixelDataFormat format, PixelDataType type,
+            uint32_t width, uint32_t height);
 
     // Evicts old unused stages and bumps the current frame number.
     void gc() noexcept;
@@ -57,6 +70,9 @@ private:
 
     // Simple unordered set for stashing a list of in-use stages that can be reclaimed later.
     std::unordered_set<VulkanStage const*> mUsedStages;
+
+    std::unordered_set<VulkanStageImage const*> mFreeImages;
+    std::unordered_set<VulkanStageImage const*> mUsedImages;
 
     // Store the current "time" (really just a frame count) and LRU eviction parameters.
     uint64_t mCurrentFrame = 0;
