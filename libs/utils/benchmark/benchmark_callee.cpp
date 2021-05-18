083150f6096aedeca3828adf483fa24e6d636c9b
Benjamin Doherty <bendoherty@google.com>
2021-05-18 14:59:27 -0700

    Fix benchmark test

--- libs/utils/benchmark/benchmark_callee.cpp
+++ libs/utils/benchmark/benchmark_callee.cpp
@@ -14,6 +14,12 @@
  * limitations under the License.
  */
 
+#if __has_attribute(visibility)
+#    define UTILS_PUBLIC __attribute__((visibility("default")))
+#else
+#    define UTILS_PUBLIC
+#endif
 
+UTILS_PUBLIC
 void bar() noexcept {
 }
