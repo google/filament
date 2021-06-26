fe0b33c3ca823840f213054d9fb3b1f24817a01e
Mathias Agopian <mathias@google.com>
2021-06-25 17:50:37 -0700

    Increase OpenGL backend handle arena to 4 MiB

--- filament/backend/src/opengl/OpenGLDriver.h
+++ filament/backend/src/opengl/OpenGLDriver.h
@@ -33,7 +33,7 @@
 #include <set>
 
 #ifndef FILAMENT_OPENGL_HANDLE_ARENA_SIZE_IN_MB
-#    define FILAMENT_OPENGL_HANDLE_ARENA_SIZE_IN_MB 2
+#    define FILAMENT_OPENGL_HANDLE_ARENA_SIZE_IN_MB 4
 #endif
 
 namespace filament {
