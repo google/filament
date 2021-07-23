75c04a8a45673dd67149b65074a450376e31ad5f
Mathias Agopian <mathias@google.com>
2021-07-22 17:31:58 -0700

    split SSAO code into multiple files
    - depthUtils.fs and geometry.fs are actually completely generic
      at this point, and could be moved out of the SSAO folder.
    
    - saoImpl and ssctImpl are just a way to be able make several versions
      of the SAO material easily (which will be needed soon)

--- filament/src/materials/ssao/ssaoUtils.fs
+++ filament/src/materials/ssao/ssaoUtils.fs
@@ -1,27 +1,54 @@
-#ifndef MATERIALS_SSAO_UTILS
-#define MATERIALS_SSAO_UTILS
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-highp float linearizeDepth(highp float depth, highp float depthParams) {
-    // Our far plane is at infinity, which causes a division by zero below, which in turn
-    // causes some issues on some GPU. We workaround it by replacing "infinity" by the closest
-    // value representable in  a 24 bit depth buffer.
-    const float preventDiv0 = 1.0 / 16777216.0;
-    return depthParams / max(depth, preventDiv0);
+#ifndef FILAMENT_MATERIALS_SSAO_UTILS
+#define FILAMENT_MATERIALS_SSAO_UTILS
+
+#include "depthUtils.fs"
+
+// random number between 0 and 1, using interleaved gradient noise
+float random(const highp vec2 w) {
+    const vec3 m = vec3(0.06711056, 0.00583715, 52.9829189);
+    return fract(m.z * fract(dot(w, m.xy)));
 }
 
-highp float sampleDepth(const highp sampler2D depthTexture, const highp vec2 uv, float lod) {
+// returns the frag coord in the GL convention with (0, 0) at the bottom-left
+// resolution : width, height, 1/width, 1/height
+highp vec2 getFragCoord(const highp vec4 resolution) {
 #if defined(TARGET_METAL_ENVIRONMENT) || defined(TARGET_VULKAN_ENVIRONMENT)
-    // On metal/vulkan, texture space is flipped vertically and we need to adjust the uv
-    // coordinates.
-    return textureLod(depthTexture, vec2(uv.x, 1.0 - uv.y), lod).r;
+    return vec2(gl_FragCoord.x, resolution.y - gl_FragCoord.y);
 #else
-    return textureLod(depthTexture, uv, lod).r;
+    return gl_FragCoord.xy;
 #endif
 }
 
-highp float sampleDepthLinear(const highp sampler2D depthTexture, const highp vec2 uv, float lod, highp float depthParams) {
-    return linearizeDepth(sampleDepth(depthTexture, uv, lod), depthParams);
+vec2 pack(highp float normalizedDepth) {
+    // we need 16-bits of precision
+    highp float z = clamp(normalizedDepth, 0.0, 1.0);
+    highp float t = floor(256.0 * z);
+    mediump float hi = t * (1.0 / 256.0);   // we only need 8-bits of precision
+    mediump float lo = (256.0 * z) - t;     // we only need 8-bits of precision
+    return vec2(hi, lo);
 }
 
-#endif // MATERIALS_SSAO_UTILS
+highp float unpack(highp vec2 depth) {
+    // depth here only has 8-bits of precision, but the unpacked depth is highp
+    // this is equivalent to (x8 * 256 + y8) / 65535, which gives a value between 0 and 1
+    return (depth.x * (256.0 / 257.0) + depth.y * (1.0 / 257.0));
+}
+
+#endif // FILAMENT_MATERIALS_SSAO_UTILS
 
