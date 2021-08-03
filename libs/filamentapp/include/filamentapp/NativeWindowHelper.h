54bba195fabd042b1c8b43c39601c1bee96c007e
Romain Guy <romainguy@curious-creature.com>
2021-08-03 11:10:27 -0700

    Tag SDL2 windows as sRGB

--- libs/filamentapp/include/filamentapp/NativeWindowHelper.h
+++ libs/filamentapp/include/filamentapp/NativeWindowHelper.h
@@ -24,6 +24,8 @@ extern "C" void* getNativeWindow(SDL_Window* sdlWindow);
 #if defined(__APPLE__)
 // Add a backing CAMetalLayer to the NSView and return the layer.
 extern "C" void* setUpMetalLayer(void* nativeWindow);
+// Setup the window the way Filament expects (color space, etc.).
+extern "C" void prepareNativeWindow(SDL_Window* sdlWindow);
 // Resize the backing CAMetalLayer's drawable to match the new view's size. Returns the layer.
 extern "C" void* resizeMetalLayer(void* nativeView);
 #endif
