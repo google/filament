google/filament

commit 7278e5b5ba8bfc3cbea716df45c5d40ff33b905e

Use a clip space that matches our screen space for the z axis (#4440)
* Use a clip space that matches our screen space for the z axis

We now set the projection matrix so that the clip space for the z
axis follows the inverted DX convention, i.e.: z_clip between 1 (near)
and 0 (far), which matches our screen space.

Note that the actual clip space in the GPU can revert to the "inverted
GL convention", i.e.: z_clip between -1 and 1, if the clip_control
extension is not present. In that case the precision benefits are 
mostly lost.

Also note that there shouldn't be any user facing changes. 
The projection matrix at the API level, still follows the GL convention.

Also, currently, the VERTEX_DOMAIN_DEVICE for materials also keeps
the GL convention (this is corrected in the vertex shader).


The gist of this change is to move the "reversed DX" mapping from the
vertex shader to the projection matrix.

* Update shaders/src/getters.vs

Co-authored-by: Ben Doherty <bendoherty@google.com>

Co-authored-by: Ben Doherty <bendoherty@google.com>

shaders/src/getters.vs
@@ -142,12 +142,14 @@ vec4 computeWorldPosition() {
     mat4 transform = getWorldFromViewMatrix();
     vec3 position = getPosition().xyz;
     return mulMat4x4Float3(transform, position);
-#else
+#elif defined(VERTEX_DOMAIN_DEVICE)
     mat4 transform = getWorldFromClipMatrix();
     vec4 position = transform * getPosition();
     if (abs(position.w) < MEDIUMP_FLT_MIN) {
         position.w = position.w < 0.0 ? -MEDIUMP_FLT_MIN : MEDIUMP_FLT_MIN;
     }
     return position * (1.0 / position.w);
+#else
+#error Unknown Vertex Domain
 #endif
 }

filament/backend/src/metal/MetalDriver.mm
@@ -667,8 +667,10 @@ bool MetalDriver::isFrameTimeSupported() {
 }
 
 math::float2 MetalDriver::getClipSpaceParams() {
-    // z-coordinate of clip-space is in [0,w]
-    return math::float2{ -0.5f, 0.5f };
+    // virtual and physical z-coordinate of clip-space is in [-w, 0]
+    // Note: this is actually never used (see: main.vs), but it's a backend API so we implement it
+    // properly.
+    return math::float2{ 1.0f, 0.0f };
 }
 
 uint8_t MetalDriver::getMaxDrawBuffers() {

filament/backend/src/noop/NoopDriver.cpp
@@ -165,7 +165,7 @@ bool NoopDriver::isFrameTimeSupported() {
 }
 
 math::float2 NoopDriver::getClipSpaceParams() {
-    return math::float2{ -1.0f, 0.0f };
+    return math::float2{ 1.0f, 0.0f };
 }
 
 uint8_t NoopDriver::getMaxDrawBuffers() {

filament/backend/src/opengl/OpenGLDriver.cpp
@@ -1565,7 +1565,10 @@ bool OpenGLDriver::isFrameTimeSupported() {
 
 math::float2 OpenGLDriver::getClipSpaceParams() {
     return mContext.ext.EXT_clip_control ?
-            math::float2{ -0.5f, 0.5f } : math::float2{ -1.0f, 0.0f };
+           // z-coordinate of virtual and physical clip-space is in [-w, 0]
+           math::float2{ 1.0f, 0.0f } :
+           // z-coordinate of virtual clip-space is in [-w,0], physical is in [-w, w]
+           math::float2{ 2.0f, -1.0f };
 }
 
 uint8_t OpenGLDriver::getMaxDrawBuffers() {

filament/backend/src/vulkan/VulkanDriver.cpp
@@ -821,8 +821,10 @@ bool VulkanDriver::isFrameTimeSupported() {
 }
 
 math::float2 VulkanDriver::getClipSpaceParams() {
-    // z-coordinate of clip-space is in [0,w]
-    return math::float2{ -0.5f, 0.5f };
+    // virtual and physical z-coordinate of clip-space is in [-w, 0]
+    // Note: this is actually never used (see: main.vs), but it's a backend API so we implement it
+    // properly.
+    return math::float2{ 1.0f, 0.0f };
 }
 
 uint8_t VulkanDriver::getMaxDrawBuffers() {

filament/src/Camera.cpp
@@ -168,10 +168,14 @@ void UTILS_NOINLINE FCamera::setProjection(Camera::Projection projection,
 }
 
 math::mat4 FCamera::getProjectionMatrix() const noexcept {
+    // This is where we transform the user clip-space (GL convention) to our virtual clip-space
+    // (inverted DX convention)
+    // Note that this math, ends up setting the projection matrix' p33 to 0, which is where we're
+    // getting back a lot of precision in the depth buffer.
     const mat4 m{ mat4::row_major_init{
             mScaling.x, 0.0, 0.0, mShiftCS.x,
             0.0, mScaling.y, 0.0, mShiftCS.y,
-            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, -0.5, 0.5,    // GL to inverted DX convention
             0.0, 0.0, 0.0, 1.0
     }};
     return m * mProjection;

filament/src/PostProcessManager.cpp
@@ -566,7 +566,7 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::screenSpaceAmbientOcclusion(
                 mi->setParameter("projectionScaleRadius",
                         projectionScale * options.radius);
                 mi->setParameter("depthParams",
-                        cameraInfo.projection[3][2] * 0.5f);
+                        -cameraInfo.projection[3][2]);  // z_v = -near / z_s
                 mi->setParameter("positionParams", float2{
                         invProjection[0][0], invProjection[1][1] } * 2.0f);
                 mi->setParameter("peak2", peak * peak);
@@ -935,12 +935,12 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::dof(FrameGraph& fg,
      *  -------------------------------
      *
      *  1/d is computed from the depth buffer value as:
-     *  (note: our z-buffer is encoded as reversed-z, so we use 1-z below)
+     *  (note: our Z clip space is 1 to 0 (inverted DirectX NDC))
      *
-     *          screen-space -> clip-space -> view-space -> distance (x-1)
+     *          screen-space -> clip-space -> view-space -> distance (*-1)
      *
-     *   v_s = { x, y, 1 - z, 1 }                 // screen space (reversed-z)
-     *   v_c = 2 * v_s - 1                        // clip space
+     *   v_s = { x, y, z, 1 }                     // screen space (reversed-z)
+     *   v_c = v_s                                // clip space (matches screen space)
      *   v   = inverse(projection) * v_c          // view space
      *   d   = -v.z / v.w                         // view space distance to camera
      *   1/d = -v.w / v.z
@@ -954,23 +954,23 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::dof(FrameGraph& fg,
      *
      * It comes that:
      *
-     *          -2C         C - A
-     *    1/d = --- . z  + -------
-     *           B            B
+     *           C          A
+     *    1/d = --- . z  - ---
+     *           B          B
      *
      * note: Here the result doesn't depend on {x, y}. This wouldn't be the case with a
      *       tilt-shift lens.
      *
      * Mathematica code:
      *      p = {{a, 0, b, 0}, {0, c, d, 0}, {0, 0, m22, m32}, {0, 0, m23, 0}};
-     *      v = {x, y, (1 - z)*2 - 1, 1};
+     *      v = {x, y, z, 1};
      *      f = Inverse[p].v;
      *      Simplify[f[[4]]/f[[3]]]
      *
      * Plugging this back into the expression of: coc(z) = Kc . Ks . (1 - S / d)
      * We get that:  coc(z) = C0 * z + C1
-     * With: C0 = - Kc * Ks * S * 2 * C / B
-     *       C1 =   Kc * Ks * (1 - S * (C - A) / B)
+     * With: C0 = - Kc * Ks * S * -C / B
+     *       C1 =   Kc * Ks * (1 + S * A / B)
      *
      * It's just a madd!
      */
@@ -982,8 +982,8 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::dof(FrameGraph& fg,
 
     auto const& p = cameraInfo.projection;
     const float2 cocParams = {
-              -K * focusDistance * 2.0 * p[2][3] / p[3][2],
-               K * (1.0 - focusDistance * (p[2][3] - p[2][2]) / p[3][2])
+              K * focusDistance * p[2][3] / p[3][2],
+              K * (1.0 + focusDistance * p[2][2] / p[3][2])
     };
 
     Blackboard& blackboard = fg.getBlackboard();

filament/src/ShadowMap.cpp
@@ -422,13 +422,13 @@ void ShadowMap::computeShadowCameraDirectional(
 
         // Computes St the transform to use in the shader to access the shadow map texture
         // i.e. it transform a world-space vertex to a texture coordinate in the shadow-map
-        const mat4f MbMt = getTextureCoordsMapping();
-        const mat4f St = MbMt * S;
+        const mat4 MbMt = getTextureCoordsMapping();
+        const mat4f St = mat4f(MbMt * S);
 
         // note: in texelSizeWorldSpace() below, we could use Mb * Mt * F * W because
         // L * Mp * Mv is a rigid transform (for directional lights)
         if (USE_LISPSM) {
-            mTexelSizeWs = texelSizeWorldSpace(Wp, MbMt * F);
+            mTexelSizeWs = texelSizeWorldSpace(Wp, mat4f(MbMt * F));
         } else {
             // We know we're using an ortho projection
             mTexelSizeWs = texelSizeWorldSpace(St.upperLeft());
@@ -486,9 +486,9 @@ void ShadowMap::computeShadowCameraSpot(math::float3 const& position, math::floa
     // Final shadow transform
     const mat4f S = MpMv;
 
-    const mat4f MbMt = getTextureCoordsMapping();
-    const mat4f St = MbMt * S;
-    mTexelSizeWs = texelSizeWorldSpace(Mp, MbMt);
+    const mat4 MbMt = getTextureCoordsMapping();
+    const mat4f St = mat4f(MbMt * S);
+    mTexelSizeWs = texelSizeWorldSpace(Mp, mat4f(MbMt));
 
     if (!mShadowMapInfo.vsm) {
         mLightSpace = St;
@@ -592,7 +592,8 @@ mat4f ShadowMap::applyLISPSM(math::mat4f& Wp,
 }
 
 
-mat4f ShadowMap::getTextureCoordsMapping() const noexcept {
+// Apply these remapping in double to maintain a high precision for the depth axis
+mat4 ShadowMap::getTextureCoordsMapping() const noexcept {
     // remapping from NDC to texture coordinates (i.e. [-1,1] -> [0, 1])
     // ([1, 0] for depth mapping)
     const mat4f Mt(mClipSpaceFlipped ? mat4f::row_major_init{
@@ -635,7 +636,7 @@ mat4f ShadowMap::getTextureCoordsMapping() const noexcept {
     }) : mat4f();
 
     // Compute shadow-map texture access transform
-    return Mf * Mb * Mv * Mt;
+    return mat4(Mf * Mb * Mv * Mt);
 }
 
 math::mat4f ShadowMap::computeVsmLightSpaceMatrix(const math::mat4f& lightSpacePcf,

commit 56aa25f251d601d5de726de12f27f33690166857

Fix VERTEX_DOMAIN_DEVICE world space
this fixes the skybox's fog.

this broke recently with #4440

shaders/src/getters.vs
@@ -144,7 +144,10 @@ vec4 computeWorldPosition() {
     return mulMat4x4Float3(transform, position);
 #elif defined(VERTEX_DOMAIN_DEVICE)
     mat4 transform = getWorldFromClipMatrix();
-    vec4 position = transform * getPosition();
+    vec4 p = getPosition();
+    // GL convention to inverted DX convention
+    p.z = p.z * -0.5 + 0.5;
+    vec4 position = transform * p;
     if (abs(position.w) < MEDIUMP_FLT_MIN) {
         position.w = position.w < 0.0 ? -MEDIUMP_FLT_MIN : MEDIUMP_FLT_MIN;
     }