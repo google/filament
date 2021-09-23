34b1356090350684816e8cb2317dfc48afe4128d
Levente Koncz <levente.koncz@shapr3d.com>
2021-09-23 16:44:21 +0200

    Compiler: more robust check for framebuffer fetch support (some macs support it too, not just iPads)

--- filament/backend/src/metal/MetalDriver.mm
+++ filament/backend/src/metal/MetalDriver.mm
@@ -662,11 +662,11 @@ bool MetalDriver::isRenderTargetFormatSupported(TextureFormat format) {
 }
 
 bool MetalDriver::isFrameBufferFetchSupported() {
-#if defined(IOS) && !defined(FILAMENT_IOS_SIMULATOR)
-    return true;
-#else
+    // Newer Intel cards support it too, but it's not documented
+    if (@available(macOS 10.15, iOS 13, *)) {
+        return [mContext->device supportsFamily:MTLGPUFamilyApple1];
+    }
     return false;
-#endif
 }
 
 bool MetalDriver::isFrameTimeSupported() {
