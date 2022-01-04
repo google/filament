e6d8c6a968d83b1458041462157613a402bf2953
Benjamin Doherty <bendoherty@google.com>
2022-01-04 15:36:41 -0800

    Fade out SSR near maxRayTraceDistance

--- shaders/src/light_reflections.fs
+++ shaders/src/light_reflections.fs
@@ -234,14 +234,20 @@ void evaluateScreenSpaceReflections(vec3 r, inout vec4 Fr) {
         highp vec4 reprojected = frameUniforms.ssrReprojection * vec4(vsHitPoint, 1.0f);
         reprojected *= (1.0 / reprojected.w);
 
-        // Compute the screen-space reflection's contribution, fading the reflections out near the
-        // edges.
+        // Compute the screen-space reflection's contribution.
         // TODO: parameterize fadeRate.
         const float fadeRate = 12.0f;
-        vec2 f = max(fadeRate * abs(reprojected.xy - 0.5f) - (fadeRate / 2.0f - 1.0f), 0.0f);
-        float fade = saturate(1.0 - dot(f, f));
 
-        Fr = vec4(textureLod(light_ssr, reprojected.xy, 0.0f).rgb, fade);
+        // Fade the reflections out near the edges.
+        vec2 edgeFactor = max(fadeRate * abs(reprojected.xy - 0.5f) - (fadeRate / 2.0f - 1.0f), 0.0f);
+        float edgeFade = saturate(1.0 - dot(edgeFactor, edgeFactor));
+
+        // Fade the reflections out near maxRayTraceDistance.
+        float t = distance(vsOrigin, vsHitPoint) / maxRayTraceDistance;
+        float distFactor = max(fadeRate * (t - 0.5f) - (fadeRate / 2.0f - 1.0f), 0.0f);
+        float distanceFade = saturate(1.0 - (distFactor, distFactor));
+
+        Fr = vec4(textureLod(light_ssr, reprojected.xy, 0.0f).rgb, edgeFade * distanceFade);
     }
 }
 
