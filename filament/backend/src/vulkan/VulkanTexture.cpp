e04c1b82eb72ed1ac47335d6fce39cfe8de5fa59
daemyung jang <dm86.jang@gmail.com>
2022-01-14 18:06:38 +0900

    Transition the initial image layout if TextureUsage::SAMPLEABLE

--- filament/backend/src/vulkan/VulkanTexture.cpp
+++ filament/backend/src/vulkan/VulkanTexture.cpp
@@ -177,7 +177,7 @@ VulkanTexture::VulkanTexture(VulkanContext& context, SamplerType target, uint8_t
     getImageView(mPrimaryViewRange);
 
     // Transition the layout of each image slice.
-    if (any(usage & (TextureUsage::COLOR_ATTACHMENT | TextureUsage::DEPTH_ATTACHMENT))) {
+    if (any(usage & (TextureUsage::COLOR_ATTACHMENT | TextureUsage::DEPTH_ATTACHMENT | TextureUsage::SAMPLEABLE))) {
         const uint32_t layers = mPrimaryViewRange.layerCount;
         transitionImageLayout(mContext.commands->get().cmdbuffer, textureTransitionHelper({
                 .image = mTextureImage,
