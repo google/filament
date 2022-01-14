02de1ad5e95ba2607299a7b6f4c6d3d935425639
ViktorHeisenberger <91476779+ViktorHeisenberger@users.noreply.github.com>
2022-01-14 17:18:08 +0100

    Filament - Bump iOS deployment target to 13.0 (#22)
    * Bump iOS deployment target to 13.0, json.hpp uses std::filesystem
    
    * Remove unnecessary branching for iOS Simulator and macOS Catalyst

--- third_party/clang/iOS.cmake
+++ third_party/clang/iOS.cmake
@@ -15,15 +15,10 @@ set(DIST_ARCH ${IOS_ARCH})
 
 add_definitions(-DIOS)
 
-set(IOS_MIN_TARGET "11.0")
+set(IOS_MIN_TARGET "13.0")
 
 if(PLATFORM_NAME STREQUAL "iphonesimulator")
     add_definitions(-DFILAMENT_IOS_SIMULATOR)
-    # The simulator only supports iOS >= 13.0
-    set(IOS_MIN_TARGET "13.0")
-elseif(PLATFORM_NAME STREQUAL "macosx")
-    # Mac Catalyst only supports iOS >= 13.0
-    set(IOS_MIN_TARGET "13.0")
 endif()
 
 SET(CMAKE_SYSTEM_NAME Darwin)
