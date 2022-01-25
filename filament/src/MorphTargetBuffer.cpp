ab7f35903e6a06d0f270b906aec9c7a299d51acf
Mathias Agopian <mathias@google.com>
2022-01-25 12:18:50 -0800

    A morphing buffer must be bound when skinning is active
    This is because we're using the same program variant for skinning
    and morphing, in the skinning-only case, the buffer won't be accessed
    in the shader, but it must be present.
    
    fixes #5085

--- filament/src/MorphTargetBuffer.cpp
+++ filament/src/MorphTargetBuffer.cpp
@@ -118,10 +118,19 @@ void FMorphTargetBuffer::terminate(FEngine& engine) {
     driver.destroyTexture(mTbHandle);
 }
 
+Handle<HwSamplerGroup> FMorphTargetBuffer::createDummySampleGroup(FEngine& engine) noexcept {
+    DriverApi& driver = engine.getDriverApi();
+    Handle<HwSamplerGroup> sgh = driver.createSamplerGroup(PerRenderPrimitiveMorphingSib::SAMPLER_COUNT);
+    backend::SamplerGroup group(PerRenderPrimitiveMorphingSib::SAMPLER_COUNT);
+    group.setSampler(PerRenderPrimitiveMorphingSib::TARGETS, engine.getOneTextureArray(), {});
+    driver.updateSamplerGroup(sgh, std::move(group.toCommandStream()));
+    return sgh;
+}
+
 void FMorphTargetBuffer::setPositionsAt(FEngine& engine, size_t targetIndex, math::float3 const* positions, size_t count) {
     ASSERT_PRECONDITION(targetIndex < mCount, "targetIndex must be < count");
 
-    auto size = getSize(mVertexCount);
+    const size_t size = getSize(mVertexCount);
 
     ASSERT_PRECONDITION((int)sizeof(math::float3) * count <= size,
             "MorphTargetBuffer (size=%lu) overflow (size=%lu)",
