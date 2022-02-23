6420bc6dd86a73f927b51cb1076ff8efe88eee95
Philip Rideout <philiprideout@gmail.com>
2022-02-23 09:32:37 -0800

    Vulkan: fix backend tests (especially ReadPixels Y flip).

--- filament/backend/src/vulkan/VulkanDriver.cpp
+++ filament/backend/src/vulkan/VulkanDriver.cpp
@@ -324,7 +324,9 @@ void VulkanDriver::terminate() {
     delete mContext.commands;
     delete mContext.emptyTexture;
 
-    destruct<VulkanRenderTarget>(mContext.defaultRenderTarget);
+    if (mContext.defaultRenderTarget) {
+        destruct<VulkanRenderTarget>(mContext.defaultRenderTarget);
+    }
 
     mBlitter.shutdown();
 
@@ -1575,7 +1577,11 @@ void VulkanDriver::readPixels(Handle<HwRenderTarget> src, uint32_t x, uint32_t y
     vkMapMemory(device, stagingMemory, 0, VK_WHOLE_SIZE, 0, (void**) &srcPixels);
     srcPixels += subResourceLayout.offset;
 
-    const bool flipY = false;
+    // NOTE: the reasons for this are unclear, but issuing ReadPixels on a VkImage that has been
+    // extracted from a swap chain does not need a Y flip, but explicitly created VkImages do. (The
+    // former can be tested with "Export Screenshots" in gltf_viewer, the latter can be tested with
+    // test_ReadPixels.cpp). We've seen this behavior with both SwiftShader and MoltenVK.
+    const bool flipY = !srcTarget->isSwapChain();
 
     if (!DataReshaper::reshapeImage(&pbd, getComponentType(srcFormat), srcPixels,
             subResourceLayout.rowPitch, width, height, swizzle, flipY)) {
