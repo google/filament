90c23a7d5d11b2749bebf15c4d16c488f5bed3a7
Ben Doherty <bendoherty@google.com>
2022-03-30 23:25:27 +0000

    Correctly prepare color grading as subpass programs (#5384)

--- filament/src/PostProcessManager.cpp
+++ filament/src/PostProcessManager.cpp
@@ -2293,8 +2293,8 @@ void PostProcessManager::colorGradingPrepareSubpass(DriverApi& driver,
     mi->commit(driver);
 
     // load both variants
-    material.getMaterial(mEngine)->getProgram(Variant{Variant::type_t(PostProcessVariant::TRANSLUCENT)});
-    material.getMaterial(mEngine)->getProgram(Variant{Variant::type_t(PostProcessVariant::OPAQUE)});
+    material.getMaterial(mEngine)->prepareProgram(Variant{Variant::type_t(PostProcessVariant::TRANSLUCENT)});
+    material.getMaterial(mEngine)->prepareProgram(Variant{Variant::type_t(PostProcessVariant::OPAQUE)});
 }
 
 void PostProcessManager::colorGradingSubpass(DriverApi& driver,
@@ -2318,7 +2318,7 @@ void PostProcessManager::customResolvePrepareSubpass(DriverApi& driver, CustomRe
     FMaterialInstance* mi = material.getMaterialInstance(mEngine);
     mi->setParameter("direction", op == CustomResolveOp::COMPRESS ? 1.0f : -1.0f),
     mi->commit(driver);
-    material.getMaterial(mEngine)->getProgram(Variant{});
+    material.getMaterial(mEngine)->prepareProgram(Variant{});
 }
 
 void PostProcessManager::customResolveSubpass(DriverApi& driver) noexcept {
