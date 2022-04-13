google/filament

commit daff8fea9f828e1eca5a8f486dd6102552da5493

Android Viewer: fix URI bug when dropping some zips.
Dragging a zip file into the Remote Viewer page would fail if it
contained a glTF with `./` prefixes in the `images` section. The best
fix is to use Java's proper URI object rather than trying to parse the
string.

android/samples/sample-gltf-viewer/src/main/java/com/google/android/filament/gltf/MainActivity.kt
@@ -36,6 +36,7 @@ import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileInputStream
 import java.io.RandomAccessFile
+import java.net.URI
 import java.nio.Buffer
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
@@ -118,7 +119,7 @@ class MainActivity : Activity() {
         }
 
         // FXAA is pretty cheap and helps a lot
-        view.antiAliasing = View.AntiAliasing.FXAA;
+        view.antiAliasing = View.AntiAliasing.FXAA
 
         // ambient occlusion is the cheapest effect that adds a lot of quality
         view.ambientOcclusionOptions = view.ambientOcclusionOptions.apply {
@@ -217,13 +218,13 @@ class MainActivity : Activity() {
 
                 val sky = Skybox.Builder().environment(skyboxTexture).build(engine)
 
-                specularFilter.destroy();
-                equirectToCubemap.destroy();
-                context.destroy();
+                specularFilter.destroy()
+                equirectToCubemap.destroy()
+                context.destroy()
 
                 // destroy the previous IBl
-                engine.destroyIndirectLight(modelViewer.scene.indirectLight!!);
-                engine.destroySkybox(modelViewer.scene.skybox!!);
+                engine.destroyIndirectLight(modelViewer.scene.indirectLight!!)
+                engine.destroySkybox(modelViewer.scene.skybox!!)
 
                 modelViewer.scene.skybox = sky
                 modelViewer.scene.indirectLight = ibl
@@ -243,7 +244,7 @@ class MainActivity : Activity() {
         val (zipStream, zipFile) = withContext(Dispatchers.IO) {
             val file = File.createTempFile("incoming", "zip", cacheDir)
             val raf = RandomAccessFile(file, "rw")
-            raf.getChannel().write(message.buffer);
+            raf.channel.write(message.buffer)
             message.buffer = null
             raf.seek(0)
             Pair(FileInputStream(file), file)
@@ -298,19 +299,16 @@ class MainActivity : Activity() {
 
         // The gltf is often not at the root level (e.g. if a folder is zipped) so
         // we need to extract its path in order to resolve the embedded uri strings.
-        var gltfPrefix = gltfPath!!.substringBeforeLast('/', "")
-        if (gltfPrefix.isNotEmpty()) {
-            gltfPrefix += "/"
-        }
+        var prefix = URI(gltfPath!!).resolve("..")
 
         withContext(Dispatchers.Main) {
             if (gltfPath!!.endsWith(".glb")) {
                 modelViewer.loadModelGlb(gltfBuffer)
             } else {
                 modelViewer.loadModelGltf(gltfBuffer) { uri ->
-                    val path = gltfPrefix + uri
+                    val path = prefix.resolve(uri).toString()
                     if (!pathToBufferMapping.contains(path)) {
-                        Log.e(TAG, "Could not find $path in the zip.")
+                        Log.e(TAG, "Could not find '$uri' in zip using prefix '$prefix'")
                         setStatusText("Zip is missing $path")
                     }
                     pathToBufferMapping[path]
@@ -343,12 +341,10 @@ class MainActivity : Activity() {
         clearStatusText()
         titlebarHint.text = message.label
         CoroutineScope(Dispatchers.IO).launch {
-            if (message.label.endsWith(".zip")) {
-                loadZip(message)
-            } else if (message.label.endsWith(".hdr")) {
-                loadHdr(message)
-            } else {
-                loadGlb(message)
+            when {
+                message.label.endsWith(".zip") -> loadZip(message)
+                message.label.endsWith(".hdr") -> loadHdr(message)
+                else -> loadGlb(message)
             }
         }
     }

commit c886a3dceed5d535ba506282ed04a93679f8fc4f

sample-gltf-viewer: fix regression with zip files.
Path resolution was broken with #5285, which only worked when the glTF
happened to be at root level. This new fix has been tested against:

- My Sponza zip file that hass all files in a "sponza" folder.
- My Bistro zip file, which has resources in a child folder relative to
  the gltf file
- A self-created flat zip using IridescentDishWithOlives, with
  everything at root level.

Side note: Bistro still fails on a Pixel 6 Pro Mali but for an unrelated
reason: (null deref in backend::TimerQueryNative::queryResultAvailable)

android/samples/sample-gltf-viewer/src/main/java/com/google/android/filament/gltf/MainActivity.kt
@@ -297,9 +297,10 @@ class MainActivity : Activity() {
 
         val gltfBuffer = pathToBufferMapping[gltfPath]!!
 
-        // The gltf is often not at the root level (e.g. if a folder is zipped) so
-        // we need to extract its path in order to resolve the embedded uri strings.
-        var prefix = URI(gltfPath!!).resolve("..")
+        // In a zip file, the gltf file might be in the same folder as resources, or in a different
+        // folder. It is crucial to test against both of these cases. In any case, the resource
+        // paths are all specified relative to the location of the gltf file.
+        var prefix = URI(gltfPath!!).resolve(".")
 
         withContext(Dispatchers.Main) {
             if (gltfPath!!.endsWith(".glb")) {
@@ -308,7 +309,7 @@ class MainActivity : Activity() {
                 modelViewer.loadModelGltf(gltfBuffer) { uri ->
                     val path = prefix.resolve(uri).toString()
                     if (!pathToBufferMapping.contains(path)) {
-                        Log.e(TAG, "Could not find '$uri' in zip using prefix '$prefix'")
+                        Log.e(TAG, "Could not find '$uri' in zip using prefix '$prefix' and base path '${gltfPath!!}'")
                         setStatusText("Zip is missing $path")
                     }
                     pathToBufferMapping[path]