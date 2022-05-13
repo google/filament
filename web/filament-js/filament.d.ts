61be9a705ecc09cfb8bac1796376fbf056e865da
Brian Stringham <bjstringham@verisk.com>
2022-05-13 12:28:42 -0600

    WebGL: Export Engine::Config

--- web/filament-js/filament.d.ts
+++ web/filament-js/filament.d.ts
@@ -504,8 +504,16 @@ interface Filamesh {
     indexBuffer: IndexBuffer;
 }
 
+export interface Engine$Config {
+    minCommandBufferSizeMB: number;
+    commandBufferSizeMB: number;
+    perFrameCommandsSizeMB: number;
+    perRenderPassArenaSizeMB: number;
+    driverHandleArenaSizeMB: number;
+}
+
 export class Engine {
-    public static create(canvas: HTMLCanvasElement, contextOptions?: object): Engine;
+    public static create(canvas: HTMLCanvasElement, contextOptions?: object, config?: Engine$Config): Engine;
     public execute(): void;
     public createCamera(entity: Entity): Camera;
     public createMaterial(urlOrBuffer: BufferReference): Material;
