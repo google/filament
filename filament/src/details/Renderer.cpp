c049a1bfff0851098bf640159fb6c5daab88d5d4
Philip Rideout <philiprideout@gmail.com>
2022-05-13 12:15:12 -0700

    Workaround: partially revert "don't issue a flush..." (#5557)
    This is a temporary workaround for a memory corruption issue observed on
    some devices from a specific vendor. We will try to make this workaround
    more targeted in a subequent change.
    
    Partial revert for b2cdf9f2b41ec732abd7bd56639b2c3c9316acc0.

--- filament/src/details/Renderer.cpp
+++ filament/src/details/Renderer.cpp
@@ -192,7 +192,6 @@ bool FRenderer::beginFrame(FSwapChain* swapChain, uint64_t vsyncSteadyClockTimeN
     const time_point<steady_clock> appVsync(vsyncSteadyClockTimeNano ? userVsync : now);
 
     mFrameId++;
-    mViewRenderedCount = 0;
 
     { // scope for frame id trace
         char buf[64];
@@ -421,13 +420,9 @@ void FRenderer::render(FView const* view) {
     }
 
     if (UTILS_LIKELY(view && view->getScene())) {
-        if (mViewRenderedCount) {
-            // this is a good place to kick the GPU, since we've rendered a View before
-            // and we're about to render another one.
-            mEngine.getDriverApi().flush();
-        }
+        // NOTE: in the past we tried to kick the GPU here with a flush (b2cdf9f), but this
+        // was problematic on certain devices. b/232224942
         renderInternal(view);
-        mViewRenderedCount++;
     }
 }
 
