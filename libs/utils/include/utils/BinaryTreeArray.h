3ffe63c02c581f2c520733a63bbc5181fdea5872
Mathias Agopian <mathias@google.com>
2022-08-23 15:49:01 -0700

    don't use std::is_pod which is soon to be deprecated
    fixes #5980

--- libs/utils/include/utils/BinaryTreeArray.h
+++ libs/utils/include/utils/BinaryTreeArray.h
@@ -31,7 +31,7 @@ class BinaryTreeArray {
 
     // Simple fixed capacity stack
     template<typename TYPE, size_t CAPACITY,
-            typename = typename std::enable_if<std::is_pod<TYPE>::value>::type>
+            typename = typename std::enable_if<std::is_trivial<TYPE>::value>::type>
     class stack {
         TYPE mElements[CAPACITY];
         size_t mSize = 0;
