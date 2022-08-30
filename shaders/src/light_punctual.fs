be12049a87bc5f291ecd8f3f10935620f44c39d9
Mathias Agopian <mathias@google.com>
2022-08-30 11:31:32 -0700

    The default arena sizes in the makefile didn't match the code
    Also added some ascii art to explain better how the various arenas are
    used.

--- shaders/src/light_punctual.fs
+++ shaders/src/light_punctual.fs
@@ -189,8 +189,8 @@ void evaluatePunctualLights(const MaterialInputs material,
     // Each froxel contains how many lights can influence
     // the current fragment. A froxel also contains a record offset that
     // tells us where the indices of those lights are in the records
-    // texture. The records texture contains the indices of the actual
-    // light data in the lightsUniforms uniform buffer
+    // buffer. The records buffer contains the indices of the actual
+    // light data in the lightsUniforms UBO.
 
     uint index = froxel.recordOffset;
     uint end = index + froxel.count;
