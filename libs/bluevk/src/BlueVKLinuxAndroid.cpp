25f3db1da991b91e4289efbe0fc6891b9e76ca5a
Yuri Victorovich <yuri@FreeBSD.org>
2022-09-09 13:13:02 -0700

    FreeBSD patches

--- libs/bluevk/src/BlueVKLinuxAndroid.cpp
+++ libs/bluevk/src/BlueVKLinuxAndroid.cpp
@@ -42,7 +42,7 @@ bool loadLibrary() {
         path = FILAMENT_VKLIBRARY_PATH;
 #elif defined(__ANDROID__)
         path = "libvulkan.so";
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__FreeBSD__)
         path = "libvulkan.so.1";
 #else
 #error "This file should only be compiled for Android or Linux"
