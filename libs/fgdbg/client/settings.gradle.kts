8c1dd0db816ae6e760a41c301fa510ce4e0bf2db
Fernando Raviola <raviola@google.com>
2022-10-26 09:38:21 +1100

    fgdbg client project structure + simple server connection (#6210)
    * fgdbg client project structure + simple server connection
    
    * pr feedback

---
+++ libs/fgdbg/client/settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+    
+}
+rootProject.name = "fgdbg"
