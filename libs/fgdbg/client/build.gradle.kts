df737936f462ec1ad1542309a6f61b1f55ea9a59
Fernando Raviola <raviola@google.com>
2022-10-26 09:38:21 +1100

    fgdbg client project structure + simple server connection (#6210)
    * fgdbg client project structure + simple server connection
    
    * pr feedback

---
+++ libs/fgdbg/client/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.1"
+}
+
+group = "com.google.filament"
+version = "1.0"
+val ktorVersion: String by project
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven {
+        url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        name = "ktor-eap"
+    }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    dependencies {
+        implementation("io.ktor:ktor-client-core:$ktorVersion")
+        implementation("io.ktor:ktor-client-cio:$ktorVersion")
+        implementation("io.ktor:ktor-client-websockets:$ktorVersion")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "fgdbg"
+            packageVersion = "1.0.0"
+        }
+    }
+}
