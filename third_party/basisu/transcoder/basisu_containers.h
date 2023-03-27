01c1c9b4676b4eb42b5591b40732c5747171eb5e
Romain Guy <romainguy@curious-creature.com>
2023-03-27 16:50:49 -0700

    Fix emscripten compilation errors and warnings
    These errors and warnings only appear with more recent versions of
    the emscripten SDK.

--- third_party/basisu/transcoder/basisu_containers.h
+++ third_party/basisu/transcoder/basisu_containers.h
@@ -189,7 +189,7 @@ namespace basisu
 #define BASISU_IS_SCALAR_TYPE(T) (scalar_type<T>::cFlag)
 
 #if defined(__GNUC__) && __GNUC__<5
-   #define BASISU_IS_TRIVIALLY_COPYABLE(...) __has_trivial_copy(__VA_ARGS__)
+   #define BASISU_IS_TRIVIALLY_COPYABLE(...) __is_trivially_copyable(__VA_ARGS__)
 #else
    #define BASISU_IS_TRIVIALLY_COPYABLE(...) std::is_trivially_copyable<__VA_ARGS__>::value
 #endif
