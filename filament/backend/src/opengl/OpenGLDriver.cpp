332b924325fe38deb49dbbc1080249fd224aeab1
Ben Doherty <bendoherty@google.com>
2023-05-23 14:28:50 -0700

    Better fix for WebGL debug assertion (#6832)

--- filament/backend/src/opengl/OpenGLDriver.cpp
+++ filament/backend/src/opengl/OpenGLDriver.cpp
@@ -2719,8 +2719,8 @@ void OpenGLDriver::endRenderPass(int) {
         discardFlags &= ~TargetBufferFlags::STENCIL;
     }
 
-    if (rt->gl.isDefault &&
-            mCurrentDrawSwapChain && mCurrentDrawSwapChain->swapChain) {
+    if (rt->gl.isDefault) {
+        assert_invariant(mCurrentDrawSwapChain);
         discardFlags &= ~mPlatform.getPreservedFlags(mCurrentDrawSwapChain->swapChain);
     }
 
