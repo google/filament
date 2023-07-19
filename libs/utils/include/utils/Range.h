6fb6cbb7f8936dd5dae7e369e996b0955c607409
Y-way <young_way2007@126.com>
2023-07-19 13:02:55 +0800

    Build error on msvc 2022

--- libs/utils/include/utils/Range.h
+++ libs/utils/include/utils/Range.h
@@ -34,7 +34,7 @@ struct Range {
     bool contains(const T& t) const noexcept { return first <= t && t < last; }
 
     bool overlaps(const Range<T>& that) const noexcept {
-        return that.first < this->last && that.last > this->first;
+        return (that.first < this->last) && (that.last > this->first);
     }
 
     class const_iterator {
