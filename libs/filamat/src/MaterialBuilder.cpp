653218e558a0f39677a79605c4de7aaea6c3f87d
Benjamin Doherty <bendoherty@google.com>
2023-08-02 15:26:25 -0700

    Filter out stereo variant for feature level 0

--- libs/filamat/src/MaterialBuilder.cpp
+++ libs/filamat/src/MaterialBuilder.cpp
@@ -1160,6 +1160,7 @@ error:
         mVariantFilter |= uint32_t(UserVariantFilterBit::SKINNING);
         mVariantFilter |= uint32_t(UserVariantFilterBit::VSM);
         mVariantFilter |= uint32_t(UserVariantFilterBit::SSR);
+        mVariantFilter |= uint32_t(UserVariantFilterBit::STE);
     }
 
     std::vector<Variant> variants;
