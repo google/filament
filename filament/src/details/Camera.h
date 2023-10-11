320c1026dba8b48c7915a95210c6ddafd4615308
Mustafa Uzun <mustafauzun0@gmail.com>
2023-10-11 13:56:43 +0300

    fix: CameraInfo.clipTransform typo

--- filament/src/details/Camera.h
+++ filament/src/details/Camera.h
@@ -241,7 +241,7 @@ struct CameraInfo {
     math::mat4f eyeFromView[CONFIG_STEREOSCOPIC_EYES];  // eye view matrix (only for stereoscopic)
     math::mat4 worldOrigin;                             // world origin transform (already applied
                                                         // to model and view)
-    math::float4 clipTransfrom{1, 1, 0, 0};  // clip-space transform, only for VERTEX_DOMAIN_DEVICE
+    math::float4 clipTransform{1, 1, 0, 0};  // clip-space transform, only for VERTEX_DOMAIN_DEVICE
     float zn{};                              // distance (positive) to the near plane
     float zf{};                              // distance (positive) to the far plane
     float ev100{};                           // exposure
