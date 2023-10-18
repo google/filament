7c4b225b9b9b54eed8bfa699d8a6e64e678dbc35
Powei Feng <powei@google.com>
2023-10-18 16:23:25 -0700

    vk: support stencil format in swapchain
    Fixes #7233
    FIXES=302197523

--- filament/backend/src/vulkan/platform/VulkanPlatform.cpp
+++ filament/backend/src/vulkan/platform/VulkanPlatform.cpp
@@ -516,9 +516,8 @@ VkFormatList findAttachmentDepthFormats(VkPhysicalDevice device) {
         VK_FORMAT_D32_SFLOAT,
         VK_FORMAT_X8_D24_UNORM_PACK32,
 
-        // TODO: support stencil attachments
-        // VK_FORMAT_D32_SFLOAT_S8_UINT,
-        // VK_FORMAT_D24_UNORM_S8_UINT,
+        VK_FORMAT_D32_SFLOAT_S8_UINT,
+        VK_FORMAT_D24_UNORM_S8_UINT,
     };
     std::vector<VkFormat> selectedFormats;
     for (VkFormat format: formats) {
