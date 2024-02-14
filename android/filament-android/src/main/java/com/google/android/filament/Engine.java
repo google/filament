b498de3586a2aac0d6eb262c497d95454a04164b
Sungun Park <sungunpark@google.com>
2024-02-13 19:22:26 -0800

    Update android/filament-android/src/main/java/com/google/android/filament/Engine.java
    Co-authored-by: Ben Doherty <bendoherty@google.com>

--- android/filament-android/src/main/java/com/google/android/filament/Engine.java
+++ android/filament-android/src/main/java/com/google/android/filament/Engine.java
@@ -364,7 +364,7 @@ public class Engine {
          * The type of technique for stereoscopic rendering.
          *
          * This setting determines the algorithm used when stereoscopic rendering is enabled. This
-         * decision applies the entire Engine during the lifetime of the Engine. E.g., multiple
+         * decision applies to the entire Engine for the lifetime of the Engine. E.g., multiple
          * Views created from the Engine must use the same stereoscopic type.
          *
          * Each view can enable stereoscopic rendering via the StereoscopicOptions::enable flag.
