de6df6dc0ebea83d70b3e944536867f25ed90874
Benjamin Doherty <bendoherty@google.com>
2024-03-14 10:40:26 -0700

    Bump MATERIAL_VERSION to 51

--- libs/filabridge/include/filament/MaterialEnums.h
+++ libs/filabridge/include/filament/MaterialEnums.h
@@ -28,7 +28,7 @@
 namespace filament {
 
 // update this when a new version of filament wouldn't work with older materials
-static constexpr size_t MATERIAL_VERSION = 50;
+static constexpr size_t MATERIAL_VERSION = 51;
 
 /**
  * Supported shading models
