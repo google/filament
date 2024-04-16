c8098a4c15562f807d66806b1f7b01280c093030
Powei Feng <powei@google.com>
2024-04-16 10:46:48 -0700

    gltfio: add Asset/Resource extended implementations (#7678)
    * gltfio: add Asset/Resource extended implementations
    
     - Add gltfio/src/extended to implement an alternate loader for
       primitives. This is largely based on the implementation in
       AssetLoader/ResourceLoader
     - Able to correctly produce flat shading from gltf that only have
       vertex positions and indices.
     - This is not hooked into current code and should have no
       practical effect on gltfio.

--- libs/gltfio/src/extended/AssetLoaderExtended.h
+++ libs/gltfio/src/extended/AssetLoaderExtended.h
@@ -17,14 +17,100 @@
 #ifndef GLTFIO_ASSETLOADEREXTENDED_H
 #define GLTFIO_ASSETLOADEREXTENDED_H
 
+#include "../FFilamentAsset.h"
+#include "../Utility.h"
+
+#include <backend/BufferDescriptor.h>
+#include <gltfio/AssetLoader.h>
+
 #include <cgltf.h>
 
+#include <string>
+
 namespace filament::gltfio {
+
+struct Primitive;
+struct FFilamentAsset;
+class DracoCache;
+
+using BufferDescriptor = filament::backend::BufferDescriptor;
 
 // The cgltf attribute is a type and the attribute index
 struct Attribute {
     cgltf_attribute_type type; // positions, tangents
     int index;
+};
+
+// The Filament Attribute is defined as a type and a slot.
+struct FilamentAttribute {
+    VertexAttribute attribute;
+    int slot;
+};
+
+// This will be removed when the extended classes are properly integrated.
+struct TmpBufferSlot {
+    // Used to denote a generated buffer. Set as `index in `CgltfAttribute`.
+    static constexpr int const GENERATED_0_INDEX = -1;
+    static constexpr int const GENERATED_1_INDEX = -2;
+
+    VertexBuffer* vertices = nullptr;
+    IndexBuffer* indices = nullptr;
+    MorphTargetBuffer* target = nullptr;
+    int slot = -1;
+    size_t sizeInBytes = 0;
+
+    void* data = nullptr;
+
+    // MorphTarget-only data;
+    struct {
+        short4* tbn = nullptr;
+        float3* positions = nullptr;
+    } targetData;
+};
+
+// This will be removed when the extended classes are properly integrated.
+struct TmpAssetConfigurationExtended {
+    //! gltfio/ResourceLoader.h
+    char const* gltfPath;
+};
+
+// AssetLoaderExtended performs the same task as AssetLoader. Specifically, it takes the data from
+// cgltf and store them in CPU memory. These buffers are then forwarded to ResourceLoader for
+// uplaoding to GPU. The difference between this class AssetLoader is that tangent space computation
+// are computed here (and not in ResourceLoader). The reason for this change is that
+// TangentSpaceMesh will remesh the input and possibly change the indices, vertex count, and
+// triangle counts, and so those changes must be resolved before the buffers are sent to the GPU.
+struct AssetLoaderExtended {
+    using BufferSlot = TmpBufferSlot;
+    using AssetConfigurationExtended = TmpAssetConfigurationExtended;
+    using Output = Primitive;
+
+    struct Input {
+        cgltf_data* gltf;
+        cgltf_primitive* prim;
+        char const* name;
+        DracoCache* dracoCache;
+        Material* material;
+    };
+
+    AssetLoaderExtended(AssetConfigurationExtended const& config, Engine* engine,
+            MaterialProvider& materials)
+        : mEngine(engine),
+          mGltfPath(config.gltfPath),
+          mMaterials(materials),
+          mUriDataCache(std::make_shared<UriDataCache>()) {}
+
+    ~AssetLoaderExtended() = default;
+
+    bool createPrimitive(Input* input, Output* out, std::vector<BufferSlot>& outSlots);
+
+    UriDataCacheHandle getUriDataCache() const noexcept { return mUriDataCache; }
+
+private:
+    filament::Engine* mEngine;
+    std::string mGltfPath;
+    MaterialProvider& mMaterials;
+    UriDataCacheHandle mUriDataCache;
 };
 
 } // namespace filament::gltfio
---
+++ libs/gltfio/src/extended/ResourceLoaderExtended.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ResourceLoaderExtended.h"
+
+#include "../FFilamentAsset.h"
+
+#include <backend/BufferDescriptor.h>
+#include <filament/BufferObject.h>
+
+#include <cgltf.h>
+
+#include <vector>
+
+static const auto FREE_CALLBACK = [](void* mem, size_t, void*) {
+    free(mem);
+ };
+
+namespace filament::gltfio {
+
+
+void ResourceLoaderExtended::loadResources(std::vector<BufferSlot> const& slots,
+        filament::Engine* engine, std::vector<BufferObject*>& bufferObjects) {
+    for (auto& slot: slots) {
+        size_t const byteCount = slot.sizeInBytes;
+        if (slot.vertices) {
+            BufferObject* bo = BufferObject::Builder().size(byteCount).build(*engine);
+            bo->setBuffer(*engine, BufferDescriptor(slot.data, byteCount, FREE_CALLBACK));
+            slot.vertices->setBufferObjectAt(*engine, slot.slot, bo);
+            bufferObjects.push_back(bo);
+        }
+        if (slot.indices) {
+            slot.indices->setBuffer(*engine, BufferDescriptor(slot.data, byteCount, FREE_CALLBACK));
+        }
+        if (slot.target) {
+            assert_invariant(slot.targetData.positions && slot.targetData.tbn);
+            slot.target->setPositionsAt(*engine, slot.slot,
+                    (float3 const*) slot.targetData.positions, slot.target->getVertexCount());
+            slot.target->setTangentsAt(*engine, slot.slot, (short4 const*) slot.targetData.tbn,
+                    slot.target->getVertexCount());
+
+            free(slot.targetData.positions);
+            free(slot.targetData.tbn);
+        }
+    }
+}
+
+} // namespace filament::gltfio
