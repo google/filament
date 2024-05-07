a2cbfe814b83d9a14b92a87dd4a4ada21040764a
Benjamin Doherty <bendoherty@google.com>
2024-05-07 16:30:51 -0700

    Metal: log slow buffer allocation times

--- filament/backend/src/metal/MetalBufferPool.mm
+++ filament/backend/src/metal/MetalBufferPool.mm
@@ -42,8 +42,12 @@ MetalBufferPoolEntry const* MetalBufferPool::acquireBuffer(size_t numBytes) {
     }
 
     // We were not able to find a sufficiently large stage, so create a new one.
-    id<MTLBuffer> buffer = [mContext.device newBufferWithLength:numBytes
-                                                        options:MTLResourceStorageModeShared];
+    id<MTLBuffer> buffer = nil;
+    {
+        ScopedAllocationTimer timer("staging");
+        buffer = [mContext.device newBufferWithLength:numBytes
+                                              options:MTLResourceStorageModeShared];
+    }
     ASSERT_POSTCONDITION(buffer, "Could not allocate Metal staging buffer of size %zu.", numBytes);
     MetalBufferPoolEntry* stage = new MetalBufferPoolEntry {
         .buffer = { buffer, TrackedMetalBuffer::Type::STAGING },
