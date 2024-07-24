1685ff39abcce9094adc6781eaa155a4cc514c49
sinyoung.jeon <sinyoung.jeon@grapicar.com>
2024-07-24 09:34:08 +0900

    dd light control function

--- HighlevelAPIs/API_SOURCE/components/VzLight.cpp
+++ HighlevelAPIs/API_SOURCE/components/VzLight.cpp
@@ -18,4 +18,36 @@ namespace vzm
         COMP_LIGHT(lcm, ett, ins, -1.f);
         return lcm.getIntensity(ins);
     }
-}
+    void VzLight::SetColor(const float color[3]) {
+        COMP_LIGHT(lcm, ett, ins, );
+        float3 _color = *(float3*)color;
+        lcm.setColor(ins, _color);
+    }
+    void VzLight::GetColor(float color[3]) {
+        COMP_LIGHT(lcm, ett, ins, );
+        float3 _color = lcm.getColor(ins);
+        if (color) *(float3*)color = float3(_color);
+    }
+    void VzLight::SetRange(const float range) {
+        COMP_LIGHT(lcm, ett, ins, );
+        lcm.setFalloff(ins, range);
+        UpdateTimeStamp();
+
+    }
+    float VzLight::GetRange() const {
+        COMP_LIGHT(lcm, ett, ins, -1.f);
+        return lcm.getIntensity(ins);
+    }
+    void VzLight::SetCone(const float inner, const float outer) {
+        COMP_LIGHT(lcm, ett, ins, );
+        lcm.setSpotLightCone(ins, inner, outer);
+    }
+    float VzLight::getInnerCone() const {
+        COMP_LIGHT(lcm, ett, ins, -1.f);
+        return lcm.getSpotLightInnerCone(ins);
+    }
+    float VzLight::getOuterCone() const {
+        COMP_LIGHT(lcm, ett, ins, -1.f);
+        return lcm.getSpotLightOuterCone(ins);
+    }
+    }
