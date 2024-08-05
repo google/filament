1bb7600aa65f32a1a3b679e27a2c88b544f3d4ad
kunyoungparkpark <kunyoung.park@grapicar.com>
2024-08-05 14:52:51 +0900

    modify VulkanPlatform.cpp

--- filament/backend/src/vulkan/platform/VulkanPlatform.cpp
+++ filament/backend/src/vulkan/platform/VulkanPlatform.cpp
@@ -174,7 +174,7 @@ ExtensionSet getInstanceExtensions(
         VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
 #elif __linux__
         VK_KHR_XCB_SURFACE_EXTENSION_NAME,
-        VK_KHR_WAYLAND_SURFACE_EXTENSION_NAME, 
+        //VK_KHR_WAYLAND_SURFACE_EXTENSION_NAME, 
 #endif
   // Request these if available.
 #if FVK_ENABLED(FVK_DEBUG_DEBUG_UTILS)
