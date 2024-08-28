2202b5ab8c91d5251a50d50d780e3b10bceafee1
Mathias Agopian <mathias@google.com>
2024-08-28 13:21:35 -0700

    Add support for depth clamp and use it for shadows
    vk, metal and desktop gl all support depth clamp, GLES/android also does
    with ANGLE. Add support for it in the backends.
    
    use depth clamp to improve directional shadow quality; this allows
    to render everything that's behind the camera at the same "zero" depth,
    so we can reduce the depth range we need.
    
    Fixes #6293

--- filament/backend/src/metal/MetalContext.h
+++ filament/backend/src/metal/MetalContext.h
@@ -112,6 +112,7 @@ struct MetalContext {
     std::array<BufferState, MAX_SSBO_COUNT> ssboState;
     CullModeStateTracker cullModeState;
     WindingStateTracker windingState;
+    DepthClampStateTracker depthClampState;
     Handle<HwRenderPrimitive> currentRenderPrimitive;
 
     // State caches.
