1809aa7b118a8c9c51800ea07768fbc82c77dafb
Ben Doherty <bendoherty@google.com>
2024-10-03 17:23:20 -0700

    Fix OpenGL ES 2.0 descriptor set crash (#8176)

--- filament/backend/src/opengl/OpenGLDriver.cpp
+++ filament/backend/src/opengl/OpenGLDriver.cpp
@@ -3892,6 +3892,7 @@ void OpenGLDriver::bindDescriptorSet(
         // `offsets` data's lifetime will end when this function returns. We have to make a copy.
         // (the data is allocated inside the CommandStream)
         mBoundDescriptorSets[set].dsh = dsh;
+        assert_invariant(offsets.data() != nullptr || ds->getDynamicBufferCount() == 0);
         std::copy_n(offsets.data(), ds->getDynamicBufferCount(),
                 mBoundDescriptorSets[set].offsets.data());
     }
