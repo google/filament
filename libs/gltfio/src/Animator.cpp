cc22a016cbde501ff975e884082efcd7af6e9891
Evan Mezeske <emezeske@gmail.com>
2024-11-21 10:44:10 -0700

    When applying animations, allow the time to be exactly equal to the duration before taking the fmod(), so that it is possible to animate to the exact last keyframe. Without this change, trying to animate to the last keyframe wraps back to the first, which only works for animations that loop.

--- libs/gltfio/src/Animator.cpp
+++ libs/gltfio/src/Animator.cpp
@@ -264,7 +264,7 @@ size_t Animator::getAnimationCount() const {
 
 void Animator::applyAnimation(size_t animationIndex, float time) const {
     const Animation& anim = mImpl->animations[animationIndex];
-    time = fmod(time, anim.duration);
+    time = time == anim.duration ? time : fmod(time, anim.duration);
     TransformManager& transformManager = *mImpl->transformManager;
     transformManager.openLocalTransformTransaction();
     for (const auto& channel : anim.channels) {
