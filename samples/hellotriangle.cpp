c8eff833b72ed551397c36739125c349c42f53a3
buranya <38773493+271812697@users.noreply.github.com>
2024-12-30 20:56:32 +0800

    Update hellotriangle.cpp

--- samples/hellotriangle.cpp
+++ samples/hellotriangle.cpp
@@ -70,14 +70,14 @@ static constexpr uint16_t TRIANGLE_INDICES[3] = { 0, 1, 2 };
 static void printUsage(char* name) {
     std::string exec_name(utils::Path(name).getName());
     std::string usage(
-            "HELLOTRIANGLE renders a spinning colored triangle\n"
-            "Usage:\n"
-            "    HELLOTRIANGLE [options]\n"
-            "Options:\n"
-            "   --help, -h\n"
-            "       Prints this message\n\n"
-            "   --api, -a\n"
-            "       Specify the backend API: opengl, vulkan, or metal\n"
+        "HELLOTRIANGLE renders a spinning colored triangle\n"
+        "Usage:\n"
+        "    HELLOTRIANGLE [options]\n"
+        "Options:\n"
+        "   --help, -h\n"
+        "       Prints this message\n\n"
+        "   --api, -a\n"
+        "       Specify the backend API: opengl, vulkan, or metal\n"
     );
     const std::string from("HELLOTRIANGLE");
     for (size_t pos = usage.find(from); pos != std::string::npos; pos = usage.find(from, pos)) {
@@ -98,22 +98,25 @@ static int handleCommandLineArguments(int argc, char* argv[], App* app) {
     while ((opt = getopt_long(argc, argv, OPTSTR, OPTIONS, &option_index)) >= 0) {
         std::string arg(optarg ? optarg : "");
         switch (opt) {
-            default:
-            case 'h':
-                printUsage(argv[0]);
-                exit(0);
-            case 'a':
-                if (arg == "opengl") {
-                    app->config.backend = Engine::Backend::OPENGL;
-                } else if (arg == "vulkan") {
-                    app->config.backend = Engine::Backend::VULKAN;
-                } else if (arg == "metal") {
-                    app->config.backend = Engine::Backend::METAL;
-                } else {
-                    std::cerr << "Unrecognized backend. Must be 'opengl'|'vulkan'|'metal'.\n";
-                    exit(1);
-                }
-                break;
+        default:
+        case 'h':
+            printUsage(argv[0]);
+            exit(0);
+        case 'a':
+            if (arg == "opengl") {
+                app->config.backend = Engine::Backend::OPENGL;
+            }
+            else if (arg == "vulkan") {
+                app->config.backend = Engine::Backend::VULKAN;
+            }
+            else if (arg == "metal") {
+                app->config.backend = Engine::Backend::METAL;
+            }
+            else {
+                std::cerr << "Unrecognized backend. Must be 'opengl'|'vulkan'|'metal'.\n";
+                exit(1);
+            }
+            break;
         }
     }
     return optind;
@@ -126,42 +129,42 @@ int main(int argc, char** argv) {
     handleCommandLineArguments(argc, argv, &app);
 
     auto setup = [&app](Engine* engine, View* view, Scene* scene) {
-        app.skybox = Skybox::Builder().color({0.1, 0.125, 0.25, 1.0}).build(*engine);
+        app.skybox = Skybox::Builder().color({ 0.1, 0.125, 0.25, 1.0 }).build(*engine);
         scene->setSkybox(app.skybox);
         view->setPostProcessingEnabled(false);
         static_assert(sizeof(Vertex) == 12, "Strange vertex size.");
         app.vb = VertexBuffer::Builder()
-                .vertexCount(3)
-                .bufferCount(1)
-                .attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT2, 0, 12)
-                .attribute(VertexAttribute::COLOR, 0, VertexBuffer::AttributeType::UBYTE4, 8, 12)
-                .normalized(VertexAttribute::COLOR)
-                .build(*engine);
+            .vertexCount(3)
+            .bufferCount(1)
+            .attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT2, 0, 12)
+            .attribute(VertexAttribute::COLOR, 0, VertexBuffer::AttributeType::UBYTE4, 8, 12)
+            .normalized(VertexAttribute::COLOR)
+            .build(*engine);
         app.vb->setBufferAt(*engine, 0,
-                VertexBuffer::BufferDescriptor(TRIANGLE_VERTICES, 36, nullptr));
+            VertexBuffer::BufferDescriptor(TRIANGLE_VERTICES, 36, nullptr));
         app.ib = IndexBuffer::Builder()
-                .indexCount(3)
-                .bufferType(IndexBuffer::IndexType::USHORT)
-                .build(*engine);
+            .indexCount(3)
+            .bufferType(IndexBuffer::IndexType::USHORT)
+            .build(*engine);
         app.ib->setBuffer(*engine,
-                IndexBuffer::BufferDescriptor(TRIANGLE_INDICES, 6, nullptr));
+            IndexBuffer::BufferDescriptor(TRIANGLE_INDICES, 6, nullptr));
         app.mat = Material::Builder()
-                .package(RESOURCES_BAKEDCOLOR_DATA, RESOURCES_BAKEDCOLOR_SIZE)
-                .build(*engine);
+            .package(RESOURCES_BAKEDCOLOR_DATA, RESOURCES_BAKEDCOLOR_SIZE)
+            .build(*engine);
         app.renderable = EntityManager::get().create();
         RenderableManager::Builder(1)
-                .boundingBox({{ -1, -1, -1 }, { 1, 1, 1 }})
-                .material(0, app.mat->getDefaultInstance())
-                .geometry(0, RenderableManager::PrimitiveType::TRIANGLES, app.vb, app.ib, 0, 3)
-                .culling(false)
-                .receiveShadows(false)
-                .castShadows(false)
-                .build(*engine, app.renderable);
+            .boundingBox({ { -1, -1, -1 }, { 1, 1, 1 } })
+            .material(0, app.mat->getDefaultInstance())
+            .geometry(0, RenderableManager::PrimitiveType::TRIANGLES, app.vb, app.ib, 0, 3)
+            .culling(false)
+            .receiveShadows(false)
+            .castShadows(false)
+            .build(*engine, app.renderable);
         scene->addEntity(app.renderable);
         app.camera = utils::EntityManager::get().create();
         app.cam = engine->createCamera(app.camera);
         view->setCamera(app.cam);
-    };
+        };
 
     auto cleanup = [&app](Engine* engine, View*, Scene*) {
         engine->destroy(app.skybox);
@@ -171,20 +174,20 @@ int main(int argc, char** argv) {
         engine->destroy(app.ib);
         engine->destroyCameraComponent(app.camera);
         utils::EntityManager::get().destroy(app.camera);
-    };
+        };
 
     FilamentApp::get().animate([&app](Engine* engine, View* view, double now) {
         constexpr float ZOOM = 1.5f;
         const uint32_t w = view->getViewport().width;
         const uint32_t h = view->getViewport().height;
-        const float aspect = (float) w / h;
+        const float aspect = (float)w / h;
         app.cam->setProjection(Camera::Projection::ORTHO,
             -aspect * ZOOM, aspect * ZOOM,
             -ZOOM, ZOOM, 0, 1);
         auto& tcm = engine->getTransformManager();
         tcm.setTransform(tcm.getInstance(app.renderable),
-                filament::math::mat4f::rotation(now, filament::math::float3{ 0, 0, 1 }));
-    });
+            filament::math::mat4f::rotation(now, filament::math::float3{ 0, 0, 1 }));
+        });
 
     FilamentApp::get().run(app.config, setup, cleanup);
 
