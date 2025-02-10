8edc387d3a999f3af09ca665105da58da328dea3
Doris Wu <doriswu@google.com>
2025-02-10 23:02:43 +0800

    Cull unused resources

--- filament/src/fg/FrameGraph.cpp
+++ filament/src/fg/FrameGraph.cpp
@@ -522,20 +522,21 @@ fgviewer::FrameGraphInfo FrameGraph::getFrameGraphInfo(const char *viewName) con
     }
 
     std::unordered_map<fgviewer::ResourceId, fgviewer::FrameGraphInfo::Resource> resources;
-    for (const auto &resource: mResourceNodes) {
+    for (const auto &resource_node: mResourceNodes) {
         std::vector<fgviewer::FrameGraphInfo::Resource::Property> resourceProps;
         // TODO: Fill in resource properties
-        fgviewer::ResourceId id = resource->getId();
-        if (resource->getRefCount() == 0)
+        fgviewer::ResourceId id = resource_node->getId();
+        auto resource = getResource(resource_node->resourceHandle);
+        if (resource->refcount == 0)
             continue;
-        if (resource->getParentNode() != nullptr) {
+        if (resource_node->getParentNode() != nullptr) {
             resourceProps.emplace_back(fgviewer::FrameGraphInfo::Resource::Property {
                 .name = "is_subresource",
-                .value = utils::to_string(resource->getParentNode()->getId())
+                .value = utils::to_string(resource_node->getParentNode()->getId())
             });
         }
         resources.emplace(id, fgviewer::FrameGraphInfo::Resource(
-                              id, utils::CString(resource->getName()),
+                              id, utils::CString(resource_node->getName()),
                               std::move(resourceProps))
         );
     }
