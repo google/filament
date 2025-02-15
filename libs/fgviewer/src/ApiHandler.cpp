9ca0594d7c400d742f2dfc25b0b042ec1004577c
Doris Wu <doriswu@google.com>
2025-02-15 10:27:42 +0800

    [Depends on #8344] Implement ApiHandler functions and add JsonWriter (#8379)
    * Introduce FrameGraphInfo class
    
    * Move the assignment into pimpl
    
    * Make ctors explicit
    
    * Add ctors to fg info structs
    
    * Revert the macro change to align with existing
    
    * Address the comments
    
    * Remove pimpl and move func def to .cc
    
    * Fix
    
    * Convert the FrameGraph to FrameGraphInfo
    
    * Initialize and update debug server on engine side
    
    * Fix compile error
    
    * Address the comments
    
    * Update
    
    * Use camelCase
    
    * Use camelCase
    
    * Update
    
    * Add JsonWriter to convert fginfo to json
    
    * Add getStatus api and implement it
    
    * Add increment
    
    * Implement GET apis
    
    * Fix compile error
    
    * Update
    
    * Use c_str_safe
    
    * Add operator== for FrameGraphInfo
    
    * Call updateFrameGraph in appropriate locations
    
    * Address the comments

--- libs/fgviewer/src/ApiHandler.cpp
+++ libs/fgviewer/src/ApiHandler.cpp
@@ -18,6 +18,7 @@
 #include "ApiHandler.h"
 
 #include <fgviewer/DebugServer.h>
+#include <fgviewer/JsonWriter.h>
 
 #include <utils/FixedCapacityVector.h>
 #include <utils/Log.h>
@@ -40,29 +41,96 @@ auto const error = [](int line, std::string const& uri) {
 
 } // anonymous
 
-bool ApiHandler::handleGetApiFgInfo(struct mg_connection* conn,
-        struct mg_request_info const* request) {
-    auto const softError = [conn, request](char const* msg) {
-        utils::slog.e << "[fgviewer] DebugServer: " << msg << ": " << request->query_string << utils::io::endl;
-        mg_printf(conn, kErrorHeader.data(), "application/txt");
-        mg_write(conn, msg, strlen(msg));
-        return true;
-    };
-
-    // TODO: Implement the method
-    return true;
-}
-
-void ApiHandler::addFrameGraph(FrameGraphInfo const* framegraph) {
-    // TODO: Implement the method
-}
-
-
 bool ApiHandler::handleGet(CivetServer* server, struct mg_connection* conn) {
     struct mg_request_info const* request = mg_get_request_info(conn);
     std::string const& uri = request->local_uri;
 
-    // TODO: Implement the method
+    if (uri.find("/api/status") == 0) {
+        return handleGetStatus(conn, request);
+    }
+
+    if (uri == "/api/framegraphs") {
+        std::unique_lock const lock(mServer->mViewsMutex);
+        mg_printf(conn, kSuccessHeader.data(), "application/json");
+        mg_printf(conn, "[");
+        int index = 0;
+        for (auto const& view: mServer->mViews) {
+            bool const last = (++index) == mServer->mViews.size();
+
+            JsonWriter writer;
+            if (!writer.writeFrameGraphInfo(view.second)) {
+                return error(__LINE__, uri);
+            }
+
+            mg_printf(conn, "{ \"fgid\": \"%8.8x\", %s } %s", view.first, writer.getJsonString(),
+                    last ? "" : ",");
+        }
+        mg_printf(conn, "]");
+        return true;
+    }
+
+    if (uri == "/api/framegraph") {
+        const FrameGraphInfo* result = getFrameGraphInfo(request);
+        if (!result) {
+            return error(__LINE__, uri);
+        }
+
+        JsonWriter writer;
+        if (!writer.writeFrameGraphInfo(*result)) {
+            return error(__LINE__, uri);
+        }
+        mg_printf(conn, kSuccessHeader.data(), "application/json");
+        mg_printf(conn, "{ %s }", writer.getJsonString());
+        return true;
+    }
+
+    return error(__LINE__, uri);
+}
+
+void ApiHandler::updateFrameGraph(ViewHandle view_handle) {
+    std::unique_lock const lock(mStatusMutex);
+    snprintf(statusFrameGraphId, sizeof(statusFrameGraphId), "%8.8x", view_handle);
+    mCurrentStatus++;
+    mStatusCondition.notify_all();
+}
+
+const FrameGraphInfo* ApiHandler::getFrameGraphInfo(struct mg_request_info const* request) {
+    size_t const qlength = strlen(request->query_string);
+    char fgid[9] = {};
+    if (mg_get_var(request->query_string, qlength, "fgid", fgid, sizeof(fgid)) < 0) {
+        return nullptr;
+    }
+    uint32_t const id = strtoul(fgid, nullptr, 16);
+    std::unique_lock const lock(mServer->mViewsMutex);
+    const auto it = mServer->mViews.find(id);
+    return it == mServer->mViews.end()
+        ? nullptr
+        : &(it->second);
+}
+
+bool ApiHandler::handleGetStatus(struct mg_connection* conn,
+                                 struct mg_request_info const* request) {
+    char const* qstr = request->query_string;
+    if (qstr && strcmp(qstr, "firstTime") == 0) {
+        mg_printf(conn, kSuccessHeader.data(), "application/txt");
+        mg_write(conn, "0", 1);
+        return true;
+    }
+
+    std::unique_lock<std::mutex> lock(mStatusMutex);
+    uint64_t const currentStatusCount = mCurrentStatus;
+    if (mStatusCondition.wait_for(lock, 10s,
+                                  [this, currentStatusCount] {
+                                      return currentStatusCount < mCurrentStatus;
+                                  })) {
+        mg_printf(conn, kSuccessHeader.data(), "application/txt");
+        mg_write(conn, statusFrameGraphId, 8);
+    } else {
+        mg_printf(conn, kSuccessHeader.data(), "application/txt");
+        // Use '1' to indicate a no-op.  This ensures that we don't block forever if the client is
+        // gone.
+        mg_write(conn, "1", 1);
+    }
     return true;
 }
 
