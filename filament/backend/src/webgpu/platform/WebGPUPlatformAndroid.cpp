88bce583f7597a9b94d1b4bef4354d53addff574
Andy Hovingh <6198728+AndyHovingh@users.noreply.github.com>
2025-03-02 11:47:26 -0600

    WebGPU: backend components created and conditionally logged
    Initial groundwork in creating WebGPU backend components, namely
    the instance, adapter, device, and queue.
    If configured to do so, the backend will print out details about
    these components.
    The samples/hellotriangle.cpp was slightly modified to include a
    webgpu option which allows for exercising the above, but does not
    yet draw anything to the screen/window.
    NOTE: This has only been sanity tested with hello triangle
    on Mac OS and the Android emulator at this time, NOT IOS,
    Windows, or Linux yet.

---
+++ filament/backend/src/webgpu/platform/WebGPUPlatformAndroid.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <backend/platforms/WebGPUPlatform.h>
+
+#include <utils/Panic.h>
+
+#include <webgpu/webgpu_cpp.h>
+
+#include <cstdint>
+
+/**
+ * Android OS specific implementation aspects of the WebGPU backend
+ */
+
+namespace filament::backend {
+
+wgpu::Surface WebGPUPlatform::createSurface(void* nativeWindow, uint64_t /*flags*/) {
+    wgpu::SurfaceSourceAndroidNativeWindow surfaceSourceAndroidWindow{};
+    surfaceSourceAndroidWindow.window = nativeWindow;
+    wgpu::SurfaceDescriptor surfaceDescriptor{
+        .nextInChain = &surfaceSourceAndroidWindow,
+        .label = "android_surface"
+    };
+    wgpu::Surface surface = mInstance.CreateSurface(&surfaceDescriptor);
+    FILAMENT_CHECK_POSTCONDITION(surface != nullptr) << "Unable to create Android-backed surface.";
+    return surface;
+}
+
+}// namespace filament::backend
