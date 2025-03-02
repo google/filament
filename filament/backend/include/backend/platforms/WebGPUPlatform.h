bbf835037de541be22df4a495673ca659763b517
Andy Hovingh <6198728+AndyHovingh@users.noreply.github.com>
2025-03-02 11:47:26 -0600

    WebGPU: backend components created and conditionally logged
    Initial groundwork in creating WebGPU backend components, namely
    the instance, adapter, device, and queue.
    If configured to do so, the backend will print out details about
    these components.
    The samples/hellotriangle.cpp was slightly modified to include a
    webgpu option which allows for exercising the above, but does not
    yet draw anything to the screen/window.
    NOTE: This has only been sanity tested with hello triangle
    on Mac OS and the Android emulator at this time, NOT IOS,
    Windows, or Linux yet.

---
+++ filament/backend/include/backend/platforms/WebGPUPlatform.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TNT_FILAMENT_BACKEND_PLATFORMS_WEBGPUPLATFORM_H
+#define TNT_FILAMENT_BACKEND_PLATFORMS_WEBGPUPLATFORM_H
+
+#include <cstdint>
+
+#include <webgpu/webgpu_cpp.h>
+
+#include "backend/Platform.h"
+
+namespace filament::backend {
+
+/**
+ * A Platform interface, handling the environment-specific concerns, e.g. OS, for creating a WebGPU
+ * driver (backend).
+ */
+class WebGPUPlatform final : public Platform {
+public:
+
+    struct SurfaceBundle final {
+        wgpu::Surface surface = nullptr;
+        // may or may not be defined based on the platform
+        wgpu::Extent2D fallbackExtent = {.width = 0, .height = 0};
+    };
+
+    WebGPUPlatform();
+    ~WebGPUPlatform() override = default;
+
+    [[nodiscard]] int getOSVersion() const noexcept final { return 0; }
+
+    [[nodiscard]] wgpu::Instance& getInstance() noexcept { return mInstance; }
+
+    //either returns a valid surface or panics
+    [[nodiscard]] SurfaceBundle createSurface(void* nativeWindow, uint64_t flags);
+    //either returns a valid adapter or panics
+    [[nodiscard]] wgpu::Adapter requestAdapter(wgpu::Surface const& surface);
+    //either returns a valid device or panics
+    [[nodiscard]] wgpu::Device requestDevice(wgpu::Adapter const& adapter);
+
+protected:
+    [[nodiscard]] Driver* createDriver(void* sharedContext,
+            const Platform::DriverConfig& driverConfig) noexcept override;
+
+private:
+    wgpu::Instance mInstance;
+};
+
+}// namespace filament::backend
+
+#endif// TNT_FILAMENT_BACKEND_PLATFORMS_WEBGPUPLATFORM_H
