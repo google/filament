3308ec7e347347a52a343598a9c1cc169355eabc
Eliza Velasquez <exv@google.com>
2025-03-13 17:59:50 -0700

    wip: editor stuff

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,15 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c++-mode . ((c-file-style . "filament")
+((c++-mode . ((eval . (progn
+                        (make-local-variable 'eglot-ignored-server-capabilities)
+                        (add-to-list 'eglot-ignored-server-capabilities
+                                     :documentOnTypeFormattingProvider)))
+              (c-file-style . "filament")
               (apheleia-inhibit . t)))
- (c-mode . ((c-file-style . "filament")
+ (c-mode . ((eval . (progn
+                      (make-local-variable 'eglot-ignored-server-capabilities)
+                      (add-to-list 'eglot-ignored-server-capabilities
+                                   :documentOnTypeFormattingProvider)))
+            (c-file-style . "filament")
             (apheleia-inhibit . t))))
