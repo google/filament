546ba48043042fe11d0c63db3e060a472e68bc8d
Powei Feng <powei@google.com>
2025-03-25 16:11:35 -0700

    Fix test breaking changes with workarounds (#8569)
    - Some tests require textures larger than 2048. Change the minimum
      to 4096 for now, and file proper bugs for tests.
    - The introduction of uint8_t to DescriptorSetLayoutBinding
      caused unintended padding with using this struct as a hash key.
      We comment out the relevent code for now.

--- filament/backend/src/vulkan/VulkanDriver.cpp
+++ filament/backend/src/vulkan/VulkanDriver.cpp
@@ -1146,7 +1146,7 @@ size_t VulkanDriver::getMaxUniformBufferSize() {
 
 size_t VulkanDriver::getMaxTextureSize(SamplerType) {
     // TODO: return the actual size instead of hardcoded value
-    return 2048;
+    return 4096;
 }
 
 size_t VulkanDriver::getMaxArrayTextureLayers() {
