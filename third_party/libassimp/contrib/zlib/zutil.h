82942de37183c10ad3341d0e10e186bd9072fc02
KeremTAN <keremtan.95@hotmail.com>
2025-04-03 14:17:46 +0300

    Fix platform-specific conditionals in zutil.h and pngpriv.h

--- third_party/libassimp/contrib/zlib/zutil.h
+++ third_party/libassimp/contrib/zlib/zutil.h
@@ -126,7 +126,7 @@ extern z_const char * const z_errmsg[10]; /* indexed by 2-zlib_error */
 #  endif
 #endif
 
-#if defined(MACOS) || defined(TARGET_OS_MAC)
+#if defined(MACOS)
 #  define OS_CODE  7
 #  ifndef Z_SOLO
 #    if defined(__MWERKS__) && __dest_os != __be_os && __dest_os != __win32_os
