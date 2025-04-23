c6ce4f1712da5a099cf7273a10d6179c0542263e
Ajmal Kunnummal <ajmalarkunnummal@gmail.com>
2025-04-23 08:17:12 -0700

    Revert initializing the transform

--- filament/backend/src/opengl/OpenGLDriver.h
+++ filament/backend/src/opengl/OpenGLDriver.h
@@ -161,7 +161,7 @@ public:
             uint8_t cur = 0;
             AcquiredImage acquired;
             AcquiredImage pending;
-            math::mat3f transform = math::mat3f();
+            math::mat3f transform;
         } user_thread;
 
          math::mat3f transform;
