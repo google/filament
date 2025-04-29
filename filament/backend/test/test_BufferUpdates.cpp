f2445a13708bec06d7d7094e701f2cc194511116
Matt Hoffman <matthewhoffman@google.com>
2025-04-29 17:11:13 -0500

    Prevent all backend vulkan tests from crashing.
    Many still fail that shouldn't though.

--- filament/backend/test/test_BufferUpdates.cpp
+++ filament/backend/test/test_BufferUpdates.cpp
@@ -84,7 +84,7 @@ TEST_F(BufferUpdatesTest, VertexBufferUpdate) {
 
         PipelineState state;
         state.program = shader.getProgram();
-        state.pipelineLayout.setLayout[1] = { shader.getDescriptorSetLayout() };
+        state.pipelineLayout.setLayout[0] = { shader.getDescriptorSetLayout() };
         state.rasterState.colorWrite = true;
         state.rasterState.depthWrite = false;
         state.rasterState.depthFunc = RasterState::DepthFunc::A;
@@ -199,8 +199,8 @@ TEST_F(BufferUpdatesTest, BufferObjectUpdateWithOffset) {
     params.flags.discardEnd = TargetBufferFlags::NONE;
     params.viewport.height = 512;
     params.viewport.width = 512;
-    renderTriangle({{ DescriptorSetLayoutHandle{}, shader.getDescriptorSetLayout() }},
-            renderTarget, swapChain, shader.getProgram(), params);
+    renderTriangle({ { shader.getDescriptorSetLayout() } }, renderTarget, swapChain,
+            shader.getProgram(), params);
 
     // Upload uniforms for the second triangle. To test partial buffer updates, we'll only update
     // color.b, color.a, scaleMinusOne, offset.x, and offset.y.
@@ -218,7 +218,7 @@ TEST_F(BufferUpdatesTest, BufferObjectUpdateWithOffset) {
 
     params.flags.clear = TargetBufferFlags::NONE;
     params.flags.discardStart = TargetBufferFlags::NONE;
-    renderTriangle({{ DescriptorSetLayoutHandle{}, shader.getDescriptorSetLayout() }},
+    renderTriangle({{ shader.getDescriptorSetLayout() }},
             renderTarget, swapChain, shader.getProgram(), params);
 
 
