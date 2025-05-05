00ede79dfd647aff10c076fb5b3d287a7a8c755f
Konrad Piascik <piascik@google.com>
2025-05-05 18:46:52 -0400

    Fix windows compile errors

--- filament/backend/src/webgpu/WebGPUHandles.cpp
+++ filament/backend/src/webgpu/WebGPUHandles.cpp
@@ -205,7 +205,7 @@ WebGPUDescriptorSetLayout::WebGPUDescriptorSetLayout(DescriptorSetLayout const& 
     // debugging. For now, hack an incrementing value.
     static int layoutNum = 0;
 
-    uint samplerCount =
+    unsigned int samplerCount =
             std::count_if(layout.bindings.begin(), layout.bindings.end(), [](auto& fEntry) {
                 return fEntry.type == DescriptorType::SAMPLER ||
                        fEntry.type == DescriptorType::SAMPLER_EXTERNAL;
@@ -403,7 +403,7 @@ wgpu::BindGroup WebGPUDescriptorSet::lockAndReturn(const wgpu::Device& device) {
     return mBindGroup;
 }
 
-void WebGPUDescriptorSet::addEntry(uint index, wgpu::BindGroupEntry&& entry) {
+void WebGPUDescriptorSet::addEntry(unsigned int index, wgpu::BindGroupEntry&& entry) {
     if (mBindGroup) {
         // We will keep getting hits from future updates, but shouldn't do anything
         // Filament guarantees this won't change after things have locked.
