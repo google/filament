8f5b76d94d413ccd4ef4795dfe56d3474f29c15c
Powei Feng <powei@google.com>
2025-07-02 10:46:47 -0700

    renderdiff: update get-mesa to use also macport on MacOS

--- docs_src/build/checks.py
+++ docs_src/build/checks.py
@@ -33,7 +33,7 @@ ROOT_DIR = os.path.join(CUR_DIR, '../../')
 def get_edited_files(commit_hash):
   INSERT = '#####?????'
   res, ret = execute(f'git show --name-only --pretty=%b{INSERT} {commit_hash}')
-  assert res == 0, 'Failed to get edited filed'
+  assert res == 0, f'Failed to get edited filed {res}: ' + ret
   files = []
   _, after = ret.split(INSERT)
   for r in filter(lambda a: len(a) > 0, after.split('\n')):
