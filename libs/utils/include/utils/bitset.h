6cb01e6c41b964bf56da9e71a7519573c644d313
Powei Feng <powei@google.com>
2025-07-25 15:44:24 -0700

    utils: add method for index of first set bit in bitset

--- libs/utils/include/utils/bitset.h
+++ libs/utils/include/utils/bitset.h
@@ -96,6 +96,17 @@ public:
         }
     }
 
+    size_t firstSetBit() const noexcept {
+        for (size_t i = 0; i < N; i++) {
+            if (T v = storage[i]) {
+                T k = utils::ctz(v);
+                v &= ~(T(1) << k);
+                return size_t(k + BITS_PER_WORD * i);
+            }
+        }
+        return std::numeric_limits<size_t>::max();
+    }
+
     size_t size() const noexcept { return N * BITS_PER_WORD; }
 
     bool empty() const noexcept { return none(); }
