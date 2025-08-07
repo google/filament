7608a3bbc61ff7c866e17d95ccf263994a69d3cd
Sungun Park <z3moon@gmail.com>
2025-08-06 19:37:32 -0700

    mat: Add CRC32 validation for material packages
    This commit introduces a CRC32 checksum to material packages to ensure
    data integrity.
    
    When a material is loaded, this checksum is verified. If the check
    fails, an error is logged, and the material fails to load. For older
    material packages without a CRC32 checksum, a warning is logged and
    proceed.
    
    BUGS=[373396840]

--- filament/src/details/Material.cpp
+++ filament/src/details/Material.cpp
@@ -188,6 +188,34 @@ Material* Material::Builder::build(Engine& engine) const {
         return nullptr;
     }
 
+    // Try checking CRC32 value for the package and skip if it's unavailable.
+    if (downcast(engine).features.material.check_crc32_after_loading) {
+        uint32_t parsedCrc32 = 0;
+        if (materialParser->getMaterialCrc32(&parsedCrc32)) {
+            size_t crc32ChunkSize = sizeof(uint64_t) + sizeof(uint32_t) + sizeof(uint32_t);
+            size_t originalSize = mImpl->mSize - crc32ChunkSize;
+            assert_invariant(mImpl->mSize > crc32ChunkSize);
+
+            std::vector<uint32_t> crc32Table;
+            hash::crc32GenerateTable(crc32Table);
+            uint32_t expectedCrc32 = hash::crc32Update(0, mImpl->mPayload, originalSize,
+                    crc32Table);
+            if (parsedCrc32 != expectedCrc32) {
+                CString name;
+                materialParser->getName(&name);
+                LOG(ERROR) << "The material '" << name.c_str_safe()
+                           << "' is corrupted: crc32_expected=" << expectedCrc32
+                           << ", crc32_parsed=" << parsedCrc32;
+                return nullptr;
+            }
+        } else {
+            // crc32 is not stored, probably due to an old version of material package.
+            CString name;
+            materialParser->getName(&name);
+            LOG(WARNING) << "The material '" << name.c_str_safe() << "' doesn't contain crc32";
+        }
+    }
+
     uint32_t v = 0;
     materialParser->getShaderModels(&v);
     bitset32 shaderModels;
@@ -223,7 +251,7 @@ Material* Material::Builder::build(Engine& engine) const {
                 materialParser->getName(&name);
                 LOG(WARNING) << "The stereoscopic type in the compiled material '"
                              << name.c_str_safe() << "' is " << (int) materialStereoscopicType
-                             << ", which is not compatiable with the engine's setting "
+                             << ", which is not compatible with the engine's setting "
                              << (int) engineStereoscopicType << ".";
             }
         }
