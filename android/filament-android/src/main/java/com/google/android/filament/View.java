google/filament

commit 48a2c6483b523b97bed76e54706a48c11ec05ca2

Add support for SGSR 1.0
SGSR stands for Snapdragon Game Super Resolution. It is an efficient
upscaler for mobile.

Split the upscaling passes into multiple methods in PostProcessManager.
Also, add a way to preserve the alpha channel in the FXAA pass.

android/filament-android/src/main/java/com/google/android/filament/View.java
@@ -1420,10 +1420,10 @@ public class View {
         /**
          * Upscaling quality
          * LOW:    bilinear filtered blit. Fastest, poor quality
-         * MEDIUM: AMD FidelityFX FSR1 w/ mobile optimizations
+         * MEDIUM: Qualcomm Snapdragon Game Super Resolution (SGSR) 1.0
          * HIGH:   AMD FidelityFX FSR1 w/ mobile optimizations
          * ULTRA:  AMD FidelityFX FSR1
-         *      FSR1 require a well anti-aliased (MSAA or TAA), noise free scene.
+         *      FSR1 and SGSR require a well anti-aliased (MSAA or TAA), noise free scene. Avoid FXAA and dithering.
          *
          * The default upscaling quality is set to LOW.
          */

filament/include/filament/Options.h
@@ -86,10 +86,10 @@ struct DynamicResolutionOptions {
     /**
      * Upscaling quality
      * LOW:    bilinear filtered blit. Fastest, poor quality
-     * MEDIUM: AMD FidelityFX FSR1 w/ mobile optimizations
+     * MEDIUM: Qualcomm Snapdragon Game Super Resolution (SGSR) 1.0
      * HIGH:   AMD FidelityFX FSR1 w/ mobile optimizations
      * ULTRA:  AMD FidelityFX FSR1
-     *      FSR1 require a well anti-aliased (MSAA or TAA), noise free scene.
+     *      FSR1 and SGSR require a well anti-aliased (MSAA or TAA), noise free scene. Avoid FXAA and dithering.
      *
      * The default upscaling quality is set to LOW.
      */

commit 6b932239b0616211ea710ae5e63bc8e94769e946

Fix dynamic resolution with TRANSLUCENT views
A few bugs in that area were introduced by #8391. Upscaling is 
supposed to perform the final blending if needed, but it didn't.
Transparency/blending is only supported by the bilinear upscaler, which
is automatically selected in that case.

The upscaling pass itself may include a final `RCAS` pass for
sharpening, in that case, blending must be performed then. So we added
the logic for that.


Fixes #9061

android/filament-android/src/main/java/com/google/android/filament/View.java
@@ -1390,7 +1390,7 @@ public class View {
      *
      * \note
      * Dynamic resolution is only supported on platforms where the time to render
-     * a frame can be measured accurately. On platform where this is not supported,
+     * a frame can be measured accurately. On platforms where this is not supported,
      * Dynamic Resolution can't be enabled unless minScale == maxScale.
      *
      * @see Renderer::FrameRateOptions
@@ -1423,9 +1423,12 @@ public class View {
          * MEDIUM: Qualcomm Snapdragon Game Super Resolution (SGSR) 1.0
          * HIGH:   AMD FidelityFX FSR1 w/ mobile optimizations
          * ULTRA:  AMD FidelityFX FSR1
-         *      FSR1 and SGSR require a well anti-aliased (MSAA or TAA), noise free scene. Avoid FXAA and dithering.
+         *      FSR1 and SGSR require a well anti-aliased (MSAA or TAA), noise free scene.
+         *      Avoid FXAA and dithering.
          *
          * The default upscaling quality is set to LOW.
+         *
+         * caveat: currently, 'quality' is always set to LOW if the View is TRANSLUCENT.
          */
         @NonNull
         public QualityLevel quality = QualityLevel.LOW;

filament/include/filament/Options.h
@@ -70,7 +70,7 @@ enum class BlendMode : uint8_t {
  *
  * \note
  * Dynamic resolution is only supported on platforms where the time to render
- * a frame can be measured accurately. On platform where this is not supported,
+ * a frame can be measured accurately. On platforms where this is not supported,
  * Dynamic Resolution can't be enabled unless minScale == maxScale.
  *
  * @see Renderer::FrameRateOptions
@@ -89,9 +89,12 @@ struct DynamicResolutionOptions {
      * MEDIUM: Qualcomm Snapdragon Game Super Resolution (SGSR) 1.0
      * HIGH:   AMD FidelityFX FSR1 w/ mobile optimizations
      * ULTRA:  AMD FidelityFX FSR1
-     *      FSR1 and SGSR require a well anti-aliased (MSAA or TAA), noise free scene. Avoid FXAA and dithering.
+     *      FSR1 and SGSR require a well anti-aliased (MSAA or TAA), noise free scene.
+     *      Avoid FXAA and dithering.
      *
      * The default upscaling quality is set to LOW.
+     *
+     * caveat: currently, 'quality' is always set to LOW if the View is TRANSLUCENT.
      */
     QualityLevel quality = QualityLevel::LOW;
 };

filament/src/PostProcessManager.cpp
@@ -3075,7 +3075,8 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::taa(FrameGraph& fg,
     // optional sharpen pass from FSR1
     if (taaOptions.sharpness > 0.0f) {
         input = rcas(fg, taaOptions.sharpness,
-                input, fg.getDescriptor(input), colorGradingConfig.translucent);
+                input, fg.getDescriptor(input),
+                colorGradingConfig.translucent ? RcasMode::ALPHA_PASSTHROUGH : RcasMode::OPAQUE);
     }
 
     struct ExportColorHistoryData {
@@ -3100,7 +3101,7 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::rcas(
         float const sharpness,
         FrameGraphId<FrameGraphTexture> const input,
         FrameGraphTexture::Descriptor const& outDesc,
-        bool const translucent) {
+        RcasMode const mode) {
 
     struct QuadBlitData {
         FrameGraphId<FrameGraphTexture> input;
@@ -3122,8 +3123,8 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::rcas(
                 auto const out = resources.getRenderPassInfo();
                 auto const& outputDesc = resources.getDescriptor(data.input);
 
-                PostProcessVariant const variant = translucent ?
-                        PostProcessVariant::TRANSLUCENT : PostProcessVariant::OPAQUE;
+                PostProcessVariant const variant = mode == RcasMode::OPAQUE ?
+                        PostProcessVariant::OPAQUE : PostProcessVariant::TRANSLUCENT;
 
                 auto const& material = getPostProcessMaterial("fsr_rcas");
                 FMaterialInstance* const mi =
@@ -3136,14 +3137,24 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::rcas(
                 mi->setParameter("resolution", float4{
                         outputDesc.width, outputDesc.height,
                         1.0f / outputDesc.width, 1.0f / outputDesc.height });
-                commitAndRenderFullScreenQuad(driver, out, mi, variant);
+                mi->commit(driver);
+                mi->use(driver);
+
+                auto pipeline = getPipelineState(material.getMaterial(mEngine), variant);
+                if (mode == RcasMode::BLENDED) {
+                    pipeline.rasterState.blendFunctionSrcRGB = BlendFunction::ONE;
+                    pipeline.rasterState.blendFunctionSrcAlpha = BlendFunction::ONE;
+                    pipeline.rasterState.blendFunctionDstRGB = BlendFunction::ONE_MINUS_SRC_ALPHA;
+                    pipeline.rasterState.blendFunctionDstAlpha = BlendFunction::ONE_MINUS_SRC_ALPHA;
+                }
+                renderFullScreenQuad(out, pipeline, driver);
                 unbindAllDescriptorSets(driver);
             });
 
     return ppFsrRcas->output;
 }
 
-FrameGraphId<FrameGraphTexture> PostProcessManager::upscale(FrameGraph& fg, bool translucent,
+FrameGraphId<FrameGraphTexture> PostProcessManager::upscale(FrameGraph& fg, bool const translucent,
     bool sourceHasLuminance, DynamicResolutionOptions dsrOptions,
     FrameGraphId<FrameGraphTexture> const input, filament::Viewport const& vp,
     FrameGraphTexture::Descriptor const& outDesc, SamplerMagFilter filter) noexcept {
@@ -3151,14 +3162,13 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::upscale(FrameGraph& fg, bool
     assert_invariant(fg.getSubResourceDescriptor(input).layer == 0);
     assert_invariant(fg.getSubResourceDescriptor(input).level == 0);
 
-    const bool lowQualityFallback = translucent;
-    if (lowQualityFallback) {
+    if (const bool lowQualityFallback = translucent; lowQualityFallback) {
         // FidelityFX-FSR nor SGSR support the source alpha channel currently
         dsrOptions.quality = QualityLevel::LOW;
     }
 
     if (dsrOptions.quality == QualityLevel::LOW) {
-        return upscaleBilinear(fg, dsrOptions, input, vp, outDesc, filter);
+        return upscaleBilinear(fg, translucent, dsrOptions, input, vp, outDesc, filter);
     }
     if (dsrOptions.quality == QualityLevel::MEDIUM) {
         return upscaleSGSR1(fg, sourceHasLuminance, dsrOptions, input, vp, outDesc);
@@ -3167,6 +3177,7 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::upscale(FrameGraph& fg, bool
 }
 
 FrameGraphId<FrameGraphTexture> PostProcessManager::upscaleBilinear(FrameGraph& fg,
+        bool const translucent,
         DynamicResolutionOptions dsrOptions, FrameGraphId<FrameGraphTexture> const input,
         filament::Viewport const& vp, FrameGraphTexture::Descriptor const& outDesc,
         SamplerMagFilter filter) noexcept {
@@ -3175,6 +3186,12 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::upscaleBilinear(FrameGraph& 
         FrameGraphId<FrameGraphTexture> input;
         FrameGraphId<FrameGraphTexture> output;
     };
+
+    // upscaleBilinear is responsible for upscaling AND blending the result (so we don't
+    // have to do it with an extra pass). Blending must then happen either during the upsacling
+    // or during the RCAS pass, whichever is last
+
+    bool const blended = translucent && (dsrOptions.sharpness == 0.0f);
 
     auto const& ppQuadBlit = fg.addPass<QuadBlitData>(dsrOptions.enabled ? "upscaling" : "compositing",
             [&](FrameGraph::Builder& builder, auto& data) {
@@ -3185,7 +3202,7 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::upscaleBilinear(FrameGraph& 
                         .attachments = { .color = { data.output } },
                         .clearFlags = TargetBufferFlags::DEPTH });
             },
-            [this, vp, filter](FrameGraphResources const& resources,
+            [this, blended, vp, filter](FrameGraphResources const& resources,
                     auto const& data, DriverApi& driver) {
                 bindPostProcessDescriptorSet(driver);
                 bindPerRenderableDescriptorSet(driver);
@@ -3217,6 +3234,13 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::upscaleBilinear(FrameGraph& 
                 auto out = resources.getRenderPassInfo();
 
                 auto pipeline = getPipelineState(material.getMaterial(mEngine));
+                if (blended) {
+                    pipeline.rasterState.blendFunctionSrcRGB = BlendFunction::ONE;
+                    pipeline.rasterState.blendFunctionSrcAlpha = BlendFunction::ONE;
+                    pipeline.rasterState.blendFunctionDstRGB = BlendFunction::ONE_MINUS_SRC_ALPHA;
+                    pipeline.rasterState.blendFunctionDstAlpha = BlendFunction::ONE_MINUS_SRC_ALPHA;
+                }
+
                 renderFullScreenQuad(out, pipeline, driver);
                 unbindAllDescriptorSets(driver);
             });
@@ -3226,7 +3250,7 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::upscaleBilinear(FrameGraph& 
     // if we had to take the low quality fallback, we still do the "sharpen pass"
     if (dsrOptions.sharpness > 0.0f) {
         output = rcas(fg, dsrOptions.sharpness, output, outDesc,
-                false /* translucent=false, because dst buffer is allocated */);
+                translucent ? RcasMode::BLENDED : RcasMode::OPAQUE);
     }
 
     // we rely on automatic culling of unused render passes
@@ -3439,7 +3463,7 @@ FrameGraphId<FrameGraphTexture> PostProcessManager::upscaleFSR1(FrameGraph& fg,
 
     auto output = ppQuadBlit->output;
     if (dsrOptions.sharpness > 0.0f) {
-        output = rcas(fg, dsrOptions.sharpness, output, outDesc, false);
+        output = rcas(fg, dsrOptions.sharpness, output, outDesc, RcasMode::OPAQUE);
     }
 
     // we rely on automatic culling of unused render passes

filament/src/PostProcessManager.h
@@ -246,7 +246,7 @@ public:
             FrameGraphId<FrameGraphTexture> input, Viewport const& vp,
             FrameGraphTexture::Descriptor const& outDesc, backend::SamplerMagFilter filter) noexcept;
 
-    FrameGraphId<FrameGraphTexture> upscaleBilinear(FrameGraph& fg,
+    FrameGraphId<FrameGraphTexture> upscaleBilinear(FrameGraph& fg, bool translucent,
             DynamicResolutionOptions dsrOptions, FrameGraphId<FrameGraphTexture> input,
             Viewport const& vp, FrameGraphTexture::Descriptor const& outDesc,
             backend::SamplerMagFilter filter) noexcept;
@@ -259,12 +259,18 @@ public:
             DynamicResolutionOptions dsrOptions, FrameGraphId<FrameGraphTexture> input,
             filament::Viewport const& vp, FrameGraphTexture::Descriptor const& outDesc) noexcept;
 
+    enum class RcasMode {
+        OPAQUE,
+        ALPHA_PASSTHROUGH,
+        BLENDED
+    };
+
     FrameGraphId<FrameGraphTexture> rcas(
             FrameGraph& fg,
             float sharpness,
             FrameGraphId<FrameGraphTexture> input,
             FrameGraphTexture::Descriptor const& outDesc,
-            bool translucent);
+            RcasMode mode);
 
     // color blitter using shaders
     FrameGraphId<FrameGraphTexture> blit(FrameGraph& fg, bool translucent,

filament/src/details/Renderer.cpp
@@ -1360,6 +1360,8 @@ void FRenderer::renderJob(RootArenaScope& rootArenaScope, FView& view) {
         }
 
         if (scaled) {
+            // upscale() below always performs the final blending if needed (View in `TRANSLUCENT`
+            // mode). So we can set `mightNeedFinalBlit` to `false` to avoid an extra copy.
             mightNeedFinalBlit = false;
             auto viewport = DEBUG_DYNAMIC_SCALING ? xvp : vp;
             bool const sourceHasLuminance = !needsAlphaChannel &&
@@ -1367,6 +1369,8 @@ void FRenderer::renderJob(RootArenaScope& rootArenaScope, FView& view) {
             input = ppm.upscale(fg, needsAlphaChannel, sourceHasLuminance, dsrOptions, input, xvp, {
                 .width = viewport.width, .height = viewport.height,
                 .format = colorGradingConfig.ldrFormat }, SamplerMagFilter::LINEAR);
+            // scaling has been applied, and offset removed
+            xvp = viewport;
             xvp.left = xvp.bottom = 0;
             svp = xvp;
         }

web/filament-js/filament.d.ts
@@ -1146,7 +1146,7 @@ export enum View$BlendMode {
  *
  * \note
  * Dynamic resolution is only supported on platforms where the time to render
- * a frame can be measured accurately. On platform where this is not supported,
+ * a frame can be measured accurately. On platforms where this is not supported,
  * Dynamic Resolution can't be enabled unless minScale == maxScale.
  *
  * @see Renderer::FrameRateOptions
@@ -1179,9 +1179,12 @@ export interface View$DynamicResolutionOptions {
      * MEDIUM: Qualcomm Snapdragon Game Super Resolution (SGSR) 1.0
      * HIGH:   AMD FidelityFX FSR1 w/ mobile optimizations
      * ULTRA:  AMD FidelityFX FSR1
-     *      FSR1 and SGSR require a well anti-aliased (MSAA or TAA), noise free scene. Avoid FXAA and dithering.
+     *      FSR1 and SGSR require a well anti-aliased (MSAA or TAA), noise free scene.
+     *      Avoid FXAA and dithering.
      *
      * The default upscaling quality is set to LOW.
+     *
+     * caveat: currently, 'quality' is always set to LOW if the View is TRANSLUCENT.
      */
     quality?: View$QualityLevel;
 }