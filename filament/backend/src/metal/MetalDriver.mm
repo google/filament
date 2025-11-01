320a5c91ad14a8e165fedc2452fba1a5b3a48581
Powei Feng <powei@google.com>
2025-11-01 16:38:59 -0700

    metal: adjust language preference
    In the case of unspecified language preference, we should return
    MSL first as oppse metal lib.

--- filament/backend/src/metal/MetalDriver.mm
+++ filament/backend/src/metal/MetalDriver.mm
@@ -1005,7 +1005,8 @@ ShaderModel MetalDriver::getShaderModel() const noexcept {
 
 utils::FixedCapacityVector<ShaderLanguage> MetalDriver::getShaderLanguages(
         ShaderLanguage preferredLanguage) const noexcept {
-    if (preferredLanguage == backend::ShaderLanguage::MSL) {
+    if (preferredLanguage == backend::ShaderLanguage::UNSPECIFIED ||
+            preferredLanguage == backend::ShaderLanguage::MSL) {
         return { backend::ShaderLanguage::MSL, backend::ShaderLanguage::METAL_LIBRARY };
     }
     return { backend::ShaderLanguage::METAL_LIBRARY, backend::ShaderLanguage::MSL };
