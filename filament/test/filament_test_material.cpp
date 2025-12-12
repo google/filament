cfd402f424113b510ed69e4a71d346b965509c46
Powei Feng <powei@google.com>
2025-12-12 12:01:40 -0800

    test: fix broken filament test again

--- filament/test/filament_test_material.cpp
+++ filament/test/filament_test_material.cpp
@@ -131,8 +131,3 @@ TEST(Material, MaterialWithoutSourceMaterialReturnsEmptySource) {
     engine->destroy(material);
     Engine::destroy(engine);
 }
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
