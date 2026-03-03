ecb4ad8d03448bdbd49fae3251def19387a50766
PatRibas <patrickwribas@gmail.com>
2026-03-03 13:08:13 -0800

    Add workaround for GLES2 mipmap requirements

--- filament/backend/src/opengl/GLDescriptorSet.cpp
+++ filament/backend/src/opengl/GLDescriptorSet.cpp
@@ -341,7 +341,41 @@ void GLDescriptorSet::bind(
                 if (arg.handle) {
                     GLTexture const* const t = handleAllocator.handle_cast<GLTexture*>(arg.handle);
                     gl.bindTexture(unit, t->gl.target, t->gl.id, t->gl.external);
-                    SamplerParams const params = arg.params;
+                    SamplerParams params = arg.params;
+
+                    // From https://registry.khronos.org/OpenGL-Refpages/es2.0/
+                    // GLES 2.0 will draw mipmapped samplers as black if the following conditions are not met:
+                    //
+                    // "...if the width or height of a texture image are not powers of two and either the
+                    // GL_TEXTURE_MIN_FILTER is set to one of the functions that requires mipmaps or the
+                    // GL_TEXTURE_WRAP_S or GL_TEXTURE_WRAP_T is not set to GL_CLAMP_TO_EDGE, then the
+                    // texture image unit will return (R, G, B, A) = (0, 0, 0, 1)."
+                    //
+                    // "If the texture has dimensions w × h , there are [floor(log2(max(w, h))) + 1] mipmap levels.
+                    // Level 0 is the original texture; level [floor(log2(max(w, h)))] is the final 1 × 1 mipmap."
+                    // "Suppose that a texture is accessed from a fragment shader or vertex shader and has set GL_TEXTURE_MIN_FILTER
+                    // to one of the functions that requires mipmaps. If either the dimensions of the texture images currently
+                    // defined (with previous calls to glTexImage2D, glCompressedTexImage2D, or glCopyTexImage2D) do not follow
+                    // the proper sequence for mipmaps (described above), or there are fewer texture images defined than are needed,
+                    // or the set of texture images were defined with different formats or types, then the texture image unit
+                    // will return (R, G, B, A) = (0, 0, 0, 1)."
+                    //
+                    // So we force the sampler to a valid state in those cases.
+                    auto isPowerOfTwo = [](uint32_t x) -> bool { return (x & (x - 1)) == 0 && x > 0; };
+                    bool textureIsNonPowerOfTwo = isPowerOfTwo(t->width) && isPowerOfTwo(t->height);
+
+                    uint32_t requiredMipLevels = (std::floor(std::log2(std::max(t->width, std::max(t->height, t->depth)))) + 1);
+                    bool textureHasRequiredMipLevels = t->levels == requiredMipLevels;
+
+                    bool samplerCanBeInvalid = params.filterMin > SamplerMinFilter::LINEAR || params.wrapS != SamplerWrapMode::CLAMP_TO_EDGE || params.wrapT != SamplerWrapMode::CLAMP_TO_EDGE;
+                    if (samplerCanBeInvalid && (textureIsNonPowerOfTwo || !textureHasRequiredMipLevels)) {
+                        if (params.filterMin > SamplerMinFilter::LINEAR) {
+                            params.filterMin = SamplerMinFilter::NEAREST;
+                        }
+                        params.wrapS = SamplerWrapMode::CLAMP_TO_EDGE;
+                        params.wrapT = SamplerWrapMode::CLAMP_TO_EDGE;
+                    }
+                    
                     glTexParameteri(t->gl.target, GL_TEXTURE_MIN_FILTER,
                             (GLint)GLUtils::getTextureFilter(params.filterMin));
                     glTexParameteri(t->gl.target, GL_TEXTURE_MAG_FILTER,
