62e0f550952d3d3deef124e423dd4066f4238bd2
Powei Feng <powei@google.com>
2026-04-21 22:53:53 -0700

    Update emsdk to 5.0.4
    - Bumped GITHUB_EMSDK_VERSION to 5.0.4 in build scripts and docs.
    - Fixed a compilation error in web/filament-js/jsbindings.cpp where stricter
      pointer binding rules in Emscripten 5.0.4 caused a build failure when
      returning a raw pointer wrapped in `emscripten::val`. Changed the lambda
      to return the raw pointer directly, leveraging Embind's `allow_raw_pointers()`.

--- web/filament-js/jsbindings.cpp
+++ web/filament-js/jsbindings.cpp
@@ -1861,14 +1861,9 @@ class_<KtxInfo>("KtxInfo")
 class_<MeshReader::MaterialRegistry>("MeshReader$MaterialRegistry")
     .constructor<>()
     .function("size", &MeshReader::MaterialRegistry::numRegistered)
-    .function("get", EMBIND_LAMBDA(val, (MeshReader::MaterialRegistry* self, std::string k), {
+    .function("get", EMBIND_LAMBDA(filament::MaterialInstance*, (MeshReader::MaterialRegistry* self, std::string k), {
           const utils::CString name(k.c_str(), k.size());
-          auto i = self->getMaterialInstance(name);
-          if (i == nullptr) {
-              return val::undefined();
-          } else {
-              return val(i);
-          }
+          return self->getMaterialInstance(name);
     }), allow_raw_pointers())
     .function("set", EMBIND_LAMBDA(void, (MeshReader::MaterialRegistry* self, std::string k, filament::MaterialInstance* v), {
           const utils::CString name(k.c_str(), k.size());
