505233af2c7b5d366d374641641a142c5c7be58d
Mathias Agopian <mathias@google.com>
2026-04-29 23:00:12 -0700

    utils: lighter way to track changed entities
    SingleInstanceComponentManager can now register a PagedArenaBitset*
    which it will use to record entities that have changed. No need for
    callbacks.

--- libs/utils/include/utils/SingleInstanceComponentManager.h
+++ libs/utils/include/utils/SingleInstanceComponentManager.h
@@ -72,6 +72,18 @@ public:
     void flushNotifications() noexcept;
 
     /**
+     * Registers a bitset to be updated when entities change.
+     * @param bitset A pointer to the PagedArenaBitset to register.
+     */
+    void registerBitset(PagedArenaBitset* bitset);
+
+    /**
+     * Unregisters a bitset.
+     * @param bitset A pointer to the PagedArenaBitset to unregister.
+     */
+    void unregisterBitset(PagedArenaBitset const* bitset);
+
+    /**
      * Records a change for the given entity.
      * Flushes notifications if the internal buffer becomes full.
      */
@@ -96,6 +108,7 @@ private:
     Entity mDirtyEntities[MAX_DIRTY_COUNT];
     size_t mDirtyCount = 0;
     std::vector<CallbackInfo> mChangeCallbacks;
+    std::vector<PagedArenaBitset*> mBitsets;
 };
 
 /*
