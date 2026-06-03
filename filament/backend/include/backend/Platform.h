3c6322afcbf91a8fdc0214ca685d6e2b814915d6
Mathias Agopian <mathias@google.com>
2026-06-03 12:17:01 -0500

    fix FrameInfo::presentDeadline
    There was two related issues. First, we got the present-deadline from
    ANativeWindow / EGL instead of from Choreographer. It's unclear how
    they differ, but the Choreographer one is the modern way of getting
    that information.
    
    The next problem is that we can't use timepoints because we can't
    correlate them to the user's view of the vsync time.  So instead
    we record deltas from the Choregrapher's vsync (normally it will match
    the user's, but it could be off by one frame), and we later recalculate
    the timepoint from the duration relative the user's vsync.

--- filament/backend/include/backend/Platform.h
+++ filament/backend/include/backend/Platform.h
@@ -114,11 +114,10 @@ public:
         duration_ns compositeInterval;
 
         /**
-         * The timestamp [ns] since epoch of the next time the compositor will begin composition.
-         * This is effectively the deadline for when the compositor must receive a newly queued
-         * frame.
+         * The time delta [ns] between the start of composition and the next time the compositor will begin composition.
+         * This is effectively the deadline for when the compositor must receive a newly queued frame.
          */
-        time_point_ns compositeDeadline;
+        duration_ns compositeDeadlineLatency;
 
         /**
          * The time delta [ns] between the start of composition and the expected present time of
