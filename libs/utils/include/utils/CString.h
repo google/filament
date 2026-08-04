70ab91a0c81cca6bb6a5eaeee12a671817f1419a
Benjamin Doherty <bendoherty@google.com>
2026-08-04 12:43:29 -0400

    Revert "Fix build issues in certain environments (#10218)"
    This reverts commit 268141a951b056cea583fa43dd4e48ed3e8a3414.

--- libs/utils/include/utils/CString.h
+++ libs/utils/include/utils/CString.h
@@ -22,9 +22,6 @@
 #include <utils/compiler.h>
 #include <utils/StaticString.h>
 
-#if defined(FILAMENT_USE_ABSEIL_LOGGING)
-#include <iosfwd>
-#endif
 #include <string_view>
 #include <type_traits>
 #include <utility>
@@ -360,10 +357,6 @@ private:
 
 #if !defined(NDEBUG)
     friend io::ostream& operator<<(io::ostream& out, const CString& rhs);
-#endif
-
-#if defined(FILAMENT_USE_ABSEIL_LOGGING)
-    friend std::ostream& operator<<(std::ostream& out, const CString& rhs);
 #endif
 
     struct Data {
--- libs/utils/include/utils/CallStack.h
+++ libs/utils/include/utils/CallStack.h
@@ -21,9 +21,6 @@
 #include <utils/compiler.h>
 #include <utils/sstream.h>
 
-#if defined(FILAMENT_USE_ABSEIL_LOGGING)
-#include <iosfwd>
-#endif
 #include <typeinfo>
 
 #include <stddef.h>
@@ -92,11 +89,6 @@ public:
      * program-counter recorded.
      */
     friend io::ostream& operator <<(io::ostream& stream, const CallStack& callstack);
-#if defined(FILAMENT_USE_ABSEIL_LOGGING)
-    friend std::ostream& operator <<(std::ostream& stream, const CallStack& callstack);
-#endif
-    template <typename Stream>
-    friend Stream& printCallStack(Stream& stream, CallStack const& callstack);
 
     // make Callstack compatible with Abseil's stringification utilities
     template<typename Sink>
