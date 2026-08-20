a011138aab7b2051495414513bf6d7d596f52a07
Romain Guy <romainguy@curious-creature.com>
2026-08-20 11:14:55 -0700

    Update android/filament-android/src/main/java/com/google/android/filament/LightManager.java

--- android/filament-android/src/main/java/com/google/android/filament/LightManager.java
+++ android/filament-android/src/main/java/com/google/android/filament/LightManager.java
@@ -526,7 +526,7 @@ public class LightManager {
               @IntRange(from = 1, to = 4) int cascades, float near, float far, float lambda) {
             if (splitPositions.length < cascades - 1) {
                 throw new ArrayIndexOutOfBoundsException(
-                        "splitPositions array length must be at least %d" + (cascades - 1));
+                        "splitPositions array length must be at least " + (cascades - 1));
             }
             nComputePracticalSplits(splitPositions, cascades, near, far, lambda);
         }
